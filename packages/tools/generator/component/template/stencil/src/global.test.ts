ed3089c9a9186cf806665998dc12f9882021fea6
aurore.stagnol <a.stagnol@gmail.com>
2023-04-24 18:05:46 +0200

    feat(generator): add global test to generator
    Signed-off-by: aurore.stagnol <a.stagnol@gmail.com>

---
+++ packages/tools/generator/component/template/stencil/src/global.test.ts
@@ -0,0 +1,8 @@
+// ###
+// global script file to include only in test mode.
+// it always has to include './components' and './global.ts'
+// then, you can import sub-component(s) needed in tests like this: import '@ovhcloud/ods-stencil/components/button';
+// ###
+
+import './components';
+import './global';

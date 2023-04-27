8377fe7b483fe1cfd018d5f9843d258b037d19fc
aurore.stagnol <a.stagnol@gmail.com>
2023-04-27 11:44:11 +0200

    fix(testing): rm components import in global test
    Signed-off-by: aurore.stagnol <a.stagnol@gmail.com>

--- packages/stencil/components/input/src/global.test.ts
+++ packages/stencil/components/input/src/global.test.ts
@@ -1,8 +1,7 @@
 // ###
 // global script file to include only in test mode.
-// it always has to include './components' and './global.ts'
+// it always has to include './global.ts'
 // ###
 
-import './components';
 import './global';
 
--- packages/stencil/components/select/src/global.test.ts
+++ packages/stencil/components/select/src/global.test.ts
@@ -1,9 +1,8 @@
 // ###
 // global script file to include only in test mode.
-// it always has to include './components' and './global.ts'
+// it always has to include './global.ts'
 // ###
 
-import './components';
 import './global';
 
 import '@ovhcloud/ods-stencil/components/icon';

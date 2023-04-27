f490fdd50bdc444ef00bcf5429f9ea119ba2979b
aurore.stagnol <a.stagnol@gmail.com>
2023-04-27 11:58:05 +0200

    feat(generator): rm components import for global test
    Signed-off-by: aurore.stagnol <a.stagnol@gmail.com>

--- packages/tools/generator/component/template/stencil/src/global.test.ts
+++ packages/tools/generator/component/template/stencil/src/global.test.ts
@@ -1,8 +1,7 @@
 // ###
 // global script file to include only in test mode.
-// it always has to include './components' and './global.ts'
+// it always has to include './global.ts'
 // then, you can import sub-component(s) needed in tests like this: import '@ovhcloud/ods-stencil/components/button';
 // ###
 
-import './components';
 import './global';

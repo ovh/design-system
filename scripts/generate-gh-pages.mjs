7fe7c23b5de3813feeb51195b75699c47ee16c85
schevreu <stevering@bkm.me>
2023-04-28 17:45:44 +0200

    refactor(gh-pages): update generate-gh-pages.mjs

--- scripts/generate-gh-pages.mjs
+++ scripts/generate-gh-pages.mjs
@@ -52,14 +52,20 @@ const outDirName = 'docs';
     const command = `curl -sS "${tarball}" | tar -xzf - -C ${dir} --strip 1`;
     await $([command]);
 
-    // todo: remove these 3 lines when packages will be ok
+    // todo: remove these 3 lines when packages will be ok (simulate we have all dist)
     await $`mkdir -p ${dir}/dist`;
     await $`echo "doc version ${version}" > ${dir}/dist/index.html`;
     await $`cat ${dir}/dist/index.html`;
+    //
 
     await $`mv ${dir}/dist dist/v${version}`;
   }
   try {
+    // todo: remove this 2 lines when packages will be ok (simulate we have the current storybook dist)
+    await $`mkdir -p packages/tools/storybook/dist`;
+    await $`echo "doc version ${currentVersion}" > packages/tools/storybook/dist/index.html`;
+    //
+
     // add the current build (released just done)
     await $`cp -r packages/tools/storybook/dist dist/v${currentVersion}`;
     await $`ln -s v${currentVersion} dist/latest`;

8ef3709c87b44ed2d0f83e9a408d49853c3800f5
Damien Pellier <dpellier@gmail.com>
2023-05-22 15:09:00 +0200

    refactor(spinner): remove ununsed style

---
+++ packages/stencil/components/tooltip/vue/src/components/stencil-generated/index.ts
@@ -0,0 +1,18 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated vue proxies */
+import { defineContainer } from './vue-component-lib/utils';
+
+import type { JSX } from '@ovhcloud/ods-stencil/components/tooltip/custom-elements';
+
+import { defineCustomElement as defineOsdsTooltip } from '@ovhcloud/ods-stencil/components/tooltip/custom-elements/osds-tooltip.js';
+import { defineCustomElement as defineOsdsTooltipContent } from '@ovhcloud/ods-stencil/components/tooltip/custom-elements/osds-tooltip-content.js';
+
+
+export const OsdsTooltip = /*@__PURE__*/ defineContainer<JSX.OsdsTooltip>('osds-tooltip', defineOsdsTooltip, [
+  'variant'
+]);
+
+
+export const OsdsTooltipContent = /*@__PURE__*/ defineContainer<JSX.OsdsTooltipContent>('osds-tooltip-content', defineOsdsTooltipContent);
+

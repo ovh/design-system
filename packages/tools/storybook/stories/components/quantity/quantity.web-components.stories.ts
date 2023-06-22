96c5cedc3fa0749b44c9f5dcfa676cae0aa86775
astagnol <53512103+astagnol@users.noreply.github.com>
2023-06-22 14:46:04 +0200

    fix/quantity four digits (#78)
    * fix(quantity): icon alignment, btn width & input 4 digits
    * fix(quantity): add fixed width for osds-input
    ---------
    
    Signed-off-by: aurore.stagnol <a.stagnol@gmail.com>

--- packages/tools/storybook/stories/components/quantity/quantity.web-components.stories.ts
+++ packages/tools/storybook/stories/components/quantity/quantity.web-components.stories.ts
@@ -41,11 +41,11 @@ export default {
 const TemplateDefault = (args: any) => html`
   <osds-quantity ...=${getTagAttributes(args)}>
     <osds-button slot="minus" size="sm" color="primary">
-      <osds-icon size="xxs" contrasted name="minus"></osds-icon>
+      <osds-icon size="sm" contrasted name="minus"></osds-icon>
     </osds-button>
-    <osds-input style="max-width: 40px" type="number" color="primary" min="1" max="10" step="1" value="1"></osds-input>
+    <osds-input type="number" color="primary" min="1" max="10" step="1" value="1"></osds-input>
     <osds-button slot="plus" size="sm" color="primary">
-      <osds-icon size="xxs" contrasted name="plus"></osds-icon>
+      <osds-icon size="sm" contrasted name="plus"></osds-icon>
     </osds-button>
   </osds-quantity>
 `;
@@ -58,7 +58,7 @@ Default.args = {
 const TemplateVariant = (args: any) => html`
   <osds-quantity ...=${getTagAttributes(args)}>
     <button slot="minus">-</button>
-    <input style="max-width: 40px" type="number" min="1" max="10" step="1" value="1"/>
+    <input style="width: 45px" type="number" min="1" max="10" step="1" value="1"/>
     <button slot="plus">+</button>
   </osds-quantity>
 `;

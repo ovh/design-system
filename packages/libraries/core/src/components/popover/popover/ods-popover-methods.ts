507155c4961c69adc5f98fd7c7c85518c483893f
skhamvon <126242816+skhamvon@users.noreply.github.com>
2023-06-27 14:30:26 +0200

    feat(popover): add new component popover (#14)
    * feat(popover): add new component popover
    ---------
    
    Signed-off-by: aurore.stagnol <a.stagnol@gmail.com>

---
+++ packages/libraries/core/src/components/popover/popover/ods-popover-methods.ts
@@ -0,0 +1,8 @@
+import { OdsComponentMethods } from '../../ods-component-methods';
+
+export interface OdsPopoverMethods extends OdsComponentMethods {
+  /**
+   * Close the surface
+   */
+  closeSurface() : void;
+}

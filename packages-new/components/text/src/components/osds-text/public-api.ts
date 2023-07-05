51b510af3771484b804a1aff104a8e4d3c5d9cb8
Damien Pellier <dpellier@gmail.com>
2023-07-05 14:54:41 +0200

    feat(arch): update starters to use new text component

--- packages-new/components/text/src/components/osds-text/public-api.ts
+++ packages-new/components/text/src/components/osds-text/public-api.ts
@@ -1,3 +1,3 @@
 export { ODS_TEXT_LEVEL, ODS_TEXT_LEVELS } from './constants/text-level';
 export { ODS_TEXT_SIZE, ODS_TEXT_SIZES } from './constants/text-size';
-export * from './osds-text';
+export { OsdsText } from './osds-text';

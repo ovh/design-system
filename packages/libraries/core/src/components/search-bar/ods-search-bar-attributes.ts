321a886941ddcdb7c8929b4005072cbfc3479e62
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2023-07-11 09:29:59 +0200

    feat(search-bar): implem V1

--- packages/libraries/core/src/components/search-bar/ods-search-bar-attributes.ts
+++ packages/libraries/core/src/components/search-bar/ods-search-bar-attributes.ts
@@ -1,7 +1,16 @@
 import { OdsComponentAttributes } from '../ods-component-attributes';
 
 export interface OdsSearchBarAttributes extends OdsComponentAttributes {
-  /**
-   * SearchBar attribute description
-   */
+  /** Indicates if the search-bar is contrasted or not: see component principles */
+  contrasted?: boolean;
+  /** Indicates if the search-bar is disabled or not: see component principles */
+  disabled?: boolean;
+  /** Indicates if the search-bar is in loading state or not */
+  loading?: boolean;
+  /** List of the options on the select */
+  options?: { label: string, value: string }[];
+  /** Placeholder text for the search-bar */
+  placeholder?: string;
+  /** Current value of the search-bar */
+  value: string;
 }
--- packages/libraries/core/src/components/search-bar/ods-search-bar-default-attributes.ts
+++ packages/libraries/core/src/components/search-bar/ods-search-bar-default-attributes.ts
@@ -1,7 +1,12 @@
 import { OdsSearchBarAttributes } from './ods-search-bar-attributes';
 
-export const odsSearchBarDefaultAttributesDoc  = {
-    // default attributes
+export const odsSearchBarDefaultAttributesDoc = {
+    contrasted: false,
+    disabled: false,
+    loading: false,
+    options: [] as { label: string; value: string; }[],
+    placeholder: undefined,
+    value: ''
 } as const;
 
 export const odsSearchBarDefaultAttributes = odsSearchBarDefaultAttributesDoc as OdsSearchBarAttributes;

ceae04c5f95dbbe489076ae73be7f9bead80ab9e
Leo Largillet <leo.largillet@gmail.com>
2023-07-17 17:42:34 +0200

    fix(input): set cursor to not-allowed when disabled

---
+++ packages/stencil/components/form-field/react/src/components/stencil-generated/index.ts
@@ -0,0 +1,10 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+
+import type { JSX } from '@ovhcloud/ods-stencil/components/form-field/custom-elements';
+
+import { defineCustomElement as defineOsdsFormField } from '@ovhcloud/ods-stencil/components/form-field/custom-elements/osds-form-field.js';
+
+export const OsdsFormField = /*@__PURE__*/createReactComponent<JSX.OsdsFormField, HTMLOsdsFormFieldElement>('osds-form-field', undefined, undefined, defineOsdsFormField);
---
+++ packages/stencil/components/form-field/vue/src/components/stencil-generated/index.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated vue proxies */
+import { defineContainer } from './vue-component-lib/utils';
+
+import type { JSX } from '@ovhcloud/ods-stencil/components/form-field/custom-elements';
+
+import { defineCustomElement as defineOsdsFormField } from '@ovhcloud/ods-stencil/components/form-field/custom-elements/osds-form-field.js';
+
+
+export const OsdsFormField = /*@__PURE__*/ defineContainer<JSX.OsdsFormField>('osds-form-field', defineOsdsFormField, [
+  'error',
+  'flex'
+]);
+

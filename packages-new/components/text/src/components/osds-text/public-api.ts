d687fec9ba99ca18a63df1ca5a98e6b78c3a864c
Damien Pellier <dpellier@gmail.com>
2023-07-25 16:33:24 +0200

    feat(text): export attribute interface

--- packages-new/components/text/src/components/osds-text/public-api.ts
+++ packages-new/components/text/src/components/osds-text/public-api.ts
@@ -1,3 +1,4 @@
+export type { OdsTextAttribute } from './interfaces/attributes'
 export { ODS_TEXT_LEVEL, ODS_TEXT_LEVELS } from './constants/text-level';
 export { ODS_TEXT_SIZE, ODS_TEXT_SIZES } from './constants/text-size';
 export { OsdsText } from './osds-text';

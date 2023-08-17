a0b84ce0b0fa8f7ecca94238202f6570e9402f81
skhamvon <sullivan.khamvongsa@ovhcloud.com>
2023-08-17 13:53:20 +0200

    fix(menu): correct import type

--- packages-new/components/menu/src/index.ts
+++ packages-new/components/menu/src/index.ts
@@ -1,1 +1,3 @@
-export { OsdsMenu } from './components/osds-menu/public-api';
+export * from './components/osds-menu/public-api';
+export * from './components/osds-menu-item/public-api';
+export * from './components/osds-menu-group/public-api';

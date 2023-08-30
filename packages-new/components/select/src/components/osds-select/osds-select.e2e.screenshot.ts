910816f1d9093a6a5deca2ea34a82a95cff75ccd
skhamvon <sullivan.khamvongsa@ovhcloud.com>
2023-08-30 15:51:28 +0200

    fix(select): surface width no longer the same than trigger

--- packages-new/components/select/src/components/osds-select/osds-select.e2e.screenshot.ts
+++ packages-new/components/select/src/components/osds-select/osds-select.e2e.screenshot.ts
@@ -1,22 +1,23 @@
 import type { E2EPage } from '@stencil/core/testing';
-import { newE2EPage } from '@stencil/core/testing';
+import { E2EElement, newE2EPage } from '@stencil/core/testing';
+import { DEFAULT_ATTRIBUTE } from './constants/default-attributes';
+import { OdsSelectAttribute } from './interfaces/attributes';
+import { odsComponentAttributes2StringAttributes, odsStringAttributes2Str } from '@ovhcloud/ods-common-testing';
+
 
 describe('e2e:osds-select', () => {
   let page: E2EPage;
+  let el: E2EElement;
 
-  async function setup({ onPage }: { onPage?: ({ page }: { page: E2EPage }) => void } = {}) {
+  async function setup({ attributes = {}, html = `` }: { attributes?: Partial<OdsSelectAttribute>, html?: string } = {}) {
+    const stringAttributes = odsComponentAttributes2StringAttributes<OdsSelectAttribute>(attributes, DEFAULT_ATTRIBUTE);
+
     page = await newE2EPage();
-    onPage && onPage({ page });
 
     await page.setContent(`
-      <osds-select>
-        <span slot="placeholder">Select country</span>
-        <osds-select-group>My group title</osds-select-group>
-        <osds-select-option>Value 1</osds-select-option>
-        <osds-select-option>Value 2</osds-select-option>
-        <osds-select-option>Value 3</osds-select-option>
-      </osds-select>
+      <osds-select ${odsStringAttributes2Str(stringAttributes)}>${html}</osds-select>
     `);
+    el = await page.find('osds-select');
     await page.evaluate(() => document.body.style.setProperty('margin', '0px'));
   }
 
@@ -27,13 +28,65 @@ describe('e2e:osds-select', () => {
         // noop
       },
     },
+    {
+      actionDescription: 'surface visible',
+      action: () => el.click(),
+    },
+    {
+      actionDescription: 'disabled',
+      action: () => el.setProperty('disabled', true),
+    },
+    {
+      actionDescription: 'hovered',
+      action: () => el.hover(),
+    },
   ];
 
-  describe('screenshots', () => {
+  describe('screenshots with short text', () => {
     // Todo : add active behaviour on top of hover and focus
     screenshotActions.forEach(({ actionDescription, action }) => {
       it(actionDescription, async () => {
-        await setup({});
+
+        await setup({
+          html: `
+          <span slot="placeholder">Select country</span>
+          <osds-select-group>My group title</osds-select-group>
+          <osds-select-option>Value 1</osds-select-option>
+          <osds-select-option>Value 2</osds-select-option>
+          <osds-select-option>Value 3</osds-select-option>
+          `
+        });
+        action();
+        await page.waitForChanges();
+
+        await page.evaluate(() => {
+          const element = document.querySelector('osds-select') as HTMLElement;
+          return { width: element.clientWidth, height: element.clientHeight };
+        });
+        await page.setViewport({ width: 600, height:600 });
+        const results = await page.compareScreenshot('select', { fullPage: false, omitBackground: true });
+        expect(results).toMatchScreenshot({ allowableMismatchedRatio: 0 });
+      });
+    });
+  });
+
+  describe('screenshots with long text', () => {
+    // Todo : add active behaviour on top of hover and focus
+    screenshotActions.forEach(({ actionDescription, action }) => {
+      it(actionDescription, async () => {
+
+        await setup({
+          attributes: {
+            inline: true
+          },
+          html: `
+          <span slot="placeholder">Select country</span>
+          <osds-select-group>My group title</osds-select-group>
+          <osds-select-option>Value 1 with looooooooooooooooooooooooooooooooooooooooooong text</osds-select-option>
+          <osds-select-option>Value 2</osds-select-option>
+          <osds-select-option>Value 3</osds-select-option>
+          `
+        });
         action();
         await page.waitForChanges();
 

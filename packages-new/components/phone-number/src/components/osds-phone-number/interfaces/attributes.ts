e018880a3674d6f9d7f705007341da5a30a08205
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2023-08-30 17:00:24 +0200

    feat(phone-number): step 1 init component with simple input

---
+++ packages-new/components/phone-number/src/components/osds-phone-number/interfaces/attributes.ts
@@ -0,0 +1,13 @@
+interface OdsPhoneNumberAttribute {
+  /** Ability to clear the phone number value */
+  clearable?: boolean;
+  disabled?: boolean;
+  /** Indicates if the phone number shows error or not */
+  error?: boolean;
+  /** Current value of the phone number */
+  value: string;
+}
+
+export {
+  OdsPhoneNumberAttribute,
+};
---
+++ packages-new/components/phone-number/src/global.test.ts
@@ -0,0 +1,1 @@
+import '@ovhcloud/ods-component-input';

3ead774d729b9c3f057e0dca89c5297acaf0ad61
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2023-09-13 10:27:33 +0200

    fix(phone-number): type countries

--- packages/components/phone-number/src/components/osds-phone-number/interfaces/attributes.ts
+++ packages/components/phone-number/src/components/osds-phone-number/interfaces/attributes.ts
@@ -5,7 +5,7 @@ interface OdsPhoneNumberAttribute {
   /** Ability to clear the phone number value */
   clearable?: boolean;
   /** A specific subset of countries to display in the select instead of the whole list  */
-  countries?: ODS_COUNTRY_ISO_CODE[] | ODS_PHONE_NUMBER_COUNTRY_PRESET.All | string;
+  countries?: ODS_COUNTRY_ISO_CODE[] | ODS_PHONE_NUMBER_COUNTRY_PRESET | string;
   disabled?: boolean;
   /** Indicates if the phone number shows error or not */
   error?: boolean;

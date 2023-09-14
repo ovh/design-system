d40f024b3365462ed649381ae93077d6f38988ed
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2023-09-14 09:12:50 +0200

    fix(phone-number): rebase

--- packages-new/components/phone-number/src/global.prod.ts
+++
@@ -1,6 +0,0 @@
-// ###
-// global script file to include only in prod mode for build prod.
-// it always has to include './global.ts'
-// ###
-import './global';
-import '@ovhcloud/ods-component-input';

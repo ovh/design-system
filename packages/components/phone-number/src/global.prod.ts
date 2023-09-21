2801cace72cd37915b05bd876bc8bb66569a0a11
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2023-09-21 11:51:57 +0200

    fix(phone-number): import prod

--- packages/components/phone-number/src/global.prod.ts
+++ packages/components/phone-number/src/global.prod.ts
@@ -3,7 +3,3 @@
 // it always has to include './global.ts'
 // ###
 import './global';
-import '@ovhcloud/ods-component-input';
-import '@ovhcloud/ods-component-select';
-import '@ovhcloud/ods-component-flag';
-import '@ovhcloud/ods-component-text';

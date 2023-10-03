ed6cdb7bc817de2847b03c0db4e04deb5a79b5bf
Leotheluck <57661577+Leotheluck@users.noreply.github.com>
2023-10-03 09:39:32 +0200

    feat(datepicker): step 3, excluding dates (#219)
    * feat(datepicker): step 3, excluding dates
    
    * feat(datepicker): step 3, excluding dates

--- packages/components/datepicker/src/components/osds-datepicker/core/controller.ts
+++ packages/components/datepicker/src/components/osds-datepicker/core/controller.ts
@@ -18,15 +18,16 @@ class OdsDatepickerController {
     }
   }
 
-  onChange(newValue: Date | undefined | null, oldValue?: Date | null) {
+  onChange(newValue: Date | undefined | null, oldValue?: Date | null): void {
     if(!this.component.disabled) {
-      if (newValue === undefined || newValue === null || isNaN(newValue.getTime())) {
+      if (!this.validateValue(newValue)) {
         this.component.value = null;
         this.component.datepickerInstance?.setDate({ clear: true });
+        this.component.emitDatepickerValueChange(null, oldValue ? oldValue : null);
       } else {
         this.component.value = newValue;
         this.component.datepickerInstance?.setDate(newValue);
-        this.component.emitValueChange(newValue, oldValue);
+        this.component.emitDatepickerValueChange(newValue, oldValue ? oldValue : null);
       }
     }
   }
@@ -34,6 +35,36 @@ class OdsDatepickerController {
   onBlur() {
     this.component.hasFocus = false;
     this.component.emitBlur();
+  }
+
+  private getMidnightDate(date: Date): Date {
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()))
+  }
+
+  private validateValue(value?: Date | null | undefined): boolean {
+    if (!value || value === null || isNaN(value.getTime())) {
+      return false;
+    }
+
+    const midnightValue = this.getMidnightDate(value).getTime();
+
+    if (this.component.datesDisabled && this.component.datesDisabled.some(d => this.getMidnightDate(d).getTime() === midnightValue)) {
+      return false;
+    }
+
+    if (this.component.daysOfWeekDisabled && this.component.daysOfWeekDisabled.includes(value.getDay())) {
+      return false;
+    }
+
+    if (this.component.maxDate && midnightValue > this.getMidnightDate(this.component.maxDate).getTime()) {
+      return false;
+    }
+
+    if (this.component.minDate && midnightValue < this.getMidnightDate(this.component.minDate).getTime()) {
+      return false;
+    }
+
+    return true;
   }
 }
 

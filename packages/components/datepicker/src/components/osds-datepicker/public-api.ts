fd4c3fb5b25ac79f6cf74297694242db29a15df2
Leo Largillet <leo.largillet@gmail.com>
2023-10-05 11:46:22 +0200

    fix(datepicker): fix exports

--- packages/components/datepicker/src/components/osds-datepicker/public-api.ts
+++ packages/components/datepicker/src/components/osds-datepicker/public-api.ts
@@ -1,2 +1,4 @@
 export type { OdsDatepickerAttribute } from './interfaces/attributes';
+export type { OdsDatepickerEvent, OdsDatepickerValueChangeEvent, OdsDatepickerValueChangeEventDetail } from './interfaces/events';
+export { ODS_DATEPICKER_DAY, ODS_DATEPICKER_DAYS } from './constants/datepicker-day';
 export { OsdsDatepicker } from './osds-datepicker';

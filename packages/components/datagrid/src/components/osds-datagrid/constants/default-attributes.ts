4dc18e11108d0c4c818593e8b4491b9480e81642
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2023-10-16 16:39:24 +0200

    feat(datagrid): add hidable columns setting

--- packages/components/datagrid/src/components/osds-datagrid/constants/default-attributes.ts
+++ packages/components/datagrid/src/components/osds-datagrid/constants/default-attributes.ts
@@ -3,6 +3,7 @@ import type { OdsDatagridAttribute } from '../interfaces/attributes';
 const DEFAULT_ATTRIBUTE: OdsDatagridAttribute = Object.freeze({
   columns: [],
   height: 0,
+  hideableColumns: [],
   isSelectable: false,
   noResultLabel: '',
   rowHeight: 52,

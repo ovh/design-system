2ee4aab1b34dde6f1d9ed04a8b3c95497145442a
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2023-10-24 11:29:56 +0200

    test(icon): add test & remove skip test

--- packages/components/icon/src/components/osds-icon/osds-icon.e2e.ts
+++ packages/components/icon/src/components/osds-icon/osds-icon.e2e.ts
@@ -1,19 +1,14 @@
 import type { E2EElement, E2EPage } from '@stencil/core/testing';
 import type { OdsIconAttribute } from './interfaces/attributes';
-import { OdsLogger } from '@ovhcloud/ods-common-core';
 import { odsComponentAttributes2StringAttributes, odsStringAttributes2Str } from '@ovhcloud/ods-common-testing';
 import { ODS_THEME_COLOR_INTENT } from '@ovhcloud/ods-common-theming';
 import { newE2EPage } from '@stencil/core/testing';
 import { DEFAULT_ATTRIBUTE } from './constants/default-attributes';
 
-const logger = new OdsLogger('osds-icon-e2e');
-
 describe('e2e:osds-icon', () => {
   const baseAttribute = { ariaName: '' };
   let page: E2EPage;
   let el: E2EElement;
-  let slotContent: E2EElement;
-  let iconElement: E2EElement;
 
   async function setup({ attributes = {} }: { attributes: Partial<OdsIconAttribute> }) {
     const stringAttributes = odsComponentAttributes2StringAttributes<OdsIconAttribute>({ ...baseAttribute, ...attributes }, DEFAULT_ATTRIBUTE);
@@ -23,11 +18,6 @@ describe('e2e:osds-icon', () => {
     await page.evaluate(() => document.body.style.setProperty('margin', '0px'));
 
     el = await page.find('osds-icon');
-
-    iconElement = await page.find('osds-icon >>> icon');
-    slotContent = iconElement && await iconElement.find('slot[name=content]');
-    logger.log(slotContent);
-    logger.log(iconElement);
   }
 
   it('should render', async () => {
@@ -35,8 +25,13 @@ describe('e2e:osds-icon', () => {
     expect(el).not.toBeNull();
   });
 
-  xit('should have the text icon', async () => {
-    await setup({ attributes: { color: ODS_THEME_COLOR_INTENT.primary, contrasted: false } });
-    // todo
+  it('should have ods-icon--contrasted class', async () => {
+    await setup({ attributes: { color: ODS_THEME_COLOR_INTENT.primary, contrasted: true } });
+    expect(el.classList.contains('ods-icon--contrasted')).toBe(true);
+  });
+
+  it('should have ods-icon--hoverable class', async () => {
+    await setup({ attributes: { color: ODS_THEME_COLOR_INTENT.primary, hoverable: true } });
+    expect(el.classList.contains('ods-icon--hoverable')).toBe(true);
   });
 });

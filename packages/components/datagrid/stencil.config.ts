d674c4a10aa5f8262d06572f2228b7e1fa2c8f4e
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2023-10-26 14:28:45 +0200

    chore(datagrid): update stencil 4

--- packages/components/datagrid/stencil.config.ts
+++ packages/components/datagrid/stencil.config.ts
@@ -1,28 +1,10 @@
-import { Config } from '@stencil/core';
 import { getStencilConfig } from '@ovhcloud/ods-common-stencil';
-import * as jestConfig from './jest.config';
+import jestConfig from './jest.config';
 
-const args = process.argv.slice(2);
-
-export const config: Config = getStencilConfig({
+export const config = getStencilConfig({
+  args: process.argv.slice(2),
+  componentCorePackage: '@ovhcloud/ods-component-datagrid',
+  excludeComponents: ['osds-icon', 'osds-text'],
+  jestConfig,
   namespace: 'osds-datagrid',
-  args,
-  jestConfig: jestConfig.default,
-  reactOutput: {
-    componentCorePackage: '@ovhcloud/ods-component-datagrid',
-    excludeComponents: ['osds-icon', 'osds-text']
-  },
-  vueOutput: {
-    componentCorePackage: '@ovhcloud/ods-component-datagrid',
-    excludeComponents: ['osds-icon', 'osds-text']
-  },
-  dev: {
-    globalScript: 'src/global.dev.ts',
-  },
-  prod: {
-    globalScript: 'src/global.prod.ts'
-  },
-  test: {
-    globalScript: 'src/global.test.ts'
-  }
 });

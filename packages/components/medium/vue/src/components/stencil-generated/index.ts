0707cea66f83d8bca702dc43cbdee2e201b9eb91
Manon Carbonnel <manon.carbonnel@gmail.com>
2023-11-09 10:01:59 +0100

    feat(medium): adding react and vue examples

---
+++ packages/components/medium/vue/src/components/stencil-generated/index.ts
@@ -0,0 +1,17 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated vue proxies */
+import { defineContainer } from './vue-component-lib/utils';
+
+import type { JSX } from '@ovhcloud/ods-component-medium/custom-elements';
+
+import { defineCustomElement as defineOsdsMedium } from '@ovhcloud/ods-component-medium/custom-elements/osds-medium.js';
+
+
+export const OsdsMedium = /*@__PURE__*/ defineContainer<JSX.OsdsMedium>('osds-medium', defineOsdsMedium, [
+  'alt',
+  'height',
+  'src',
+  'width'
+]);
+

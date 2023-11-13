942d58faa1dcc82d678d95150089787e7be37337
Damien Pellier <dpellier@gmail.com>
2023-11-13 14:11:22 +0100

    feat(scripts): add component generator

---
+++ scripts/component-generator/plopfile.js
@@ -0,0 +1,83 @@
+const  { spawnSync } = require('child_process');
+const path = require('path');
+const { version: libVersion } = require (path.resolve(process.cwd(), 'lerna.json'));
+
+module.exports = function (plop) {
+  const componentsBaseDirName = 'components';
+  const destinationPath = `${process.cwd()}/packages`;
+  const packagePrefix = '@ovhcloud/ods';
+
+  plop.setActionType('yarn-install', function () {
+    spawnSync('yarn', []);
+  });
+
+  plop.setActionType('lerna-add-dependency', function (answers, config, plop) {
+    const componentsDirName = plop.getHelper('suffix-join')(answers.prefix, componentsBaseDirName);
+    const libPrefix = plop.getHelper('suffix-join')(answers.prefix, 'component');
+    const dependency = `${packagePrefix}-${libPrefix}-${answers.name}@${libVersion}`;
+
+    spawnSync('lerna', ['--scope', `${packagePrefix}-${componentsDirName}`, 'exec', '--', 'yarn', 'add', dependency]);
+  });
+
+  plop.setHelper('prefix-join', function (prefix, text) {
+    return prefix ? `${prefix}-${text}` : text;
+  });
+
+  plop.setHelper('suffix-join', function (suffix, text) {
+    return suffix ? `${text}-${suffix}` : text;
+  });
+
+  plop.setPartial('component-dir', `{{ suffix-join prefix "${componentsBaseDirName}" }}`);
+  plop.setPartial('component-name', '{{ prefix-join prefix name }}');
+  plop.setPartial('componentName', '{{ pascalCase (prefix-join prefix name) }}');
+
+  plop.setGenerator('component', {
+    description: 'New component',
+    prompts: [{
+      type: 'list',
+      name: 'prefix',
+      message: 'Which kind of component do you want to create?',
+      choices: [
+        { name: 'ODS component (<osds-___>)', value: '' },
+        { name: 'OVH component (<osds-ovh-___>)', value: 'ovh' },
+      ],
+    }, {
+      type: 'list',
+      name: 'level',
+      message: 'Which atomic stage your component is?',
+      choices: ['quark', 'atom', 'molecule', 'meta', 'organism', 'layout', 'meta'],
+    }, {
+      type: 'input',
+      name: 'name',
+      message: 'Type the component name using kebab-case, without any "ods-" prefix (ex: text, search-bar, ...):',
+      validate: function(value) {
+        if (/.+/.test(value)) {
+          return true;
+        }
+        return 'Component name is required';
+      },
+    }],
+    actions: [{
+      type: 'addMany',
+      base: 'templates/component',
+      templateFiles: 'templates/component/**/*',
+      stripExtensions: ['hbs'],
+      globOptions: { dot: true },
+      destination: `${destinationPath}/{{ suffix-join prefix "${componentsBaseDirName}" }}/{{name}}`,
+      data: {
+        'component-version': libVersion,
+      },
+    }, {
+      type: 'addMany',
+      base: 'templates/storybook',
+      templateFiles: 'templates/storybook/**/*',
+      stripExtensions: ['hbs'],
+      globOptions: { dot: true },
+      destination: `${destinationPath}/storybook/stories/{{ suffix-join prefix "${componentsBaseDirName}" }}/{{name}}`,
+    }, {
+      type: 'yarn-install',
+    }, {
+      type: 'lerna-add-dependency',
+    }],
+  });
+};

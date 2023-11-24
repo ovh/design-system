dd3316a478b4b86328567efbace105ff38e5b5a3
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2023-11-24 11:29:19 +0100

    Feat/dg bottom scroll (#296)
    * feat(datagrid): add event ods bottomscroll
    
    * fix(datagrid): export in ods-components
    
    * fix(datagrid): lint

--- packages/components/datagrid/src/components/osds-datagrid/interfaces/events.ts
+++ packages/components/datagrid/src/components/osds-datagrid/interfaces/events.ts
@@ -4,6 +4,7 @@ import type { EventEmitter } from '@stencil/core';
 interface OdsDatagridEvent {
   odsSortChange: EventEmitter<{ field: string, dir: 'asc' | 'desc' | 'none' }>;
   odsRowSelectionChange: EventEmitter<{ rows: OdsDatagridRow[] }>;
+  odsBottomScroll: EventEmitter<void>;
 }
 
 export type {

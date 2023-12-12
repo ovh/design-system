5294c1d343a52fb04cd40559f688225a4c2e1745
skhamvon <sullivan.khamvongsa@ovhcloud.com>
2023-12-12 10:22:20 +0100

    refactor(input): move events to common core

---
+++ packages/common/core/src/form/interfaces/events.ts
@@ -0,0 +1,23 @@
+import type { OdsValidityState } from '@ovhcloud/ods-common-core';
+import type { EventEmitter } from '@stencil/core';
+
+interface OdsInputValueChangeEventDetail {
+  validity: OdsValidityState;
+  value: string | undefined | null;
+  oldValue?: string | undefined | null;
+}
+
+type OdsInputValueChangeEvent = CustomEvent<OdsInputValueChangeEventDetail>;
+
+interface OdsInputEvent {
+  odsInputBlur: EventEmitter<void>;
+  odsInputFocus: EventEmitter<void>;
+  /** the input value changed */
+  odsValueChange: EventEmitter<OdsInputValueChangeEventDetail>;
+}
+
+export {
+  OdsInputEvent,
+  OdsInputValueChangeEvent,
+  OdsInputValueChangeEventDetail,
+};

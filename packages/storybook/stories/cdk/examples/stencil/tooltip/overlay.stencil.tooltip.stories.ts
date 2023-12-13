1c1a4ab1903adaee4bad4641f62ec72eaf8f23df
Damien Pellier <dpellier@gmail.com>
2023-12-13 09:52:34 +0100

    fix(ci): ensure components.d.ts file is present

--- packages/storybook/stories/cdk/examples/stencil/tooltip/overlay.stencil.tooltip.stories.ts
+++ packages/storybook/stories/cdk/examples/stencil/tooltip/overlay.stencil.tooltip.stories.ts
@@ -1,4 +1,4 @@
-import { defineCustomElements } from '@ovhcloud/ods-cdk/dev/loader';
+import { defineCustomElements } from '@ovhcloud/ods-cdk/dev/dist/loader';
 import { OcdkSurfaceTooltipPosition, OcdkSurfaceTooltipPositionList } from '@ovhcloud/ods-cdk-dev';
 import { html } from 'lit-html';
 import { styleMap } from 'lit-html/directives/style-map';

0b092293e972f7c923a2d676108f162fe7f540ee
skhamvon <sullivan.khamvongsa@ovhcloud.com>
2023-12-15 16:03:20 +0100

    fix(tabs): add accessibility tests

--- packages/components/src/tabs/src/components/osds-tabs/osds-tabs.tsx
+++ packages/components/src/tabs/src/components/osds-tabs/osds-tabs.tsx
@@ -1,26 +1,23 @@
+import type { ODS_TABS_SIZE } from './constants/tabs-size';
+import type { OsdsTabBarItem } from '../osds-tab-bar-item/osds-tab-bar-item';
 import type { OdsTabsAttribute } from './interfaces/attributes';
 import type { OdsTabsChangeEventDetail, OdsTabsEvent } from './interfaces/events';
 import type { OdsTabItemSelectEventDetail } from '../osds-tab-bar-item/interfaces/events';
 import type { EventEmitter } from '@stencil/core';
 import type { HTMLStencilElement } from '@stencil/core/internal';
-
 import { OdsLogger } from '@ovhcloud/ods-common-core';
 import { Component, Element, Event, Host, Listen, Prop, Watch, h } from '@stencil/core';
-
-
 import { DEFAULT_ATTRIBUTE } from './constants/default-attributes';
-import { ODS_TABS_SIZE } from './constants/tabs-size';
 import { OdsTabsController } from './core/controller';
-
-
+import { OsdsTabsPanel } from '../osds-tab-panel/osds-tab-panel';
 
 /**
  * Main tabs component
  */
 @Component({
-  tag: 'osds-tabs',
-  styleUrl: 'osds-tabs.scss',
   shadow: true,
+  styleUrl: 'osds-tabs.scss',
+  tag: 'osds-tabs',
 })
 export class OsdsTabs implements OdsTabsAttribute, OdsTabsEvent {
   private logger = new OdsLogger('OsdsTabs');
@@ -35,7 +32,7 @@ export class OsdsTabs implements OdsTabsAttribute, OdsTabsEvent {
   @Prop({ reflect: true }) contrasted: boolean = DEFAULT_ATTRIBUTE.contrasted;
 
   /** @see OdsTabsAttributes.panel */
-  @Prop({ reflect: true, mutable: true }) panel: string = DEFAULT_ATTRIBUTE.panel;
+  @Prop({ mutable: true, reflect: true }) panel: string = DEFAULT_ATTRIBUTE.panel;
 
   /** @see OdsTabsAttributes.size */
   @Prop({ reflect: true }) size: ODS_TABS_SIZE = DEFAULT_ATTRIBUTE.size;
@@ -47,7 +44,7 @@ export class OsdsTabs implements OdsTabsAttribute, OdsTabsEvent {
    * @see OdsTabsBehavior.handleTabItemSelection
    */
   @Listen('odsTabItemSelectEvent')
-  handleTabItemSelection(event: CustomEvent<OdsTabItemSelectEventDetail>) {
+  handleTabItemSelection(event: CustomEvent<OdsTabItemSelectEventDetail>):void {
     this.logger.log('[handleTabItemSelection]', { event });
     this.controller.changeActivePanel(event.detail.panel);
   }
@@ -57,7 +54,7 @@ export class OsdsTabs implements OdsTabsAttribute, OdsTabsEvent {
    * @see OdsTabsBehavior.emitChanged
    * @see OdsTabsController.changeActivePanel
    */
-  emitChanged() {
+  emitChanged(): void {
     this.odsTabsChanged.emit({ panel: this.panel });
   }
 
@@ -66,8 +63,12 @@ export class OsdsTabs implements OdsTabsAttribute, OdsTabsEvent {
    * @see OdsTabsBehavior.getTabItems
    * @see OdsTabsController.changeActivePanel
    */
-  getTabItems() {
-    return this.controller.getTabItems('osds-tab-bar-item');
+  getTabItems(): (Array<OsdsTabBarItem & HTMLElement>) {
+    return Array.from(this.el.querySelectorAll<OsdsTabBarItem & HTMLElement>('osds-tab-bar-item'));
+  }
+
+  setTabItems(): void {
+    this.controller.tabItems = this.getTabItems();
   }
 
   /**
@@ -75,8 +76,12 @@ export class OsdsTabs implements OdsTabsAttribute, OdsTabsEvent {
    * @see OdsTabsBehavior.getTabPanels
    * @see OdsTabsController.changeActivePanel
    */
-  getTabPanels() {
-    return this.controller.getTabPanels('osds-tab-panel');
+  getTabPanels(): (Array<OsdsTabsPanel & HTMLElement>) {
+    return Array.from(this.el.querySelectorAll<OsdsTabsPanel & HTMLElement>('osds-tab-panel'));
+  }
+
+  setTabPanels(): void {
+    this.controller.tabPanels = this.getTabPanels();
   }
 
   handleArrowKey(event: KeyboardEvent): void {
@@ -86,35 +91,45 @@ export class OsdsTabs implements OdsTabsAttribute, OdsTabsEvent {
     }
   }
 
+  async handleSlotChange(): Promise<void> {
+    this.setTabItems();
+    this.setTabPanels();
+  }
+
   /**
    * when panel property changes, it will change the active panel
    * @see OdsTabsBehavior.onPanelPropChange
    */
   @Watch('panel')
-  onPanelPropChange(panel: string) {
+  onPanelPropChange(panel: string): void {
     this.controller.changeActivePanel(panel);
   }
 
   @Watch('contrasted')
-  onContrastedPropChange(contrasted: boolean) {
+  onContrastedPropChange(contrasted: boolean): void {
     this.controller.propagateContrastedToItems(contrasted);
   }
 
-  beforeInit() {
+  beforeInit(): void {
     this.controller.beforeInit();
   }
 
-  componentWillLoad() {
+  componentWillLoad(): void {
     this.beforeInit();
   }
 
-  render() {
+  componentDidLoad(): void {
+    this.controller.changeActivePanel(this.panel);
+    this.setTabItems();
+    this.setTabPanels();
+  }
+
+  render(): JSX.Element {
     return (
       <Host>
         <div class="tabs-nav-wrap"
-             onKeyDown={ this.handleArrowKey.bind(this) }
-        >
-          <slot name='top' />
+             onKeyDown={ this.handleArrowKey.bind(this) }>
+          <slot name='top' onSlotchange={(): Promise<void> => this.handleSlotChange() }/>
         </div>
         <slot />
       </Host>

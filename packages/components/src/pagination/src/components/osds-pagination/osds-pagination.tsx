68a5884ef89253903982c36086bf3b09cbf09663
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2023-12-21 16:16:50 +0100

    refactor(formfield): use attributes from common core

--- packages/components/src/pagination/src/components/osds-pagination/osds-pagination.tsx
+++ packages/components/src/pagination/src/components/osds-pagination/osds-pagination.tsx
@@ -68,7 +68,7 @@ export class OsdsPagination implements OdsPaginationAttribute, OdsPaginationEven
     const { value } = event.detail;
 
     if (value) {
-      this.itemPerPage = typeof value === 'number' ? value : parseInt(value, 10);
+      this.itemPerPage = typeof value === 'number' ? value : parseInt(value.toString(), 10);
     }
   }
 

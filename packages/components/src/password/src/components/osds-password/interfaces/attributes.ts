67aef75e434601637fd9566dbad4a1056cedea73
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-01-25 16:22:02 +0100

    fix(password): focus & storybook

--- packages/components/src/password/src/components/osds-password/interfaces/attributes.ts
+++ packages/components/src/password/src/components/osds-password/interfaces/attributes.ts
@@ -1,6 +1,6 @@
 import type { OdsCommonFieldAttribute } from '@ovhcloud/ods-common-core';
 
-interface OdsPasswordAttribute extends Omit<OdsCommonFieldAttribute, 'prefixValue' | 'step'> {
+interface OdsPasswordAttribute extends Omit<OdsCommonFieldAttribute, 'step'> {
   /** Indicates if the password is inline or not */
   inline?: boolean;
   /** Indicates if the input is masked or not */

fb2fd24b8317d84ff0a4f13b9bb159b2e391e32b
Damien Pellier <dpellier@gmail.com>
2024-03-05 15:08:43 +0100

    feat(spinner): remove inline attribute

--- packages/ods/src/components/spinner/tests/accessibility/ods-spinner.e2e.ts
+++ packages/ods/src/components/spinner/tests/accessibility/ods-spinner.e2e.ts
@@ -1,24 +1,23 @@
-// import type { E2EElement, E2EPage } from '@stencil/core/testing';
-// import { newE2EPage } from '@stencil/core/testing';
+import type { E2EElement, E2EPage } from '@stencil/core/testing';
+import { newE2EPage } from '@stencil/core/testing';
 
 describe('ods-spinner accessibility', () => {
-  // let el: E2EElement;
-  // let page: E2EPage;
-  //
-  // async function setup(content: string): Promise<void> {
-  //   page = await newE2EPage();
-  //
-  //   await page.setContent(content);
-  //   await page.evaluate(() => document.body.style.setProperty('margin', '0px'));
-  //
-  //   el = await page.find('ods-spinner');
-  // }
+  let el: E2EElement;
+  let page: E2EPage;
+
+  async function setup(content: string): Promise<void> {
+    page = await newE2EPage();
+
+    await page.setContent(content);
+    await page.evaluate(() => document.body.style.setProperty('margin', '0px'));
+
+    el = await page.find('ods-spinner');
+  }
 
   it('should render the web component with the correct role', async() => {
-    // await setup('<ods-spinner></ods-spinner>');
-    //
-    // expect(el.shadowRoot).not.toBeNull();
-    // expect(el.getAttribute('role')).toBe('progressbar');
-    expect(true).not.toBeNull();
+    await setup('<ods-spinner></ods-spinner>');
+
+    expect(el.shadowRoot).not.toBeNull();
+    expect(el.getAttribute('role')).toBe('progressbar');
   });
 });
--- packages/ods/src/components/spinner/tests/rendering/ods-spinner.e2e.ts
+++ packages/ods/src/components/spinner/tests/rendering/ods-spinner.e2e.ts
@@ -1,57 +1,56 @@
-// import type { E2EElement, E2EPage } from '@stencil/core/testing';
-// import { newE2EPage } from '@stencil/core/testing';
-// import { ODS_SPINNER_SIZE } from '../../src';
+import type { E2EElement, E2EPage } from '@stencil/core/testing';
+import { newE2EPage } from '@stencil/core/testing';
+import { ODS_SPINNER_SIZE } from '../../src';
 
 describe('ods-spinner rendering', () => {
-  // let container: E2EElement;
-  // let el: E2EElement;
-  // let page: E2EPage;
-  //
-  // async function setup(content: string): Promise<void> {
-  //   page = await newE2EPage();
-  //
-  //   await page.setContent(content);
-  //   await page.evaluate(() => document.body.style.setProperty('margin', '0px'));
-  //
-  //   el = await page.find('ods-spinner');
-  //   container = await page.find('ods-spinner >>> .ods-spinner__container');
-  // }
-  //
+  let container: E2EElement;
+  let el: E2EElement;
+  let page: E2EPage;
+
+  async function setup(content: string): Promise<void> {
+    page = await newE2EPage();
+
+    await page.setContent(content);
+    await page.evaluate(() => document.body.style.setProperty('margin', '0px'));
+
+    el = await page.find('ods-spinner');
+    container = await page.find('ods-spinner >>> .ods-spinner__container');
+  }
+
   it('should render the web component', async() => {
-  //   await setup('<ods-spinner></ods-spinner>');
-  //
-  //   expect(el.shadowRoot).not.toBeNull();
-  //   expect(container).not.toBeNull();
-    expect(true).not.toBeNull();
+    await setup('<ods-spinner></ods-spinner>');
+
+    expect(el.shadowRoot).not.toBeNull();
+    expect(container).not.toBeNull();
   });
-  //
-  // describe('sizes', () => {
-  //   it('should respect increase order (sm < md < lg)', async() => {
-  //     await setup(`
-  //       <ods-spinner size="${ODS_SPINNER_SIZE.sm}"></ods-spinner>
-  //       <ods-spinner size="${ODS_SPINNER_SIZE.md}"></ods-spinner>
-  //       <ods-spinner size="${ODS_SPINNER_SIZE.lg}"></ods-spinner>
-  //     `)
-  //
-  //     const smSpinner = await page.find(`ods-spinner[size=${ODS_SPINNER_SIZE.sm}]`);// >>> .ods-spinner__container`);
-  //     const mdSpinner = await page.find(`ods-spinner[size=${ODS_SPINNER_SIZE.md}]`);
-  //     const lgSpinner = await page.find(`ods-spinner[size=${ODS_SPINNER_SIZE.lg}]`);
-  //
-  //     const smSpinnerStyle = await smSpinner.getComputedStyle();
-  //     const smSpinnerHeight = parseInt(smSpinnerStyle.getPropertyValue('height'), 10);
-  //
-  //     const mdSpinnerStyle = await mdSpinner.getComputedStyle();
-  //     const mdSpinnerHeight = parseInt(mdSpinnerStyle.getPropertyValue('height'), 10);
-  //
-  //     const lgSpinnerStyle = await lgSpinner.getComputedStyle();
-  //     const lgSpinnerHeight = parseInt(lgSpinnerStyle.getPropertyValue('height'), 10);
-  //
-  //     expect(smSpinnerHeight).toBeLessThan(mdSpinnerHeight);
-  //     expect(smSpinnerHeight).toBeLessThan(lgSpinnerHeight);
-  //     expect(mdSpinnerHeight).toBeLessThan(lgSpinnerHeight);
-  //     expect(mdSpinnerHeight).toBeGreaterThan(smSpinnerHeight);
-  //     expect(lgSpinnerHeight).toBeGreaterThan(smSpinnerHeight);
-  //     expect(lgSpinnerHeight).toBeGreaterThan(mdSpinnerHeight);
-  //   });
-  // });
+
+  describe('sizes', () => {
+    it('should respect increase order (sm < md < lg)', async() => {
+      await setup(`
+        <ods-spinner size="${ODS_SPINNER_SIZE.sm}"></ods-spinner>
+        <ods-spinner size="${ODS_SPINNER_SIZE.md}"></ods-spinner>
+        <ods-spinner size="${ODS_SPINNER_SIZE.lg}"></ods-spinner>
+      `)
+
+      const smSpinner = await page.find(`ods-spinner[size=${ODS_SPINNER_SIZE.sm}]`);
+      const mdSpinner = await page.find(`ods-spinner[size=${ODS_SPINNER_SIZE.md}]`);
+      const lgSpinner = await page.find(`ods-spinner[size=${ODS_SPINNER_SIZE.lg}]`);
+
+      const smSpinnerStyle = await smSpinner.getComputedStyle();
+      const smSpinnerHeight = parseInt(smSpinnerStyle.getPropertyValue('height'), 10);
+
+      const mdSpinnerStyle = await mdSpinner.getComputedStyle();
+      const mdSpinnerHeight = parseInt(mdSpinnerStyle.getPropertyValue('height'), 10);
+
+      const lgSpinnerStyle = await lgSpinner.getComputedStyle();
+      const lgSpinnerHeight = parseInt(lgSpinnerStyle.getPropertyValue('height'), 10);
+
+      expect(smSpinnerHeight).toBeLessThan(mdSpinnerHeight);
+      expect(smSpinnerHeight).toBeLessThan(lgSpinnerHeight);
+      expect(mdSpinnerHeight).toBeLessThan(lgSpinnerHeight);
+      expect(mdSpinnerHeight).toBeGreaterThan(smSpinnerHeight);
+      expect(lgSpinnerHeight).toBeGreaterThan(smSpinnerHeight);
+      expect(lgSpinnerHeight).toBeGreaterThan(mdSpinnerHeight);
+    });
+  });
 });

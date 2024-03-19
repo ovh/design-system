874715e7397989db56bd9e00d3de1d6170dc3ad5
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-03-19 09:45:48 +0100

    refactor(skeleton): migrate arch
    chore: active pipeline

---
+++ packages/ods/react/tests/_app/src/components/ods-skeleton.tsx
@@ -0,0 +1,10 @@
+import React from 'react-dom/client';
+import { OdsSkeleton } from 'ods-components-react';
+
+const Skeleton = () => {
+  return (
+    <OdsSkeleton />
+  );
+};
+
+export default Skeleton;

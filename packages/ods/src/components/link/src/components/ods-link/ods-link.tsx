01e6a8aaa3088966d686ae8dc99f66340a193905
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-04-02 14:10:40 +0200

    fix(link): docs & cursor

--- packages/ods/src/components/link/src/components/ods-link/ods-link.tsx
+++ packages/ods/src/components/link/src/components/ods-link/ods-link.tsx
@@ -1,4 +1,4 @@
-import { Component, type FunctionalComponent, Host, Prop, h } from '@stencil/core';
+import { Component, type FunctionalComponent, Host, Listen, Prop, h } from '@stencil/core';
 import { type OdsIconName } from '../../../../icon/src/constants/icon-name';
 import { ODS_LINK_COLOR, type OdsLinkColor } from '../../constant/link-color';
 
@@ -18,28 +18,35 @@ export class OdsLink {
   @Prop({ reflect: true }) public rel?: HTMLAnchorElement['rel'];
   @Prop({ reflect: true }) public target?: HTMLAnchorElement['target'];
 
+  @Listen('click')
+  onClick(event: MouseEvent): void {
+    if (this.disabled) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  }
+
   render(): FunctionalComponent {
     return (
-      <Host class='ods-link'>
-        <a
-          class={{
-            'ods-link__link': true,
-            'ods-link__link--disabled': this.disabled ?? false,
-            [`ods-link__link--${this.color}`]: true,
-          }}
-          download={ this.download }
-          href={ this.href }
-          part="link"
-          referrerPolicy={ this.referrerpolicy }
-          rel={ this.rel }
-          tabindex={ this.disabled ? -1 : 0 }
-          target={ this.target }>
+      <Host class="ods-link">
+        <a class={{
+          'ods-link__link': true,
+          'ods-link__link--disabled': this.disabled ?? false,
+          [`ods-link__link--${this.color}`]: true,
+        }}
+        download={ this.download }
+        href={ this.href }
+        part="link"
+        referrerPolicy={ this.referrerpolicy }
+        rel={ this.rel }
+        tabindex={ this.disabled ? -1 : 0 }
+        target={ this.target }>
           <span class="ods-link__link__text">
             { this.label }
           </span>
 
           {
-            !!this.icon && <ods-icon name={ this.icon }></ods-icon>
+            !!this.icon && <ods-icon part="icon" name={ this.icon } class="ods-link__link__icon"></ods-icon>
           }
         </a>
       </Host>

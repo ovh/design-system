d2ae3f900b0feff01e45e4d2c64f8699b93b0611
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-04-04 14:32:53 +0200

    fix(link): docs with new version

--- packages/storybook/stories/components/link/link.stories.ts
+++ packages/storybook/stories/components/link/link.stories.ts
@@ -16,7 +16,7 @@ export default meta;
 export const Demo: StoryObj = {
   render: (args) => html`
     <ods-link
-      class="my-link"
+      class="my-link-demo"
       color="${args.color}"
       disabled="${args.disabled}"
       download="${args.download}"
@@ -28,10 +28,10 @@ export const Demo: StoryObj = {
       target="${args.target}">
     </ods-link>
     <style>
-      .my-link::part(link) {
+      .my-link-demo::part(link) {
         ${args.customCssLink}
       }
-      .my-link::part(icon) {
+      .my-link-demo::part(icon) {
         ${args.customCssIcon}
       }
     </style>
@@ -136,4 +136,74 @@ export const Demo: StoryObj = {
     disabled: false,
     label: 'my label',
   },
+};
+
+export const Default: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-link href="https://www.ovhcloud.com/" label="Default Link"></ods-link>
+  `,
+};
+
+export const Color: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-link href="https://www.ovhcloud.com/" color="primary" label="Primary Color"></ods-link>
+  `,
+};
+
+
+export const Disabled: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-link href="https://www.ovhcloud.com/" disabled="false" label="Not Disabled"></ods-link>
+<ods-link href="https://www.ovhcloud.com/" disabled label="Disabled"></ods-link>  `,
+};
+
+
+export const Icon: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-link href="https://www.ovhcloud.com/" label="Icon Link" icon="warning"></ods-link>
+  `,
+};
+
+
+export const Target: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+  <div style="display: flex; flex-direction: column;">
+<ods-link href="https://www.ovhcloud.com/" target="_blank" label="Blank target"></ods-link>
+<ods-link href="https://www.ovhcloud.com/" target="_self" label="Self target"></ods-link>
+<ods-link href="https://www.ovhcloud.com/" target="_parent" label="Parent target"></ods-link>
+<ods-link href="https://www.ovhcloud.com/" target="_top" label="Top target"></ods-link>
+</div>
+  `,
+};
+
+
+export const CustomCSSLink: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-link class="my-link" href="/page" label="Content"></ods-link>
+<style>
+  .my-link::part(link) {
+    color: green;
+    font-size: 32px;
+  }
+</style>
+  `,
+}
+
+export const CustomCSSLinkIcon: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+  <ods-link class="my-link-icon" href="/page" label="Content" icon="warning"></ods-link>
+  <style>
+    .my-link-icon::part(icon) {
+      width: 2rem;
+      height: 2rem;
+    }
+  </style>
+  `,
 };

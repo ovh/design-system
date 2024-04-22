af1123918753e0d5f8a385772a004e6063bfc588
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-04-22 15:33:44 +0200

    test(input): new behaviour spec file

---
+++ packages/ods/src/components/input/tests/behaviour/ods-input.spec.ts
@@ -0,0 +1,79 @@
+jest.mock('../../src/controller/ods-input');
+import type { SpecPage } from '@stencil/core/testing';
+import { newSpecPage } from '@stencil/core/testing';
+import { OdsInput } from '../../src';
+
+describe('ods-input rendering', () => {
+  let page: SpecPage;
+  let root: HTMLElement | undefined;
+  let rootInstance: OdsInput | undefined;
+
+  async function setup(html: string): Promise<void> {
+    page = await newSpecPage({
+      components: [OdsInput],
+      html,
+    });
+
+    root = page.root;
+    rootInstance = page.rootInstance;
+  }
+
+  describe('methods', () => {
+    describe('clear', () => {
+      it('should clear the input', async() => {
+        await setup('<ods-input value="value"></ods-input>');
+        await rootInstance?.clear();
+        await page.waitForChanges();
+        expect(root?.getAttribute('value')).toBeNull();
+      });
+
+      it('should do nothing with disabled', async() => {
+        const value = 'value';
+        await setup(`<ods-input value="${value}" is-disabled></ods-input>`);
+        await rootInstance?.clear();
+        await page.waitForChanges();
+        expect(root?.getAttribute('value')).toBe(value);
+      });
+    });
+
+    describe('reset', () => {
+      it('should reset input with default value', async() => {
+        const defaultValue = 'defaultValue';
+        const value = 'value';
+        await setup(`<ods-input value="${value}" default-value="${defaultValue}"></ods-input>`);
+        expect(root?.getAttribute('value')).toBe(value);
+        await rootInstance?.reset();
+        await page.waitForChanges();
+        expect(root?.getAttribute('value')).toBe(defaultValue);
+      });
+
+      it('should do nothing with disabled', async() => {
+        const defaultValue = 'defaultValue';
+        const value = 'value';
+        await setup(`<ods-input is-disabled value="${value}" default-value="${defaultValue}"></ods-input>`);
+        expect(root?.getAttribute('value')).toBe(value);
+        await rootInstance?.reset();
+        await page.waitForChanges();
+        expect(root?.getAttribute('value')).toBe(value);
+      });
+    });
+
+    describe('toggleMasked', () => {
+      it('should change masked', async() => {
+        await setup('<ods-input is-masked></ods-input>');
+        expect(root?.getAttribute('is-masked')).toBe('');
+        await rootInstance?.toggleMask();
+        await page.waitForChanges();
+        expect(root?.getAttribute('is-masked')).toBeNull();
+      });
+
+      it('should do nothing because of disabled', async() => {
+        await setup('<ods-input is-masked is-disabled></ods-input>');
+        expect(root?.getAttribute('is-masked')).toBe('');
+        await rootInstance?.toggleMask();
+        await page.waitForChanges();
+        expect(root?.getAttribute('is-masked')).toBe('');
+      });
+    });
+  });
+});
--- packages/ods/src/components/input/tests/rendering/ods-input.spec.ts
+++ packages/ods/src/components/input/tests/rendering/ods-input.spec.ts
@@ -6,7 +6,6 @@ import { ODS_INPUT_TYPE, OdsInput } from '../../src';
 describe('ods-input rendering', () => {
   let page: SpecPage;
   let root: HTMLElement | undefined;
-  let rootInstance: OdsInput | undefined;
 
   async function setup(html: string): Promise<void> {
     page = await newSpecPage({
@@ -15,67 +14,7 @@ describe('ods-input rendering', () => {
     });
 
     root = page.root;
-    rootInstance = page.rootInstance;
   }
-
-  describe('methods', () => {
-    describe('clear', () => {
-      it('should clear the input', async() => {
-        await setup('<ods-input value="value"></ods-input>');
-        await rootInstance?.clear();
-        await page.waitForChanges();
-        expect(root?.getAttribute('value')).toBeNull();
-      });
-
-      it('should do nothing with disabled', async() => {
-        const value = 'value';
-        await setup(`<ods-input value="${value}" is-disabled></ods-input>`);
-        await rootInstance?.clear();
-        await page.waitForChanges();
-        expect(root?.getAttribute('value')).toBe(value);
-      });
-    });
-
-    describe('reset', () => {
-      it('should reset input with default value', async() => {
-        const defaultValue = 'defaultValue';
-        const value = 'value';
-        await setup(`<ods-input value="${value}" default-value="${defaultValue}"></ods-input>`);
-        expect(root?.getAttribute('value')).toBe(value);
-        await rootInstance?.reset();
-        await page.waitForChanges();
-        expect(root?.getAttribute('value')).toBe(defaultValue);
-      });
-
-      it('should do nothing with disabled', async() => {
-        const defaultValue = 'defaultValue';
-        const value = 'value';
-        await setup(`<ods-input is-disabled value="${value}" default-value="${defaultValue}"></ods-input>`);
-        expect(root?.getAttribute('value')).toBe(value);
-        await rootInstance?.reset();
-        await page.waitForChanges();
-        expect(root?.getAttribute('value')).toBe(value);
-      });
-    });
-
-    describe('toggleMasked', () => {
-      it('should change masked', async() => {
-        await setup('<ods-input is-masked></ods-input>');
-        expect(root?.getAttribute('is-masked')).toBe('');
-        await rootInstance?.toggleMask();
-        await page.waitForChanges();
-        expect(root?.getAttribute('is-masked')).toBeNull();
-      });
-
-      it('should do nothing because of disabled', async() => {
-        await setup('<ods-input is-masked is-disabled></ods-input>');
-        expect(root?.getAttribute('is-masked')).toBe('');
-        await rootInstance?.toggleMask();
-        await page.waitForChanges();
-        expect(root?.getAttribute('is-masked')).toBe('');
-      });
-    });
-  });
 
   describe('attributes', () => {
     describe('ariaLabel', () => {

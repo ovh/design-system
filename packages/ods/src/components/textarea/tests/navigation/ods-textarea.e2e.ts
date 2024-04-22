706fed3090ee0ceb336a8f561fb5e93fd77e87c5
Damien Pellier <dpellier@gmail.com>
2024-04-22 11:10:11 +0200

    feat(textarea): implement component

---
+++ packages/ods/src/components/textarea/tests/navigation/ods-textarea.e2e.ts
@@ -0,0 +1,41 @@
+import { type E2EPage, newE2EPage } from '@stencil/core/testing';
+
+describe('ods-textarea navigation', () => {
+  let page: E2EPage;
+
+  async function isFocused(): Promise<boolean> {
+    return await page.evaluate(() => {
+      const element = document.querySelector('ods-textarea');
+      return document.activeElement === element;
+    });
+  }
+
+  async function setup(content: string): Promise<void> {
+    page = await newE2EPage();
+
+    await page.setContent(content);
+    await page.evaluate(() => document.body.style.setProperty('margin', '0px'));
+  }
+
+  it('should be focused on tabulation', async() => {
+    await setup('<ods-textarea></ods-textarea>');
+
+    expect(await isFocused()).toBe(false);
+
+    await page.keyboard.press('Tab');
+    await page.waitForChanges();
+
+    expect(await isFocused()).toBe(true);
+  });
+
+  it('should not be focusable if disabled', async() => {
+    await setup('<ods-textarea is-disabled></ods-textarea>');
+
+    expect(await isFocused()).toBe(false);
+
+    await page.keyboard.press('Tab');
+    await page.waitForChanges();
+
+    expect(await isFocused()).toBe(false);
+  });
+});
---
+++ packages/storybook/stories/components/textarea/textarea.stories.ts
@@ -0,0 +1,211 @@
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { defineCustomElement } from '@ovhcloud/ods-components/dist/components/ods-textarea';
+import { html } from 'lit-html';
+import { CONTROL_CATEGORY, orderControls } from '../../control';
+
+defineCustomElement();
+
+const meta: Meta = {
+  title: 'ODS Components/Form/Textarea',
+  component: 'ods-textarea',
+};
+
+export default meta;
+
+export const Demo: StoryObj = {
+  render: (arg) => html`
+  <ods-textarea class="my-textarea"
+                ariaLabel="${arg.ariaLabel}"
+                ariaLabelledby="${arg.ariaLabelledby}"
+                cols="${arg.cols}"
+                has-error="${arg.hasError}"
+                is-disabled="${arg.isDisabled}"
+                is-readonly="${arg.isReadonly}"
+                is-required="${arg.isRequired}"
+                is-resizable="${arg.isResizable}"
+                placeholder="${arg.placeholder}"
+                rows="${arg.rows}"
+                spellcheck="${arg.spellcheck}">
+  </ods-textarea>
+<style>
+  .my-textarea::part(textarea) {
+    ${arg.customCss}
+  }
+</style>
+  `,
+  argTypes: orderControls({
+    ariaLabel: {
+      table: {
+        category: CONTROL_CATEGORY.accessibility,
+        defaultValue: { summary: 'ø' },
+        type: { summary: 'string' },
+      },
+      control: 'text',
+    },
+    ariaLabelledby: {
+      table: {
+        category: CONTROL_CATEGORY.accessibility,
+        defaultValue: { summary: 'ø' },
+        type: { summary: 'string' },
+      },
+      control: 'text',
+    },
+    cols: {
+      table: {
+        category: CONTROL_CATEGORY.general,
+        defaultValue: { summary: 'ø' },
+        type: { summary: 'number' },
+      },
+      control: 'number',
+    },
+    customCss: {
+      table: {
+        category: CONTROL_CATEGORY.design,
+        defaultValue: { summary: 'ø' },
+        type: { summary: 'string' },
+      },
+      control: 'text',
+      description: 'Set a custom style properties. Example: "height: 50px; width: 500px;"',
+    },
+    hasError: {
+      table: {
+        category: CONTROL_CATEGORY.general,
+        defaultValue: { summary: false },
+        type: { summary: 'boolean' },
+      },
+      control: 'boolean',
+    },
+    isDisabled: {
+      table: {
+        category: CONTROL_CATEGORY.general,
+        defaultValue: { summary: false },
+        type: { summary: 'boolean' },
+      },
+      control: 'boolean',
+    },
+    isReadonly: {
+      table: {
+        category: CONTROL_CATEGORY.general,
+        defaultValue: { summary: false },
+        type: { summary: 'boolean' },
+      },
+      control: 'boolean',
+    },
+    isRequired: {
+      table: {
+        category: CONTROL_CATEGORY.general,
+        defaultValue: { summary: false },
+        type: { summary: 'boolean' },
+      },
+      control: 'boolean',
+    },
+    isResizable: {
+      table: {
+        category: CONTROL_CATEGORY.general,
+        defaultValue: { summary: false },
+        type: { summary: 'boolean' },
+      },
+      control: 'boolean',
+    },
+    placeholder: {
+      table: {
+        category: CONTROL_CATEGORY.general,
+        defaultValue: { summary: 'ø' },
+        type: { summary: 'string' },
+      },
+      control: 'text',
+    },
+    rows: {
+      table: {
+        category: CONTROL_CATEGORY.general,
+        defaultValue: { summary: 'ø' },
+        type: { summary: 'number' },
+      },
+      control: 'number',
+    },
+    spellcheck: {
+      table: {
+        category: CONTROL_CATEGORY.general,
+        defaultValue: { summary: false },
+        type: { summary: 'boolean' },
+      },
+      control: 'boolean',
+    },
+  }),
+  args: {
+    hasError: false,
+    isDisabled: false,
+    isReadonly: false,
+    isRequired: false,
+    isResizable: false,
+    spellcheck: false,
+  },
+};
+
+export const Default: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-textarea></ods-textarea>
+  `,
+};
+
+export const Cols: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-textarea cols="50">
+</ods-textarea>
+  `,
+};
+
+export const CustomCSS: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-textarea class="my-textarea">
+</ods-textarea>
+<style>
+  .my-textarea::part(textarea) {
+    width: 100px;
+  }
+</style>
+  `,
+};
+
+export const Disabled: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-textarea is-disabled>
+</ods-textarea>
+  `,
+};
+
+export const Error: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-textarea has-error>
+</ods-textarea>
+  `,
+};
+
+export const Placeholder: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-textarea placeholder="Placeholder">
+</ods-textarea>
+  `,
+};
+
+export const Resizable: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-textarea is-resizable>
+</ods-textarea>
+  `,
+};
+
+export const Rows: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-textarea rows="8">
+</ods-textarea>
+  `,
+};

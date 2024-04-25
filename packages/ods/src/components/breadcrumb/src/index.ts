97b847e04b7a9ef8d20da28060faed764259a0b7
Damien Pellier <dpellier@gmail.com>
2024-04-25 14:00:49 +0200

    feat(breadcrumb): implement component

---
+++ packages/ods/src/components/breadcrumb/src/index.ts
@@ -0,0 +1,3 @@
+export { OdsBreadcrumb } from './components/ods-breadcrumb/ods-breadcrumb';
+export { OdsBreadcrumbItem } from './components/ods-breadcrumb-item/ods-breadcrumb-item';
+export { type OdsBreadcrumbItemClickEvent, type OdsBreadcrumbItemExpandEvent } from './interfaces/events';

3f5393ae53c666095d699a80f1cf913566767e06
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-05-03 16:23:15 +0200

    fix(quantity): e2e file

--- packages/ods/src/components/quantity/button/src/constants/button-color.js
+++
@@ -1,7 +0,0 @@
-var ODS_BUTTON_COLOR;
-(function (ODS_BUTTON_COLOR) {
-    ODS_BUTTON_COLOR["critical"] = "critical";
-    ODS_BUTTON_COLOR["primary"] = "primary";
-})(ODS_BUTTON_COLOR || (ODS_BUTTON_COLOR = {}));
-const ODS_BUTTON_COLORS = Object.freeze(Object.values(ODS_BUTTON_COLOR));
-export { ODS_BUTTON_COLOR, ODS_BUTTON_COLORS, };

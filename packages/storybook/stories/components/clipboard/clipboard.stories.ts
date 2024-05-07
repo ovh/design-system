19a7f7de0424498403465d71895219398a33454a
Damien Pellier <dpellier@gmail.com>
2024-05-07 09:48:50 +0200

    feat(clipboard): add part on tooltip

--- packages/storybook/stories/components/clipboard/clipboard.stories.ts
+++ packages/storybook/stories/components/clipboard/clipboard.stories.ts
@@ -13,6 +13,19 @@ const meta: Meta = {
 export default meta;
 
 export const Demo: StoryObj = {
+  render: (arg) => html`
+<ods-clipboard class="my-clipboard"
+  is-disabled="${arg.isDisabled}"
+  label-copy="${arg.labelCopy}"
+  label-copy-success="${arg.labelCopySuccess}"
+  value="${arg.value}">
+</ods-clipboard>
+<style>
+  .my-clipboard::part(tooltip) {
+    ${arg.customTooltipCss}
+  }
+</style>
+  `,
   argTypes: orderControls({
     isDisabled: {
       table: {
@@ -22,10 +35,19 @@ export const Demo: StoryObj = {
       },
       control: 'boolean',
     },
+    customTooltipCss: {
+      table: {
+        category: CONTROL_CATEGORY.design,
+        defaultValue: { summary: 'ø' },
+        type: { summary: 'string' },
+      },
+      control: 'text',
+      description: 'Set a custom style to the tooltip. Example: "width: 200px;"',
+    },
     labelCopy: {
       table: {
         category: CONTROL_CATEGORY.general,
-        defaultValue: { summary: 'ø' },
+        defaultValue: { summary: 'Copy to clipboard' },
         type: { summary: 'string' },
       },
       control: 'text',
@@ -33,7 +55,7 @@ export const Demo: StoryObj = {
     labelCopySuccess: {
       table: {
         category: CONTROL_CATEGORY.general,
-        defaultValue: { summary: 'ø' },
+        defaultValue: { summary: 'Copied!' },
         type: { summary: 'string' },
       },
       control: 'text',
@@ -49,6 +71,8 @@ export const Demo: StoryObj = {
   }),
   args: {
     isDisabled: false,
+    labelCopy: 'Copy to clipboard',
+    labelCopySuccess: 'Copied!',
   },
 };
 
@@ -59,6 +83,22 @@ export const CustomLabels: StoryObj = {
                label-copy-success="Copié !"
                value="Copy me">
 </ods-clipboard>
+  `,
+};
+
+export const CustomTooltipCSS = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-clipboard class="my-custom-clipboard"
+               label-copy="Copier le contenu"
+               label-copy-success="Copié !"
+               value="Copy me">
+</ods-clipboard>
+<style>
+  .my-custom-clipboard::part(tooltip) {
+    width: 200px;
+  }
+</style>
   `,
 };
 

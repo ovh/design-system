df16cdc8b1e6dbb44bf2bebe6ae5f55725eb3137
Damien Pellier <dpellier@gmail.com>
2024-05-07 09:50:58 +0200

    fix(ods): prevent utils transpiled code to be committed

--- packages/ods/src/components/utils/dom.js
+++
@@ -1,7 +0,0 @@
-async function copyToClipboard(value) {
-    return navigator.clipboard.writeText(value || '');
-}
-function getRandomHTMLId() {
-    return `id-${Date.now()}-${Math.random().toString(36).replace('0.', '')}`;
-}
-export { copyToClipboard, getRandomHTMLId, };

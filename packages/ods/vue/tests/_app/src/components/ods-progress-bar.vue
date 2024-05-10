4f733361cda910c8a1151cf28f3efb1a5836f5d2
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-05-10 16:33:45 +0200

    refactor(progress-bar): implementation component

---
+++ packages/ods/vue/tests/_app/src/components/ods-progress-bar.vue
@@ -0,0 +1,15 @@
+<template>
+  <OdsProgressBar />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import { OdsProgressBar } from '@ovhcloud/ods-components/vue';
+
+  export default defineComponent({
+    name: 'ProgressBar',
+    components: {
+      OdsProgressBar,
+    },
+  });
+</script>

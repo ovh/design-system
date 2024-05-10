265f8332dc3be866b3347a73ac06fb0af130cb54
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-05-10 16:33:45 +0200

    refactor(progress-bar): implementation component

---
+++ packages/ods/react/tests/_app/src/components/ods-progress-bar.tsx
@@ -0,0 +1,10 @@
+import React from 'react-dom/client';
+import { OdsProgressBar } from 'ods-components-react';
+
+const ProgressBar = () => {
+  return (
+    <OdsProgressBar />
+  );
+};
+
+export default ProgressBar;
---
+++ packages/ods/src/components/progress-bar/src/components/ods-progress-bar/ods-progress-bar.tsx
@@ -0,0 +1,24 @@
+import type { FunctionalComponent } from '@stencil/core';
+import { Component, Host, Prop, h } from '@stencil/core';
+
+@Component({
+  shadow: true,
+  styleUrl: 'ods-progress-bar.scss',
+  tag: 'ods-progress-bar',
+})
+export class OdsProgressBar {
+  @Prop({ reflect: true }) public max: number = 100;
+  @Prop({ reflect: true }) public value: number = 0;
+
+  render(): FunctionalComponent {
+    return (
+      <Host class="ods-progress-bar">
+        <progress
+          class="ods-progress-bar__progress"
+          max={ this.max }
+          part="progress"
+          value={ this.value } />
+      </Host>
+    );
+  }
+}

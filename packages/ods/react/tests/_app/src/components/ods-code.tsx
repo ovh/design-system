ee51a73f4be10ded808e9c8291037edfefe6bbef
Damien Pellier <dpellier@gmail.com>
2024-05-10 10:22:10 +0200

    feat(code): implement component

---
+++ packages/ods/react/tests/_app/src/components/ods-code.tsx
@@ -0,0 +1,12 @@
+import React from 'react-dom/client';
+import { OdsCode } from 'ods-components-react';
+
+const Code = () => {
+  return (
+    <OdsCode>
+      Some code to copy
+    </OdsCode>
+  );
+};
+
+export default Code;
---
+++ packages/ods/src/components/code/tests/behaviour/ods-code.e2e.ts
@@ -0,0 +1,41 @@
+import { type E2EElement, type E2EPage, newE2EPage } from '@stencil/core/testing';
+
+describe('ods-code behaviour', () => {
+  let el: E2EElement;
+  let page: E2EPage;
+
+  async function setup(content: string, customStyle?: string): Promise<void> {
+    page = await newE2EPage();
+
+    await page.setContent(content);
+    await page.evaluate(() => document.body.style.setProperty('margin', '0px'));
+
+    if (customStyle) {
+      await page.addStyleTag({ content: customStyle });
+    }
+
+    el = await page.find('ods-code');
+  }
+
+  beforeEach(jest.clearAllMocks);
+
+  describe('methods', () => {
+    describe('copy', () => {
+      it('should emit an odsCodeCopy event', async() => {
+        const dummyValue = 'dummy value';
+        await setup(`<ods-code can-copy>${dummyValue}</ods-code>`);
+        const copySpy = await page.spyOnEvent('odsCodeCopy');
+
+        // We have to focus the document to be able to call the navigator copy function
+        await page.keyboard.press('Tab');
+        await page.waitForChanges();
+
+        await el.callMethod('copy');
+        await page.waitForChanges();
+
+        expect(copySpy).toHaveReceivedEventTimes(1);
+        expect(copySpy).toHaveReceivedEventDetail(dummyValue);
+      });
+    });
+  });
+});

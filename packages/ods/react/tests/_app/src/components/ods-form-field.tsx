0fc9082b63f06d0cde4c9b9a0e7f2a7bc5fbfb11
Leo Largillet <leo.largillet@gmail.com>
2024-05-13 16:51:03 +0200

    feat(form-field): implement component

---
+++ packages/ods/react/tests/_app/src/components/ods-form-field.tsx
@@ -0,0 +1,10 @@
+import React from 'react-dom/client';
+import { OdsFormField } from 'ods-components-react';
+
+const FormField = () => {
+  return (
+    <OdsFormField />
+  );
+};
+
+export default FormField;

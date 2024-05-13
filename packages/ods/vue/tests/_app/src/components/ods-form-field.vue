8f79419cc0fdc73d3a5519962936bf536d8dead9
Leo Largillet <leo.largillet@gmail.com>
2024-05-13 16:51:03 +0200

    feat(form-field): implement component

---
+++ packages/ods/vue/tests/_app/src/components/ods-form-field.vue
@@ -0,0 +1,15 @@
+<template>
+  <OdsFormField />
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import { OdsFormField } from '@ovhcloud/ods-components/vue';
+
+  export default defineComponent({
+    name: 'FormField',
+    components: {
+      OdsFormField,
+    },
+  });
+</script>

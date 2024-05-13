51eb5c96256fc80d5ec72ce0d223bd64cb5e4a16
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-05-13 14:20:21 +0200

    fix(password): docs & test navigation

--- packages/ods/src/components/password/tests/behaviour/ods-password.e2e.ts
+++ packages/ods/src/components/password/tests/behaviour/ods-password.e2e.ts
@@ -80,4 +80,35 @@ describe('ods-password behaviour', () => {
       expect(odsChangeSpy).not.toHaveReceivedEvent();
     });
   });
+
+  describe('Form', () => {
+    it('should get form data with button type submit', async() => {
+      await setup(`<form method="get">
+        <ods-password name="odsPassword" value="On Vous Heberge ?"></ods-password>
+        <button type="reset">Reset</button>
+        <button type="submit">Submit</button>
+      </form>`);
+      const submitButton = await page.find('button[type="submit"]');
+      await submitButton.click();
+      await page.waitForNetworkIdle();
+      const url = new URL(page.url());
+      expect(url.searchParams.get('odsPassword')).toBe('On Vous Heberge ?');
+    });
+
+    it('should reset form with button type reset', async() => {
+      await setup(`<form method="get">
+        <ods-password name="odsPassword" value="On Vous Heberge ?"></ods-password>
+        <button type="reset">Reset</button>
+        <button type="submit">Submit</button>
+      </form>`);
+      const resetButton = await page.find('button[type="reset"]');
+      await resetButton.click();
+
+      const submitButton = await page.find('button[type="submit"]');
+      await submitButton.click();
+      await page.waitForNetworkIdle();
+      const url = new URL(page.url());
+      expect(url.searchParams.get('odsPassword')).toBe('');
+    });
+  });
 });

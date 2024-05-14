ec2c46f6fe669f28f74705382e04363e9136b594
Leo Largillet <leo.largillet@gmail.com>
2024-05-14 17:12:06 +0200

    feat(form-field): implement component

--- packages/ods/react/tests/_app/src/components/ods-form-field.tsx
+++ packages/ods/react/tests/_app/src/components/ods-form-field.tsx
@@ -1,9 +1,14 @@
 import React from 'react-dom/client';
-import { OdsFormField } from 'ods-components-react';
+import { OdsFormField, OdsInput, OdsText } from 'ods-components-react';
 
 const FormField = () => {
   return (
-    <OdsFormField />
+    <OdsFormField error="Wrong format.">
+      <OdsText slot="label" preset='label'>Description</OdsText>
+      <OdsText slot="visual-hint" preset='caption'>02/11/1999</OdsText>
+      <OdsInput name='input' />
+      <OdsText slot="helper" preset='span'>A little helper text</OdsText>
+    </OdsFormField>
   );
 };
 

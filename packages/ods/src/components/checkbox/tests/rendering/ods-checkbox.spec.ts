8c16f2c3dff8115bc6fbfe701ba965cc748bccf0
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-05-27 15:08:44 +0200

    refactor(checkbox): implementation component
    chore: test pipeline

---
+++ packages/ods/src/components/checkbox/tests/rendering/ods-checkbox.spec.ts
@@ -0,0 +1,153 @@
+import type { SpecPage } from '@stencil/core/testing';
+import { newSpecPage } from '@stencil/core/testing';
+import { OdsCheckbox } from '../../src';
+
+describe('ods-checkbox rendering', () => {
+  let page: SpecPage;
+  let root: HTMLElement | undefined;
+
+  async function setup(html: string): Promise<void> {
+    page = await newSpecPage({
+      components: [OdsCheckbox],
+      html,
+    });
+
+    root = page.root;
+  }
+
+  describe('ariaLabel', () => {
+    it('should be reflected', async() => {
+      const ariaLabelValue = 'ariaLabel value';
+
+      await setup(`<ods-checkbox aria-label="${ariaLabelValue}"></ods-checkbox>`);
+
+      expect(root?.getAttribute('aria-label')).toBe(ariaLabelValue);
+    });
+
+    it('should not be set by default', async() => {
+      await setup('<ods-checkbox></ods-checkbox>');
+
+      expect(root?.getAttribute('aria-label')).toBeNull();
+    });
+  });
+
+  describe('ariaLabelledby', () => {
+    it('should be reflected', async() => {
+      const ariaLabelledbyValue = 'ariaLabel value';
+
+      await setup(`<ods-checkbox aria-labelledby="${ariaLabelledbyValue}"></ods-checkbox>`);
+
+      expect(root?.getAttribute('aria-labelledby')).toBe(ariaLabelledbyValue);
+    });
+
+    it('should not be set by default', async() => {
+      await setup('<ods-checkbox></ods-checkbox>');
+
+      expect(root?.getAttribute('aria-labelledby')).toBeNull();
+    });
+  });
+
+  describe('isChecked', () => {
+    it('should be reflected', async() => {
+      await setup('<ods-checkbox is-checked></ods-checkbox>');
+
+      expect(root?.getAttribute('is-checked')).toBe('');
+    });
+
+    it('should be set by default', async() => {
+      await setup('<ods-checkbox></ods-checkbox>');
+
+      expect(root?.getAttribute('is-checked')).toBeNull();
+    });
+  });
+
+  describe('isDisabled', () => {
+    it('should be reflected', async() => {
+      await setup('<ods-checkbox is-disabled></ods-checkbox>');
+
+      expect(root?.getAttribute('is-disabled')).toBe('');
+    });
+
+    it('should be set by default', async() => {
+      await setup('<ods-checkbox></ods-checkbox>');
+
+      expect(root?.getAttribute('is-disabled')).toBeNull();
+    });
+  });
+
+  describe('isIndeterminate', () => {
+    it('should be reflected', async() => {
+      await setup('<ods-checkbox is-indeterminate></ods-checkbox>');
+
+      expect(root?.getAttribute('is-indeterminate')).toBe('');
+    });
+
+    it('should be set by default', async() => {
+      await setup('<ods-checkbox></ods-checkbox>');
+
+      expect(root?.getAttribute('is-indeterminate')).toBeNull();
+    });
+  });
+
+  describe('isRequired', () => {
+    it('should be reflected', async() => {
+      await setup('<ods-checkbox is-required></ods-checkbox>');
+
+      expect(root?.getAttribute('is-required')).toBe('');
+    });
+
+    it('should be set by default', async() => {
+      await setup('<ods-checkbox></ods-checkbox>');
+
+      expect(root?.getAttribute('is-required')).toBeNull();
+    });
+  });
+
+  describe('inputId', () => {
+    it('should be reflected', async() => {
+      const inputIdValue = 'inputId value';
+
+      await setup(`<ods-checkbox input-id="${inputIdValue}"></ods-checkbox>`);
+
+      expect(root?.getAttribute('input-id')).toBe(inputIdValue);
+    });
+
+    it('should not be set by default', async() => {
+      await setup('<ods-checkbox></ods-checkbox>');
+
+      expect(root?.getAttribute('input-id')).toBeNull();
+    });
+  });
+
+  describe('name', () => {
+    it('should be reflected', async() => {
+      const nameValue = 'name value';
+
+      await setup(`<ods-checkbox name="${nameValue}"></ods-checkbox>`);
+
+      expect(root?.getAttribute('name')).toBe(nameValue);
+    });
+
+    it('should not be set by default', async() => {
+      await setup('<ods-checkbox></ods-checkbox>');
+
+      expect(root?.getAttribute('name')).toBeNull();
+    });
+  });
+
+  describe('value', () => {
+    it('should be reflected', async() => {
+      const value = 'value';
+
+      await setup(`<ods-checkbox value="${value}"></ods-checkbox>`);
+
+      expect(root?.getAttribute('value')).toBe(value);
+    });
+
+    it('should not be set by default', async() => {
+      await setup('<ods-checkbox></ods-checkbox>');
+
+      expect(root?.getAttribute('value')).toBeNull();
+    });
+  });
+});
---
+++ packages/storybook/stories/components/checkbox/checkbox.stories.ts
@@ -0,0 +1,160 @@
+import type { Meta, StoryObj } from '@storybook/web-components';
+import { defineCustomElement } from '@ovhcloud/ods-components/dist/components/ods-checkbox';
+import { html } from 'lit-html';
+import { CONTROL_CATEGORY, orderControls } from '../../control';
+
+defineCustomElement();
+
+const meta: Meta = {
+  title: 'ODS Components/Form/Checkbox',
+  component: 'ods-checkbox',
+};
+
+export default meta;
+
+export const Demo: StoryObj = {
+  render: (args) => html`
+    <ods-checkbox
+      ariaLabel="${args.ariaLabel}"
+      ariaLabelledby="${args.ariaLabelledby}"
+      class="my-checkbox-demo"
+      is-disabled="${args.isDisabled}"
+      is-indeterminate="${args.isIndeterminate}"
+      is-required="${args.isRequired}"
+    ></ods-checkbox>
+    <style>
+      .my-checkbox-demo > input[type="checkbox"]:not(:disabled):checked {
+        ${args.customCss}
+      }
+    </style>`,
+  argTypes: orderControls({
+    ariaLabel: {
+      table: {
+        category: CONTROL_CATEGORY.accessibility,
+        defaultValue: { summary: 'ø' },
+        type: { summary: 'string' },
+      },
+      control: 'text',
+    },
+    ariaLabelledby: {
+      table: {
+        category: CONTROL_CATEGORY.accessibility,
+        defaultValue: { summary: 'ø' },
+        type: { summary: 'string' },
+      },
+      control: 'text',
+    },
+    customCss: {
+      table: {
+        category: CONTROL_CATEGORY.design,
+        defaultValue: { summary: 'ø' },
+        type: { summary: 'string' },
+      },
+      control: 'text',
+      description: 'Set a custom style properties. Example: "background-color: red; border-color: red;"',
+    },
+    isDisabled: {
+      table: {
+        category: CONTROL_CATEGORY.general,
+        defaultValue: { summary: false },
+        type: { summary: 'boolean' },
+      },
+      control: 'boolean',
+    },
+    isIndeterminate: {
+      table: {
+        category: CONTROL_CATEGORY.general,
+        defaultValue: { summary: false },
+        type: { summary: 'boolean' },
+      },
+      control: 'boolean',
+    },
+    isRequired: {
+      table: {
+        category: CONTROL_CATEGORY.general,
+        defaultValue: { summary: false },
+        type: { summary: 'boolean' },
+      },
+      control: 'boolean',
+    },
+  }),
+  args: {
+    isDisabled: false,
+    isIndeterminate: false,
+    isRequired: false,
+  },
+};
+export const Alignment: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+  <div style="display: inline-flex; justify-content: center; align-items: center; gap: 5px;">
+    <ods-checkbox input-id="alignment" value="alignment"></ods-checkbox>
+    <label for="alignment" style="font-size: 2rem;">Alignment</label>
+    <ods-checkbox input-id="alignment2" value="alignment2"></ods-checkbox>
+    <label for="alignment2" style="">Alignment2</label>
+  </div>
+ `,
+};
+
+export const CustomCSS: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-checkbox class="my-checkbox"></ods-checkbox>
+<style>
+  .my-checkbox > input[type="checkbox"]:not(:disabled):checked {
+    background-color: red;
+    border-color: red;
+  }
+</style>
+  `,
+};
+
+export const Checked: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-checkbox is-checked></ods-checkbox>
+  `,
+};
+
+export const Default: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-checkbox></ods-checkbox>
+  `,
+};
+
+export const Disabled: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-checkbox is-disabled></ods-checkbox>
+  `,
+};
+
+export const DisabledChecked: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-checkbox is-disabled is-checked></ods-checkbox>
+  `,
+};
+
+export const DisabledIndeterminate: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-checkbox is-disabled is-indeterminate></ods-checkbox>
+  `,
+};
+
+export const InputId: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+  <ods-checkbox input-id="input-id-example" name="name" value="huey"></ods-checkbox>
+  <label for="input-id-example">Example</label>
+ `,
+};
+
+export const Indeterminate: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-checkbox is-indeterminate></ods-checkbox>
+  `,
+};

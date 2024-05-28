f21b3fe5639c56e84f4a28fd2bae77fc81e3e729
Leo Largillet <leo.largillet@gmail.com>
2024-05-28 11:13:51 +0200

    feat(modal): implement component

--- packages/ods/src/components/modal/tests/rendering/ods-modal.e2e.ts
+++ packages/ods/src/components/modal/tests/rendering/ods-modal.e2e.ts
@@ -11,16 +11,136 @@ describe('ods-modal rendering', () => {
     await page.setContent(content);
     await page.evaluate(() => document.body.style.setProperty('margin', '0px'));
 
+    await page.waitForChanges();
+
     if (customStyle) {
       await page.addStyleTag({ content: customStyle });
     }
 
     el = await page.find('ods-modal');
+
+    await page.evaluate(() => {
+      const dialog = document.querySelector('ods-modal')?.shadowRoot?.querySelector('.ods-modal__dialog') as HTMLDialogElement;
+      dialog.style.setProperty('animation', 'none');
+    });
+
+    await page.waitForChanges();
   }
 
   it('should render the web component', async() => {
-    await setup('<ods-modal></ods-modal>');
+    await setup(`
+      <ods-modal is-open>
+        <ods-text>Hello, world!</ods-text>
+      </ods-modal>
+    `);
 
     expect(el.shadowRoot).not.toBeNull();
+  });
+
+  describe('color', () => {
+    it('should render with different headline color', async() => {
+      await setup(`
+        <ods-modal is-open color="warning">
+          <ods-text>Hello, world!</ods-text>
+        </ods-modal>
+      `);
+
+      const header = await page.find('ods-modal >>> .ods-modal__dialog__header--warning');
+
+      expect(header).not.toBeNull();
+    });
+  });
+
+  describe('isDismissible', () => {
+    it('should render with close button by default', async() => {
+      await setup(`
+        <ods-modal is-open>
+          <ods-text>Hello, world!</ods-text>
+        </ods-modal>
+      `);
+
+      const closeButton = await page.find('ods-modal >>> .ods-modal__dialog__header__close');
+
+      expect(closeButton).not.toBeNull();
+    });
+
+    it('should render without close button if set to false', async() => {
+      await setup(`
+        <ods-modal is-open is-dismissible="false">
+          <ods-text>Hello, world!</ods-text>
+        </ods-modal>
+      `);
+
+      const closeButton = await page.find('ods-modal >>> .ods-modal__dialog__header__close');
+
+      expect(closeButton).toBeNull();
+    });
+  });
+
+  describe('isOpen', () => {
+    it('should not display modal by default', async() => {
+      await setup(`
+        <ods-modal>
+          <ods-text>Hello, world!</ods-text>
+        </ods-modal>
+      `);
+
+      const isVisible = await page.evaluate(() => {
+        const dialog = document.querySelector('ods-modal')?.shadowRoot?.querySelector('.ods-modal__dialog') as HTMLDialogElement;
+        return dialog && dialog.hasAttribute('open');
+      });
+
+      expect(isVisible).toBe(false);
+    });
+
+    it('should display modal if set to true', async() => {
+      await setup(`
+        <ods-modal is-open>
+          <ods-text>Hello, world!</ods-text>
+        </ods-modal>
+      `);
+
+      const isVisible = await page.evaluate(() => {
+        const dialog = document.querySelector('ods-modal')?.shadowRoot?.querySelector('.ods-modal__dialog') as HTMLDialogElement;
+        return dialog && dialog.hasAttribute('open');
+      });
+
+      expect(isVisible).toBe(true);
+    });
+  });
+
+  describe('slots', () => {
+    it('should render content slot', async() => {
+      await setup(`
+        <ods-modal>
+          <div class="test-content">Content</div>
+        </ods-modal>
+      `);
+
+      const content = await el.find('.test-content');
+      expect(content).not.toBeNull();
+    });
+
+    it('should not render actions slot if not set', async() => {
+      await setup(`
+        <ods-modal>
+          <div class="test-content">Content</div>
+        </ods-modal>
+      `);
+
+      const actionsSlot = await el.find('slot[name="actions"]');
+      expect(actionsSlot).toBeNull();
+    });
+
+    it('should render actions slot if set', async() => {
+      await setup(`
+        <ods-modal>
+          <button slot="actions" class="test-actions">Actions</button>
+        </ods-modal>
+      `);
+
+      const actions = await el.find('.test-actions');
+      expect(actions).not.toBeNull();
+    });
   });
 });
--- packages/ods/vue/tests/_app/src/components/ods-modal.vue
+++ packages/ods/vue/tests/_app/src/components/ods-modal.vue
@@ -1,10 +1,15 @@
 <template>
-  <OdsModal />
+  <OdsModal isOpen>
+    <OdsText>Lorem ipsum</OdsText>
+    <OdsText>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur, nunc nec ultricies ultricies, nunc nunc ultricies
+    </OdsText>
+  </OdsModal>
 </template>
 
 <script lang="ts">
   import { defineComponent } from 'vue';
-  import { OdsModal } from '@ovhcloud/ods-components/vue';
+  import { OdsModal, OdsText } from '@ovhcloud/ods-components/vue';
 
   export default defineComponent({
     name: 'Modal',

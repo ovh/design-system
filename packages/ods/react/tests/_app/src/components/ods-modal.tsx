e7fff0afd54d490bfaf224aefae124904eebea52
Leo Largillet <leo.largillet@gmail.com>
2024-05-29 11:31:06 +0200

    feat(modal): improve react ods-text presets

--- packages/ods/react/tests/_app/src/components/ods-modal.tsx
+++ packages/ods/react/tests/_app/src/components/ods-modal.tsx
@@ -4,8 +4,8 @@ import { OdsModal, OdsText } from 'ods-components-react';
 const Modal = () => {
   return (
     <OdsModal isOpen>
-      <OdsText preset={'heading-3'}>Lorem ipsum</OdsText>
-      <OdsText preset={'span'}>
+      <OdsText preset='heading-3'>Lorem ipsum</OdsText>
+      <OdsText preset='span'>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur, nunc nec ultricies ultricies, nunc nunc ultricies
       </OdsText>
     </OdsModal>

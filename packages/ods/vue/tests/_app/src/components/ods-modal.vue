9698ff9bd13d1fdd03f464629c090de0a6d30895
Leo Largillet <leo.largillet@gmail.com>
2024-06-11 16:43:18 +0200

    feat(modal): fix react & vue e2e tests by adding a delay

--- packages/ods/vue/tests/_app/src/components/ods-modal.vue
+++ packages/ods/vue/tests/_app/src/components/ods-modal.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <OdsModal :isOpen="true">
+    <OdsButton label="Open Modal" @click="showModal = true"></OdsButton>
+    <OdsModal :isOpen="showModal" @odsModalClose="showModal = false">
       <OdsText preset="heading-3">Lorem ipsum</OdsText>
       <OdsText preset="span">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur, nunc nec ultricies ultricies, nunc nunc ultricies
@@ -11,13 +12,21 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import { OdsModal, OdsText } from '@ovhcloud/ods-components/vue';
+import { OdsButton, OdsModal, OdsText } from '@ovhcloud/ods-components/vue';
 
 export default defineComponent({
   name: 'Modal',
   components: {
+    OdsButton,
     OdsModal,
     OdsText
   },
+  setup() {
+    const showModal = ref(false);
+
+    return {
+      showModal
+    };
+  }
 });
 </script>

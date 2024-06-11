c5ba056894aec3e02c3bd20a1fd50c4a6fa72636
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-06-11 17:20:00 +0200

    fix(switch): e2e test vue & react

--- packages/ods/vue/tests/_app/src/components/ods-switch.vue
+++ packages/ods/vue/tests/_app/src/components/ods-switch.vue
@@ -6,9 +6,9 @@
   </OdsSwitch>
 
   <OdsSwitch @onOdsChange="onOdsChange" isDisabled>
-    <OdsSwitchItem name="switch-radio" is-checked input-id="label1" value="1">label1</OdsSwitchItem>
-    <OdsSwitchItem name="switch-radio" input-id="label2" value="2">label2</OdsSwitchItem>
-    <OdsSwitchItem name="switch-radio" input-id="label3" value="3">label3</OdsSwitchItem>
+    <OdsSwitchItem name="switch-radio-disabled" is-checked input-id="disabled1" value="1">label1</OdsSwitchItem>
+    <OdsSwitchItem name="switch-radio-disabled" input-id="disabled2" value="2">label2</OdsSwitchItem>
+    <OdsSwitchItem name="switch-radio-disabled" input-id="disabled3" value="3">label3</OdsSwitchItem>
   </OdsSwitch>
 </template>
 

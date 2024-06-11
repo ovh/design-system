44a8a3b5786a0f16fe7fff74663a710cb3ad8969
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-06-11 17:20:00 +0200

    fix(switch): e2e test vue & react

--- packages/ods/react/tests/_app/src/components/ods-switch.tsx
+++ packages/ods/react/tests/_app/src/components/ods-switch.tsx
@@ -9,15 +9,15 @@ const Switch = () => {
   return (
     <>
     <OdsSwitch onOdsChange={ onOdsChange }>
-      <OdsSwitchItem name="switch-radio" is-checked input-id="label1" value="1">label1</OdsSwitchItem>
+      <OdsSwitchItem name="switch-radio" input-id="label1" value="1">label1</OdsSwitchItem>
       <OdsSwitchItem name="switch-radio" input-id="label2" value="2">label2</OdsSwitchItem>
       <OdsSwitchItem name="switch-radio" input-id="label3" value="3">label3</OdsSwitchItem>
     </OdsSwitch>
 
     <OdsSwitch isDisabled onOdsChange={ onOdsChange }>
-      <OdsSwitchItem name="switch-radio" is-checked input-id="label1" value="1">label1</OdsSwitchItem>
-      <OdsSwitchItem name="switch-radio" input-id="label2" value="2">label2</OdsSwitchItem>
-      <OdsSwitchItem name="switch-radio" input-id="label3" value="3">label3</OdsSwitchItem>
+      <OdsSwitchItem name="switch-radio-disabled" input-id="disabled1" value="1">label1</OdsSwitchItem>
+      <OdsSwitchItem name="switch-radio-disabled" input-id="disabled2" value="2">label2</OdsSwitchItem>
+      <OdsSwitchItem name="switch-radio-disabled" input-id="disabled3" value="3">label3</OdsSwitchItem>
     </OdsSwitch>
     </>
   );

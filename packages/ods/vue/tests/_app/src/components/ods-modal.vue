330c3f962ae49af5ba9efaf9e2308eda62bb5362
Leo Largillet <leo.largillet@gmail.com>
2024-06-11 09:26:08 +0200

    feat(modal): simplify vue test & fix alphabetical order of Modal in Storybook

--- packages/ods/vue/tests/_app/src/components/ods-modal.vue
+++ packages/ods/vue/tests/_app/src/components/ods-modal.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <OdsButton label="Open Modal" @click="showModal = true"></OdsButton>
-    <OdsModal :isOpen="showModal" @odsModalClose="showModal = false">
+    <OdsModal :isOpen="true">
       <OdsText preset="heading-3">Lorem ipsum</OdsText>
       <OdsText preset="span">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam consectetur, nunc nec ultricies ultricies, nunc nunc ultricies
@@ -12,21 +11,13 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import { OdsButton, OdsModal, OdsText } from '@ovhcloud/ods-components/vue';
+import { OdsModal, OdsText } from '@ovhcloud/ods-components/vue';
 
 export default defineComponent({
   name: 'Modal',
   components: {
-    OdsButton,
     OdsModal,
     OdsText
   },
-  setup() {
-    const showModal = ref(false);
-
-    return {
-      showModal
-    };
-  }
 });
 </script>

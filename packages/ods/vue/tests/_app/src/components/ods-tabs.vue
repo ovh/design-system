cf96de64a088b3f02ae62dae54bb60335c823b02
Damien Pellier <dpellier@gmail.com>
2024-06-13 16:18:02 +0200

    feat(tabs): implement component

---
+++ packages/ods/vue/tests/_app/src/components/ods-tabs.vue
@@ -0,0 +1,20 @@
+<template>
+  <OdsTabs>
+    <OdsTab>Tab 1</OdsTab>
+    <OdsTab>Tab 2</OdsTab>
+    <OdsTab>Tab 3</OdsTab>
+  </OdsTabs>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+  import { OdsTab, OdsTabs } from '@ovhcloud/ods-components/vue';
+
+  export default defineComponent({
+    name: 'Tabs',
+    components: {
+      OdsTab,
+      OdsTabs,
+    },
+  });
+</script>

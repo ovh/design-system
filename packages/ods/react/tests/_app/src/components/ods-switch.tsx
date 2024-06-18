91fdf0a253f469f82395ad462a036a3c8d2c1faf
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-06-18 16:51:21 +0200

    fix(switch): test react & vue

--- packages/ods/react/tests/_app/src/components/ods-switch.tsx
+++ packages/ods/react/tests/_app/src/components/ods-switch.tsx
@@ -8,16 +8,16 @@ const Switch = () => {
 
   return (
     <>
-    <OdsSwitch onOdsChange={ onOdsChange }>
-      <OdsSwitchItem name="switch-radio" input-id="label1" value="1">label1</OdsSwitchItem>
-      <OdsSwitchItem name="switch-radio" input-id="label2" value="2">label2</OdsSwitchItem>
-      <OdsSwitchItem name="switch-radio" input-id="label3" value="3">label3</OdsSwitchItem>
+    <OdsSwitch onOdsChange={ onOdsChange } name="switch-radio">
+      <OdsSwitchItem value="1">label1</OdsSwitchItem>
+      <OdsSwitchItem value="2">label2</OdsSwitchItem>
+      <OdsSwitchItem value="3">label3</OdsSwitchItem>
     </OdsSwitch>
 
-    <OdsSwitch isDisabled onOdsChange={ onOdsChange }>
-      <OdsSwitchItem name="switch-radio-disabled" input-id="disabled1" value="1">label1</OdsSwitchItem>
-      <OdsSwitchItem name="switch-radio-disabled" input-id="disabled2" value="2">label2</OdsSwitchItem>
-      <OdsSwitchItem name="switch-radio-disabled" input-id="disabled3" value="3">label3</OdsSwitchItem>
+    <OdsSwitch isDisabled onOdsChange={ onOdsChange } name="switch-radio-disabled" >
+      <OdsSwitchItem value="1">label1</OdsSwitchItem>
+      <OdsSwitchItem value="2">label2</OdsSwitchItem>
+      <OdsSwitchItem value="3">label3</OdsSwitchItem>
     </OdsSwitch>
     </>
   );
--- packages/ods/vue/tests/_app/src/components/ods-switch.vue
+++ packages/ods/vue/tests/_app/src/components/ods-switch.vue
@@ -1,14 +1,14 @@
 <template>
-  <OdsSwitch @onOdsChange="onOdsChange">
-    <OdsSwitchItem name="switch-radio" is-checked input-id="label1" value="1">label1</OdsSwitchItem>
-    <OdsSwitchItem name="switch-radio" input-id="label2" value="2">label2</OdsSwitchItem>
-    <OdsSwitchItem name="switch-radio" input-id="label3" value="3">label3</OdsSwitchItem>
+  <OdsSwitch @onOdsChange="onOdsChange" name="switch-radio">
+    <OdsSwitchItem is-checked value="1">label1</OdsSwitchItem>
+    <OdsSwitchItem value="2">label2</OdsSwitchItem>
+    <OdsSwitchItem value="3">label3</OdsSwitchItem>
   </OdsSwitch>
 
-  <OdsSwitch @onOdsChange="onOdsChange" isDisabled>
-    <OdsSwitchItem name="switch-radio-disabled" is-checked input-id="disabled1" value="1">label1</OdsSwitchItem>
-    <OdsSwitchItem name="switch-radio-disabled" input-id="disabled2" value="2">label2</OdsSwitchItem>
-    <OdsSwitchItem name="switch-radio-disabled" input-id="disabled3" value="3">label3</OdsSwitchItem>
+  <OdsSwitch @onOdsChange="onOdsChange" isDisabled name="switch-radio-disabled">
+    <OdsSwitchItem is-checked value="1">label1</OdsSwitchItem>
+    <OdsSwitchItem value="2">label2</OdsSwitchItem>
+    <OdsSwitchItem value="3">label3</OdsSwitchItem>
   </OdsSwitch>
 </template>
 

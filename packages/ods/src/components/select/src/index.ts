03042c4883b1c9dfd30441eceb004d1e93e72835
Damien Pellier <dpellier@gmail.com>
2024-06-28 15:39:49 +0200

    feat(phone-number): implement component

--- packages/ods/src/components/select/src/index.ts
+++ packages/ods/src/components/select/src/index.ts
@@ -1,2 +1,3 @@
 export { OdsSelect } from './components/ods-select/ods-select';
 export { type OdsSelectEventChange, type OdsSelectEventChangeDetail } from './interfaces/events';
+export { type OdsSelectCustomRenderer, type OdsSelectCustomRendererData } from './interfaces/options';
--- packages/ods/src/components/select/stencil.config.ts
+++ packages/ods/src/components/select/stencil.config.ts
@@ -3,5 +3,8 @@ import { getStencilConfig } from '../../config/stencil';
 export const config = getStencilConfig({
   args: process.argv.slice(2),
   componentCorePackage: '@ovhcloud/ods-component-select',
-  namespace: 'ods-select',
+  jestOption: {
+    setupFiles: ['<rootDir>/tests/setup.ts'],
+  },
+  namespace: 'ods-select'
 });

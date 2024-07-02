24b10985de328b3a1a7133e57b55e52f5e27701a
Leo Largillet <leo.largillet@gmail.com>
2024-07-02 09:13:55 +0200

    feat(pagination): implement component

---
+++ packages/ods/src/components/pagination/src/index.ts
@@ -0,0 +1,2 @@
+export { OdsPagination } from './components/ods-pagination/ods-pagination';
+export { ODS_PAGINATION_PER_PAGE, ODS_PAGINATION_PER_PAGE_OPTIONS } from './constants/pagination-per-page';

83a4d277f9c636b0bd558be79add0618d7e19f16
Leo Largillet <leo.largillet@gmail.com>
2024-07-02 10:17:19 +0200

    feat(pagination): fix linters

--- packages/ods/src/components/pagination/src/index.ts
+++ packages/ods/src/components/pagination/src/index.ts
@@ -1,3 +1,3 @@
 export { OdsPagination } from './components/ods-pagination/ods-pagination';
 export { ODS_PAGINATION_PER_PAGE, ODS_PAGINATION_PER_PAGE_OPTIONS } from './constants/pagination-per-page';
-export { OdsPaginationCurrentChangeEvent, OdsPaginationChangedEventDetail, OdsPaginationEvent, OdsPaginationItemPerPageChangedEvent, OdsPaginationItemPerPageChangedEventDetail } from './interfaces/events';
+export type { OdsPaginationCurrentChangeEvent, OdsPaginationChangedEventDetail, OdsPaginationEvent, OdsPaginationItemPerPageChangedEvent, OdsPaginationItemPerPageChangedEventDetail } from './interfaces/events';

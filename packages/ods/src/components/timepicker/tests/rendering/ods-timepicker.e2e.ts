999243ed95575d54d2278d9fa9b73511753b7fe4
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-07-08 11:44:06 +0200

    refactor(timeicker): implement component

---
+++ packages/ods/src/components/timepicker/tests/rendering/ods-timepicker.e2e.ts
@@ -0,0 +1,64 @@
+import type { E2EElement, E2EPage } from '@stencil/core/testing';
+import { newE2EPage } from '@stencil/core/testing';
+
+describe('ods-timepicker rendering', () => {
+  let el: E2EElement;
+  let input: E2EElement;
+  let page: E2EPage;
+  let select: E2EElement;
+
+  async function setup(content: string, customStyle?: string): Promise<void> {
+    page = await newE2EPage();
+
+    await page.setContent(content);
+    await page.evaluate(() => document.body.style.setProperty('margin', '0px'));
+
+    if (customStyle) {
+      await page.addStyleTag({ content: customStyle });
+    }
+
+    el = await page.find('ods-timepicker');
+    input = await page.find('ods-timepicker >>> .ods-timepicker__time');
+    select = await page.find('ods-timepicker >>> .ods-timepicker__timezones');
+  }
+
+  it('should render the web component', async() => {
+    await setup('<ods-timepicker></ods-timepicker>');
+
+    expect(el.shadowRoot).not.toBeNull();
+    expect(select).toBeNull();
+  });
+
+  it('should render the web component with select with all timezones', async() => {
+    await setup('<ods-timepicker timezones="all"></ods-timepicker>');
+
+    expect(el.shadowRoot).not.toBeNull();
+    expect(select).not.toBeNull();
+  });
+
+  it('should render the web component with select with specific timezones ', async() => {
+    await setup('<ods-timepicker timezones=\'["utc-2", "utc+2", "utc+30"]\'></ods-timepicker>');
+
+    expect(el.shadowRoot).not.toBeNull();
+    expect(select).not.toBeNull();
+  });
+
+  it('should render the web component with select because of currentTimezone', async() => {
+    await setup('<ods-timepicker current-timezone="UTC+1"></ods-timepicker>');
+
+    expect(el.shadowRoot).not.toBeNull();
+    expect(select).not.toBeNull();
+  });
+
+  describe('part', () => {
+    it('should render with custom style applied on input & select', async() => {
+      const width = '300px';
+      await setup('<ods-timepicker timezones="all"></ods-timepicker>', `ods-timepicker::part(input) { width: ${width}; } ods-timepicker::part(select) { width: ${width}; }`);
+
+      const inputStyle = await input.getComputedStyle();
+      expect(inputStyle.getPropertyValue('width')).toBe(width);
+      const selectStyle = await select.getComputedStyle();
+      expect(selectStyle.getPropertyValue('width')).toBe(width);
+    });
+  });
+});

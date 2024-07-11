a7f33daf41851498efcceff0983ca02bde4691b4
Damien Pellier <dpellier@gmail.com>
2024-07-11 16:43:45 +0200

    refactor(datepicker): rewrite using native input

--- packages/ods/src/components/datepicker/tests/behaviour/ods-datepicker.e2e.ts
+++ packages/ods/src/components/datepicker/tests/behaviour/ods-datepicker.e2e.ts
@@ -1,9 +1,15 @@
-import type { E2EElement, E2EPage } from '@stencil/core/testing';
-import { newE2EPage } from '@stencil/core/testing';
+import { type E2EElement, type E2EPage, newE2EPage } from '@stencil/core/testing';
+import { type OdsDatepicker } from '../../src';
 
 describe('ods-datepicker behaviour', () => {
   let el: E2EElement;
   let page: E2EPage;
+
+  async function isDatepickerOpen(): Promise<boolean> {
+    return page.evaluate(() => {
+      return document.querySelector('ods-datepicker')?.shadowRoot?.querySelector('.datepicker')?.classList.contains('active') || false;
+    });
+  }
 
   async function setup(content: string): Promise<void> {
     page = await newE2EPage();
@@ -18,82 +24,91 @@ describe('ods-datepicker behaviour', () => {
 
   beforeEach(jest.clearAllMocks);
 
-  describe('method:clear', () => {
-    it('should clear value', async() => {
-      const value = new Date('01/02/2024');
-      await setup('<ods-datepicker></ods-datepicker>');
+  describe('methods', () => {
+    describe('clear', () => {
+      it('should emit an odsClear event', async() => {
+        await setup('<ods-datepicker value="value"></ods-datepicker>');
+        const odsClearSpy = await page.spyOnEvent('odsClear');
 
-      await el.setProperty('value',  value)
-      await el.callMethod('clear');
-      await page.waitForChanges();
+        await el.callMethod('clear');
+        await page.waitForChanges();
 
-      expect(await el.getProperty('value')).toBeNull();
+        expect(await el.getProperty('value')).toBeNull();
+        expect(odsClearSpy).toHaveReceivedEventTimes(1);
+      });
+
+      it('should emit an odsClear event even if disabled', async() => {
+        await setup('<ods-datepicker is-disabled value="value"></ods-datepicker>');
+        const odsClearSpy = await page.spyOnEvent('odsClear');
+
+        await el.callMethod('clear');
+        await page.waitForChanges();
+
+        expect(await el.getProperty('value')).toBeNull();
+        expect(odsClearSpy).toHaveReceivedEventTimes(1);
+      });
+    });
+
+    describe('close', () => {
+      it('should close the datepicker', async() => {
+        await setup('<ods-datepicker></ods-datepicker>');
+        await el.callMethod('open');
+        await page.waitForChanges();
+
+        expect(await isDatepickerOpen()).toBe(true);
+
+        await el.callMethod('close');
+        await page.waitForChanges();
+
+        expect(await isDatepickerOpen()).toBe(false);
+      });
+    });
+
+    describe('open', () => {
+      it('should open the datepicker', async() => {
+        await setup('<ods-datepicker></ods-datepicker>');
+
+        expect(await isDatepickerOpen()).toBe(false);
+
+        await el.callMethod('open');
+        await page.waitForChanges();
+
+        expect(await isDatepickerOpen()).toBe(true);
+      });
+    });
+
+    describe('reset', () => {
+      it('should emit an odsReset event', async() => {
+        await setup('<ods-datepicker></ods-datepicker>');
+        await page.evaluate(() => {
+          document.querySelector<OdsDatepicker & HTMLElement>('ods-datepicker')!.defaultValue = new Date(0);
+          document.querySelector<OdsDatepicker & HTMLElement>('ods-datepicker')!.value = new Date();
+        });
+        await page.waitForChanges();
+        const odsResetSpy = await page.spyOnEvent('odsReset');
+
+        await el.callMethod('reset');
+        await page.waitForChanges();
+
+        expect(await page.evaluate(() => document.querySelector('ods-datepicker')?.shadowRoot?.querySelector('input')?.value)).toBe('01/01/1970');
+        expect(odsResetSpy).toHaveReceivedEventTimes(1);
+      });
+
+      it('should emit an odsReset event even if disabled', async() => {
+        await setup('<ods-datepicker is-disabled></ods-datepicker>');
+        await page.evaluate(() => {
+          document.querySelector<OdsDatepicker & HTMLElement>('ods-datepicker')!.defaultValue = new Date(0);
+          document.querySelector<OdsDatepicker & HTMLElement>('ods-datepicker')!.value = new Date();
+        });
+        await page.waitForChanges();
+        const odsResetSpy = await page.spyOnEvent('odsReset');
+
+        await el.callMethod('reset');
+        await page.waitForChanges();
+
+        expect(await page.evaluate(() => document.querySelector('ods-datepicker')?.shadowRoot?.querySelector('input')?.value)).toBe('01/01/1970');
+        expect(odsResetSpy).toHaveReceivedEventTimes(1);
+      });
     });
   });
-
-  describe('method:reset', () => {
-    it('should reset value to default value', async() => {
-      const defaultValue = new Date('01/01/2024');
-      const value = new Date('01/02/2024');
-      await setup(`<ods-datepicker default-value="${defaultValue.toString()}"></ods-datepicker>`);
-
-      await el.setProperty('value',  value);
-      await el.callMethod('reset');
-      await page.waitForChanges();
-
-      expect(await el.getProperty('value')).toBe(defaultValue);
-    });
-  });
-
-  describe('event', () => {
-    it('should receive odsClear event', async() => {
-      await setup('<ods-datepicker></ods-datepicker>');
-      const odsClearSpy = await page.spyOnEvent('odsClear');
-      await el.callMethod('clear');
-      await page.waitForChanges();
-      expect(odsClearSpy).toHaveReceivedEventTimes(1);
-    });
-
-    it('should receive odsReset event', async() => {
-      await setup(`<ods-datepicker></ods-datepicker>`);
-      const odsResetSpy = await page.spyOnEvent('odsReset');
-      await el.callMethod('reset');
-      await page.waitForChanges();
-      expect(odsResetSpy).toHaveReceivedEventTimes(1);
-    });
-
-    it('should receive odsFocus event', async() => {
-      await setup('<ods-datepicker></ods-datepicker>');
-      const odsFocusSpy = await page.spyOnEvent('odsFocus');
-      await page.keyboard.press('Tab');
-      await page.waitForChanges();
-
-      expect(odsFocusSpy).toHaveReceivedEventTimes(1);
-    });
-
-    it('should receive odsBlur event', async() => {
-      await setup('<ods-datepicker></ods-datepicker>');
-      const odsBlurSpy = await page.spyOnEvent('odsBlur');
-      await page.keyboard.press('Tab');
-      await page.waitForChanges();
-
-      await page.keyboard.press('Tab');
-      await page.waitForChanges();
-
-      expect(odsBlurSpy).toHaveReceivedEventTimes(1);
-    });
-
-    it('should receive odsChange event', async() => {
-      await setup('<ods-datepicker></ods-datepicker>');
-      const odsChangeSpy = await page.spyOnEvent('odsChange');
-      await page.keyboard.press('Tab');
-      await page.waitForChanges();
-
-      await page.keyboard.press('0');
-      await page.waitForChanges();
-
-      expect(odsChangeSpy).toHaveReceivedEventTimes(1);
-    });
-  });
-
 });
---
+++ packages/ods/src/components/datepicker/tests/controller/ods-datepicker.spec.ts
@@ -0,0 +1,68 @@
+jest.mock('vanillajs-datepicker', () => ({
+  Datepicker: {
+    formatDate: jest.fn().mockReturnValue('formatted date'),
+  },
+}));
+
+import { Datepicker } from 'vanillajs-datepicker';
+import { formatDate, setFormValue } from '../../src/controller/ods-datepicker';
+
+describe('ods-datepicker controller', () => {
+  beforeEach(jest.clearAllMocks);
+
+  describe('formatDate', () => {
+    const dummyFormat = 'dd/mm/yyyy';
+
+    it('should return an empty string if no date is passed', () => {
+      // @ts-ignore for test purpose
+      expect(formatDate(undefined, dummyFormat)).toBe('');
+      expect(formatDate(null, dummyFormat)).toBe('');
+    });
+
+    it('should return an empty string if the date is not a correct Date', () => {
+      // @ts-ignore for test purpose
+      expect(formatDate('I am not a date', dummyFormat)).toBe('');
+      // @ts-ignore for test purpose
+      expect(formatDate(true, dummyFormat)).toBe('');
+      // @ts-ignore for test purpose
+      expect(formatDate({ dummy: 'date' }, dummyFormat)).toBe('');
+      // @ts-ignore for test purpose
+      expect(formatDate([new Date()], dummyFormat)).toBe('');
+    });
+
+    it('should return a formatted date', () => {
+      const dummyDate = new Date();
+
+      expect(formatDate(dummyDate, dummyFormat)).toBe('formatted date');
+      expect(Datepicker.formatDate).toHaveBeenCalledTimes(1);
+      expect(Datepicker.formatDate).toHaveBeenCalledWith(dummyDate, dummyFormat);
+    });
+  });
+
+  describe('setFormValue', () => {
+    const dummyInternal = {
+      setFormValue: jest.fn(),
+    } as unknown as ElementInternals;
+
+    it('should set internal value with empty string', () => {
+      // @ts-ignore for test purpose
+      setFormValue(dummyInternal);
+      expect(dummyInternal.setFormValue).toHaveBeenCalledWith('');
+
+      // @ts-ignore for test purpose
+      setFormValue(dummyInternal, undefined);
+      expect(dummyInternal.setFormValue).toHaveBeenCalledWith('');
+
+      setFormValue(dummyInternal, null);
+      expect(dummyInternal.setFormValue).toHaveBeenCalledWith('');
+    });
+
+    it('should set internal value with string value', () => {
+      const dummyValue = 'dummy value';
+
+      setFormValue(dummyInternal, dummyValue);
+
+      expect(dummyInternal.setFormValue).toHaveBeenCalledWith(dummyValue);
+    });
+  });
+});

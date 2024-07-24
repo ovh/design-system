cfd9f45383351d1904595d1c7afa00fbc3ade75c
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-07-24 12:05:57 +0200

    fix(pagination): after rebase pagination event

--- packages/ods/src/components/pagination/tests/navigation/ods-pagination.e2e.ts
+++ packages/ods/src/components/pagination/tests/navigation/ods-pagination.e2e.ts
@@ -24,7 +24,7 @@ describe('ods-pagination navigation', () => {
   it('should emit event when current page changes', async() => {
     await setup('<ods-pagination default-current-page="2" total-pages="5"></ods-pagination>');
     const buttonList = await page.findAll('ods-pagination >>> li >>> ods-button');
-    const odsPaginationChanged = await el.spyOnEvent('odsPaginationChanged');
+    const odsChanged = await el.spyOnEvent('odsChanged');
 
     // There is the "previous" arrow that also counts as a button
     await buttonList[4].click();
@@ -36,8 +36,8 @@ describe('ods-pagination navigation', () => {
       oldCurrent: 2,
     };
 
-    expect(odsPaginationChanged).toHaveReceivedEventDetail(expected);
-    expect(odsPaginationChanged).toHaveReceivedEventTimes(1);
+    expect(odsChanged).toHaveReceivedEventDetail(expected);
+    expect(odsChanged).toHaveReceivedEventTimes(1);
   });
 
   it('should not allow to go to next page if current page is the last one', async() => {
@@ -57,7 +57,7 @@ describe('ods-pagination navigation', () => {
   it('should change items per page and emit event', async() => {
     await setup('<ods-pagination default-current-page="2" total-items="60"></ods-pagination>');
     const perPageSelectElement = await page.find('ods-pagination >>> ods-select');
-    const odsPaginationItemPerPageChanged = await el.spyOnEvent('odsPaginationItemPerPageChanged');
+    const odsItemPerPageChanged = await el.spyOnEvent('odsItemPerPageChanged');
     perPageSelectElement.setAttribute('value', '50');
     await page.waitForChanges();
 
@@ -67,7 +67,7 @@ describe('ods-pagination navigation', () => {
       totalPages: 2,
     };
 
-    expect(odsPaginationItemPerPageChanged).toHaveReceivedEventDetail(expected);
-    expect(odsPaginationItemPerPageChanged).toHaveReceivedEventTimes(1);
+    expect(odsItemPerPageChanged).toHaveReceivedEventDetail(expected);
+    expect(odsItemPerPageChanged).toHaveReceivedEventTimes(1);
   });
 });

bc643c32b7748c6880c9792e50ff35b78a4f7ab9
Leo Largillet <leo.largillet@gmail.com>
2024-07-25 17:49:38 +0200

    fix(badge): remove shape attribute

--- packages/ods/src/components/badge/src/index.ts
+++ packages/ods/src/components/badge/src/index.ts
@@ -1,4 +1,3 @@
 export { OdsBadge } from './components/ods-badge/ods-badge';
 export { ODS_BADGE_COLOR, ODS_BADGE_COLORS, type OdsBadgeColor } from './constants/badge-color';
-export { ODS_BADGE_SHAPE, ODS_BADGE_SHAPES, type OdsBadgeShape } from './constants/badge-shape';
 export { ODS_BADGE_SIZE, ODS_BADGE_SIZES, type OdsBadgeSize } from './constants/badge-size';

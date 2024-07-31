d863bdf08e95c5685cdc3fa9acf94f1a83480fe5
Francois Eoche <francois.eoche@ovhcloud.com>
2024-07-31 18:33:03 +0200

    docs(form-field): add examples for all form elements

--- packages/storybook/stories/components/form-field/form-field.stories.ts
+++ packages/storybook/stories/components/form-field/form-field.stories.ts
@@ -12,20 +12,13 @@ export default meta;
 
 export const Demo: StoryObj = {
   render: (arg) => html`
-  <ods-form-field class="demo" error="${arg.error}">
+  <ods-form-field error="${arg.error}">
     ${unsafeHTML(arg.label)}
     ${unsafeHTML(arg.visualHint)}
     ${unsafeHTML(arg.content)}
     ${unsafeHTML(arg.helper)}
   </ods-form-field>
-  <style>
-    .demo ods-text[slot="label"],
-    .demo ods-text[slot="helper"],
-    .demo ods-text[slot="visual-hint"] {
-      color: #4d5592;
-    }
-  </style>
-    `,
+  `,
   argTypes: orderControls({
     content: {
       table: {
@@ -83,25 +76,20 @@ export const Overview: StoryObj = {
   },
   render: () => html`
 <ods-form-field error="Error message">
-  <ods-text id="form-field__overview-label" slot="label" preset="label">
+  <ods-text slot="label" preset="label">
     Description
   </ods-text>
 
-  <ods-input id="form-field__overview-input" type="text" placeholder="Placeholder" clearable></ods-input>
+  <ods-textarea placeholder="Placeholder" is-resizable="true"></ods-textarea>
 
-  <ods-text id="form-field__overview-helper" slot="helper" preset="caption">
+  <ods-text slot="helper" preset="span">
     Helper text
   </ods-text>
+
+  <ods-text slot="visual-hint" preset="span">
+    0/200
+  </ods-text>
 </ods-form-field>
-<style>
-  #form-field__overview-label,
-  #form-field__overview-helper {
-    color: var(--ods-color-text);
-  }
-  #form-field__overview-helper::part(text) {
-    display: inline-flex;
-  }
-</style>
   `,
 };
 
@@ -116,7 +104,7 @@ export const Default: StoryObj = {
 export const Error: StoryObj = {
   tags: ['isHidden'],
   render: () => html`
-<ods-form-field error="Wrong format.">
+<ods-form-field error="Wrong format">
   <ods-text slot="label" preset="label">
     Description
   </ods-text>
@@ -225,19 +213,320 @@ export const Tooltip: StoryObj = {
   `,
 };
 
-export const Textarea: StoryObj = {
+export const Checkbox: StoryObj = {
   tags: ['isHidden'],
   render: () => html`
 <ods-form-field>
+  <div class="form-field__checkbox__field">
+    <ods-checkbox input-id="checkbox-terms" name="name"></ods-checkbox>
+
+    <label class="form-field__checkbox__field__label" for="checkbox-terms">
+      I agree to the terms and conditions
+    </label>
+  </div>
+</ods-form-field>
+
+<style>
+  .form-field__checkbox__field {
+    display: flex;
+    align-items: center;
+    gap: .5rem;
+  }
+
+  .form-field__checkbox__field__label {
+    color: var(--ods-color-text);
+  }
+</style>
+  `,
+};
+
+export const Datepicker: StoryObj = {
+  tags: ['isHidden'],
+  decorators: [(story) => html`<div style="height: 350px;">${story()}</div>`],
+  render: () => html`
+<ods-form-field>
+  <ods-text slot="label" preset="label">
+    Start date
+  </ods-text>
+
+  <ods-datepicker id="form-field-datepicker-field"></ods-datepicker>
+
+  <script>
+    (() => {
+      const datepicker = document.querySelector('#form-field-datepicker-field');
+      datepicker.value = new Date();
+    })();
+  </script>
+</ods-form-field>
+  `,
+};
+
+export const Input: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-form-field>
+  <ods-text slot="label" preset="label">
+    Name
+  </ods-text>
+
+  <ods-input placeholder="John"></ods-input>
+</ods-form-field>
+  `,
+};
+
+export const Password: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-form-field>
+  <ods-text slot="label" preset="label">
+    Password
+  </ods-text>
+
+  <ods-password placeholder="Password" is-clearable></ods-password>
+</ods-form-field>
+  `,
+};
+
+export const PhoneNumber: StoryObj = {
+  tags: ['isHidden'],
+  decorators: [(story) => html`<div style="height: 250px;">${story()}</div>`],
+  render: () => html`
+<ods-form-field>
+  <ods-text slot="label" preset="label">
+    Phone number
+  </ods-text>
+
+  <ods-phone-number countries="all" is-clearable></ods-phone-number>
+</ods-form-field>
+  `,
+};
+
+export const Quantity: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-form-field>
+  <ods-text slot="label" preset="label">
+    Number of CPUs
+  </ods-text>
+
+  <ods-quantity min="0" max="10" value="0"></ods-quantity>
+</ods-form-field>
+  `,
+};
+
+export const Radio: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-form-field>
+  <ods-text slot="label" preset="label">
+    Output file format
+  </ods-text>
+
+  <div class="form-field__radio__field">
+    <ods-radio input-id="radio-text-format" name="file-format" value="text-format" is-checked></ods-radio>
+
+    <label class="form-field__radio__field__label" for="radio-text-format">
+      Text
+    </label>
+  </div>
+
+  <div class="form-field__radio__field">
+    <ods-radio input-id="radio-csv-format" name="file-format" value="csv-format"></ods-radio>
+
+    <label class="form-field__radio__field__label" for="radio-csv-format">
+      CSV
+    </label>
+  </div>
+
+  <div class="form-field__radio__field">
+    <ods-radio input-id="radio-html-format" name="file-format" value="html-format"></ods-radio>
+
+    <label class="form-field__radio__field__label" for="radio-html-format">
+      HTML
+    </label>
+  </div>
+
+</ods-form-field>
+<style>
+  .form-field__radio__field {
+    display: flex;
+    align-items: center;
+    gap: .5rem;
+  }
+
+  .form-field__radio__field__label {
+    color: var(--ods-color-text);
+  }
+</style>
+  `,
+};
+
+export const Range: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-form-field>
+  <div slot="label">
+    <ods-text preset="label">
+      Price range
+    </ods-text>
+
+    <ods-text class="form-field__range__sub-label" preset="span">
+      Selected values : <span id="range-min-bound">0</span> - <span id="range-max-bound">0</span>€
+    </ods-text>
+  </div>
+
+  <ods-range step="10" id="form-field-range-field"></ods-range>
+
+  <script>
+    (() => {
+      const rangeDualStep = document.querySelector('#form-field-range-field');
+      const minBound = document.querySelector('#range-min-bound');
+      const maxBound = document.querySelector('#range-max-bound');
+
+      const updateBounds = () => {
+        minBound.textContent = rangeDualStep.value[0];
+        maxBound.textContent = rangeDualStep.value[1];
+      };
+
+      rangeDualStep.value = [30, 70];
+      updateBounds();
+
+      rangeDualStep.addEventListener('odsChange', updateBounds);
+    })()
+  </script>
+</ods-form-field>
+
+<style>
+  .form-field__range__sub-label {
+    display: block;
+  }
+
+  .form-field__range__sub-label::part(text) {
+    color: var(--ods-color-text);
+    font-size: 12px;
+  }
+</style>
+  `,
+};
+
+export const Select: StoryObj = {
+  tags: ['isHidden'],
+  decorators: [(story) => html`<div style="height: 150px;">${story()}</div>`],
+  render: () => html`
+<ods-form-field>
+  <ods-text slot="label" preset="label">
+    Select a Web hosting
+  </ods-text>
+
+  <ods-select class="form-field__select__field" placeholder="Select">
+    <option value="hosting-1">1 vCore 2,4 GHz, 2 Go RAM</option>
+    <option value="hosting-2">1 vCore 2,4 GHz, 4 Go RAM</option>
+    <option value="hosting-3">2 vCores 2,4 GHz, 8 Go RAM</option>
+  </ods-select>
+</ods-form-field>
+
+<style>
+  .form-field__select__field {
+    width: 250px;
+  }
+</style>
+  `,
+};
+
+export const Switch: StoryObj = {
+  tags: ['isHidden'],
+  decorators: [(story) => html`<div style="height: 150px;">${story()}</div>`],
+  render: () => html`
+<ods-form-field>
+  <ods-text slot="label" preset="label">
+    Preferred Operating System
+  </ods-text>
+
+  <ods-switch>
+    <ods-switch-item value="windows" is-checked>Windows</ods-switch-item>
+    <ods-switch-item value="macos">macOS</ods-switch-item>
+    <ods-switch-item value="linux">Linux</ods-switch-item>
+  </ods-switch>
+</ods-form-field>
+  `,
+};
+
+export const Textarea: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-form-field id="form-field-textarea">
   <ods-text slot="label" preset="label">
     Description
   </ods-text>
 
   <ods-text slot="visual-hint" preset="span">
-    0/200
+    <span id="textarea-character-count">0</span>/<span id="textarea-max-characters">0</span>
   </ods-text>
 
-  <ods-textarea placeholder="Placeholder" is-resizable="true"></ods-textarea>
+  <ods-textarea id="form-field-textarea-field" placeholder="Placeholder" is-resizable="true"></ods-textarea>
+</ods-form-field>
+
+<script>
+  (() => {
+    const MAX_CHARACTERS = 200;
+    const formFieldElement = document.querySelector('#form-field-textarea');
+    const textareaElement = document.querySelector('#form-field-textarea-field');
+    const characterCountElement = document.querySelector('#textarea-character-count');
+    const maxCharactersElement = document.querySelector('#textarea-max-characters');
+    let characterCount = 0;
+
+    characterCountElement.textContent = characterCount.toString();
+    maxCharactersElement.textContent = MAX_CHARACTERS.toString();
+
+    const updateCharacterCount = (event) => {
+      characterCount = event.target.value.length;
+      characterCountElement.textContent = characterCount.toString();
+
+      if (characterCount > MAX_CHARACTERS) {
+        textareaElement.hasError = true;
+        formFieldElement.error = "Max number of characters exceeded";
+      } else {
+        textareaElement.removeAttribute("has-error");
+        formFieldElement.removeAttribute("error");
+      }
+    };
+
+    textareaElement.addEventListener('odsChange', updateCharacterCount);
+  })();
+</script>
+  `,
+};
+
+export const Timepicker: StoryObj = {
+  tags: ['isHidden'],
+  decorators: [(story) => html`<div style="height: 250px;">${story()}</div>`],
+  render: () => html`
+<ods-form-field>
+  <ods-text slot="label" preset="label">
+    Starting time
+  </ods-text>
+
+  <ods-timepicker id="form-field-timepicker-field" timezones="all" with-seconds></ods-timepicker>
+
+  <script>
+    (() => {
+      const timepicker = document.querySelector('#form-field-timepicker-field');
+      timepicker.value = new Date().toLocaleTimeString('fr-FR');
+    })();
+  </script>
+</ods-form-field>
+  `,
+};
+
+export const Toggle: StoryObj = {
+  tags: ['isHidden'],
+  render: () => html`
+<ods-form-field>
+  <ods-text slot="label" preset="label">
+    Multi-AZ mode
+  </ods-text>
+
+  <ods-toggle with-label></ods-toggle>
 </ods-form-field>
   `,
 };

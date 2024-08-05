41d565f1cb45da6de36909f9b1f0f27dd80232d8
Damien Pellier <dpellier@gmail.com>
2024-08-05 11:34:13 +0200

    fix(timepicker): set internals value on change

--- packages/examples/react-webpack/src/app/App.tsx
+++ packages/examples/react-webpack/src/app/App.tsx
@@ -1,17 +1,17 @@
 import React, { type ReactElement } from 'react';
 // import { Gallery } from './components/gallery/Gallery';
-// import { FormFormik } from './components/formFormik/FormFormik';
+import { FormFormik } from './components/formFormik/FormFormik';
 // import { FormNative } from './components/formNative/FormNative';
-import { TestModal } from './components/testModal/TestModal';
+// import { TestModal } from './components/testModal/TestModal';
 import styles from './app.scss';
 
 function App(): ReactElement {
   return (
     <div className={ styles.app }>
       {/* <Gallery /> */}
-      {/*<FormFormik />*/}
+      <FormFormik />
       {/*<FormNative />*/}
-      <TestModal />
+      {/*<TestModal />*/}
     </div>
   );
 }

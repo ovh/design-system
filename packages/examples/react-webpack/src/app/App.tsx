afc4f26a63061c8c8978ae042c5b7bd3577375fa
Damien Pellier <dpellier@gmail.com>
2024-08-05 11:36:33 +0200

    fix(doc): use toggle control for timezones demo

--- packages/examples/react-webpack/src/app/App.tsx
+++ packages/examples/react-webpack/src/app/App.tsx
@@ -1,7 +1,7 @@
 import React, { type ReactElement } from 'react';
 // import { Gallery } from './components/gallery/Gallery';
-import { FormFormik } from './components/formFormik/FormFormik';
-// import { FormNative } from './components/formNative/FormNative';
+// import { FormFormik } from './components/formFormik/FormFormik';
+import { FormNative } from './components/formNative/FormNative';
 // import { TestModal } from './components/testModal/TestModal';
 import styles from './app.scss';
 
@@ -9,8 +9,8 @@ function App(): ReactElement {
   return (
     <div className={ styles.app }>
       {/* <Gallery /> */}
-      <FormFormik />
-      {/*<FormNative />*/}
+      {/*<FormFormik />*/}
+      <FormNative />
       {/*<TestModal />*/}
     </div>
   );

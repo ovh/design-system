8104373793044fef92f0f457e3de51f65ae686d6
Damien Pellier <dpellier@gmail.com>
2024-08-29 10:57:21 +0200

    feat(build): add gh-pages script

---
+++ scripts/generate-gh-pages.mjs
@@ -0,0 +1,61 @@
+#!/usr/bin/env zx
+/**
+ * execution from monorepo:
+ * `npx zx scripts/generate-gh-pages.mjs`
+ */
+import { resolve } from 'path';
+import { tmpdir } from 'os';
+
+const currentVersion = require('../lerna.json').version;
+
+const packageName = '@ovhcloud/ods-storybook';
+const tmpDirName = 'ods-gh-pages';
+const outDirName = 'docs';
+
+(async () => {
+
+  await $`rm -rf dist`;
+  await $`mkdir -p dist`;
+  await $`echo "Documentation generated for version ${currentVersion} at ${new Date().toISOString()}" > dist/status.html`;
+
+  // clean tmp specific dir
+  const tmpOdsDir = resolve(tmpdir(), `${tmpDirName}`);
+  await $`rm -rf ${tmpOdsDir}/*`;
+
+  let versions = await $`npm view ${packageName} versions --json`;
+  versions = JSON.parse(versions);
+
+  for (let versionsKey in versions) {
+    const version = versions[versionsKey];
+    // create a dir for this version
+    const dir = resolve(tmpOdsDir, `${version}`);
+    await $`mkdir -p ${dir}`;
+    await $`ls -l ${dir}`;
+
+    // find the url and download
+    let tarball = await $`npm view ${packageName}@${version} dist.tarball`;
+    tarball = `${tarball.stdout.trim()}`;
+    const command = `curl -sS "${tarball}" | tar -xzf - -C ${dir} --strip 1`;
+    await $([command]);
+
+    try {
+      await $`mv ${dir}/dist dist/v${version}`;
+    } catch (e) {
+      console.error(`No dist dir found, ignoring the version`, e);
+    }
+  }
+  try {
+    // add the current build (released just done)
+    await $`cp -r packages/storybook/dist dist/v${currentVersion}`;
+    await $`ln -s v${currentVersion} dist/latest`;
+  } catch (e) {
+    console.error(`cannot add the current storybook build. ignore it`, e);
+  }
+  try {
+    // move all into out dir
+    await $`rm -rf ${outDirName}/v*`;
+    await $`mv dist/* ${outDirName}`;
+  } catch (e) {
+    console.error(`cannot move files into outDir.`, e);
+  }
+})();

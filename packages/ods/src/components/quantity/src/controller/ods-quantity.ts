1214318a6f4b6aeffdcd16291cc96dd8f407b73d
Damien Pellier <dpellier@gmail.com>
2024-09-10 10:30:58 +0200

    feat(textarea): add error state management

--- packages/ods/src/components/quantity/src/controller/ods-quantity.ts
+++ packages/ods/src/components/quantity/src/controller/ods-quantity.ts
@@ -1,17 +1,25 @@
-function isPlusButtonDisabled(isDisabled: boolean, isReadonly: boolean, value: number | null, max?: number): boolean {
-  return isDisabled || isReadonly || (max !== undefined && value !== null && max <= value);
-}
+import { setInternalsValidityFromOdsComponent } from '../../../../utils/dom';
+import { type OdsInput } from '../../../input/src';
 
 function isMinusButtonDisabled(isDisabled: boolean, isReadonly: boolean, value: number | null, min?: number): boolean {
   return isDisabled || isReadonly || (min !== undefined && value !== null && min >= value);
 }
 
-function setFormValue(internals: ElementInternals, value: number | null): void {
+function isPlusButtonDisabled(isDisabled: boolean, isReadonly: boolean, value: number | null, max?: number): boolean {
+  return isDisabled || isReadonly || (max !== undefined && value !== null && max <= value);
+}
+
+//function updateInternals(internals: ElementInternals, value: number | string | null, inputEl?: HTMLInputElement): void {
+async function updateInternals(internals: ElementInternals, value: number | string | null, inputEl?: HTMLElement & OdsInput): Promise<void> {
   internals.setFormValue(value?.toString() ?? '');
+
+  if (inputEl) {
+    await setInternalsValidityFromOdsComponent(inputEl, internals);
+  }
 }
 
 export {
-  isPlusButtonDisabled,
   isMinusButtonDisabled,
-  setFormValue,
+  isPlusButtonDisabled,
+  updateInternals,
 };

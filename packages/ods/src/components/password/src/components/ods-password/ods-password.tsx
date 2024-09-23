f8815ba86296c3d499aca8e9f512bed0dcf636fd
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-09-23 10:01:24 +0200

    feat(password): add form validity

--- packages/ods/src/components/password/src/components/ods-password/ods-password.tsx
+++ packages/ods/src/components/password/src/components/ods-password/ods-password.tsx
@@ -1,8 +1,10 @@
-import { AttachInternals, Component, Event, type EventEmitter, type FunctionalComponent, Host, Method, Prop, h } from '@stencil/core';
+import { AttachInternals, Component, Event, type EventEmitter, type FunctionalComponent, Host, Listen, Method, Prop, State, h } from '@stencil/core';
 import { submitFormOnEnter } from '../../../../../utils/dom';
 import { type OdsInput, type OdsInputChangeEvent } from '../../../../input/src';
-import { setFormValue } from '../../controller/ods-password';
+import { updateInternals } from '../../controller/ods-password';
 import { type OdsPasswordChangeEventDetail } from '../../interfaces/events';
+
+const VALUE_DEFAULT_VALUE = null;
 
 @Component({
   formAssociated: true,
@@ -13,9 +15,12 @@ import { type OdsPasswordChangeEventDetail } from '../../interfaces/events';
   tag: 'ods-password',
 })
 export class OdsPassword {
-  private odsInput?: OdsInput;
+  private odsInput?: HTMLElement & OdsInput;
+  private updateIsInvalid: boolean = false;
 
   @AttachInternals() private internals!: ElementInternals;
+
+  @State() isInvalid: boolean = false;
 
   @Prop({ reflect: true }) public ariaLabel: HTMLElement['ariaLabel'] = null;
   @Prop({ reflect: true }) public ariaLabelledby?: string;
@@ -30,7 +35,7 @@ export class OdsPassword {
   @Prop({ reflect: true }) public name!: string;
   @Prop({ reflect: true }) public pattern?: string;
   @Prop({ reflect: true }) public placeholder?: string;
-  @Prop({ mutable: true, reflect: true }) public value: string | null = null;
+  @Prop({ mutable: true, reflect: true }) public value: string | null = VALUE_DEFAULT_VALUE;
 
   @Event() odsBlur!: EventEmitter<void>;
   @Event() odsClear!: EventEmitter<void>;
@@ -39,14 +44,39 @@ export class OdsPassword {
   @Event() odsReset!: EventEmitter<void>;
   @Event() odsChange!: EventEmitter<OdsPasswordChangeEventDetail>;
 
-  @Method()
-  async clear(): Promise<void> {
-    return this.odsInput?.clear();
+  @Listen('invalid')
+  onInvalidEvent(event: Event): void {
+    // Remove the native validation message popup
+    event.preventDefault();
+
+    // Enforce the state here as we may still be in pristine state (if the form is submitted before any changes occurs)
+    this.isInvalid = true;
   }
 
   @Method()
-  async getValidity(): Promise<ValidityState | undefined> {
-    return this.odsInput?.getValidity();
+  async checkValidity(): Promise<boolean> {
+    return this.internals?.checkValidity();
+  }
+
+  @Method()
+  async clear(): Promise<void> {
+    await this.odsInput?.clear();
+    this.updateIsInvalid = true;
+  }
+
+  @Method()
+  async getValidationMessage(): Promise<string> {
+    return this.internals.validationMessage;
+  }
+
+  @Method()
+  async getValidity(): Promise<ValidityState> {
+    return this.internals.validity;
+  }
+
+  @Method()
+  async reportValidity(): Promise<boolean> {
+    return this.internals.reportValidity();
   }
 
   @Method()
@@ -56,34 +86,55 @@ export class OdsPassword {
 
   @Method()
   async reset(): Promise<void> {
-    return this.odsInput?.reset();
+    await this.odsInput?.reset();
+    this.updateIsInvalid = true;
+
+  }
+
+  @Method()
+  async willValidate(): Promise<boolean> {
+    return this.internals.willValidate;
   }
 
   componentWillLoad(): void {
-    if (!this.value) {
+    if (!this.value && (this.value !== VALUE_DEFAULT_VALUE || this.defaultValue)) {
       this.value = this.defaultValue ?? null;
     }
-    setFormValue(this.internals, this.value);
+  }
+
+  async componentDidLoad(): Promise<void> {
+    await updateInternals(this.internals, this.value, this.odsInput);
   }
 
   async formResetCallback(): Promise<void> {
     await this.reset();
   }
 
-  private onOdsChange(event: OdsInputChangeEvent): void {
+  private onBlur(): void {
+    this.isInvalid = !this.internals.validity.valid;
+  }
+
+  private async onOdsChange(event: OdsInputChangeEvent): Promise<void> {
     this.value = event.detail.value?.toString() ?? null;
-    setFormValue(this.internals, this.value);
+    await updateInternals(this.internals, this.value, this.odsInput);
+     // update here after update internals
+     if (this.updateIsInvalid) {
+      this.isInvalid = !this.internals.validity.valid;
+      this.updateIsInvalid = false;
+    }
   }
 
   render(): FunctionalComponent {
     return (
-      <Host class="ods-password">
+      <Host class="ods-password"
+        disabled={ this.isDisabled }
+        readonly={ this.isReadonly }>
         <ods-input
           ariaLabel={ this.ariaLabel }
           ariaLabelledby={ this.ariaLabelledby }
           defaultValue={ this.defaultValue }
           exportparts="input"
-          hasError={ this.hasError }
+          hasError={ this.hasError || this.isInvalid }
           isClearable={ this.isClearable }
           isDisabled={ this.isDisabled }
           isLoading={ this.isLoading }
@@ -91,12 +142,13 @@ export class OdsPassword {
           isReadonly={ this.isReadonly }
           isRequired={ this.isRequired }
           name={ this.name }
+          onOdsBlur={() => this.onBlur() }
           onKeyUp={ (event: KeyboardEvent): void => submitFormOnEnter(event, this.internals.form) }
           onOdsChange={ (event: OdsInputChangeEvent) => this.onOdsChange(event) }
           onOdsToggleMask={ () => this.isMasked = !this.isMasked }
           pattern={ this.pattern }
           placeholder={ this.placeholder }
-          ref={ (el?: unknown): OdsInput => this.odsInput = el as OdsInput }
+          ref={ (el?: unknown): OdsInput => this.odsInput = el as HTMLElement & OdsInput }
           value={ this.value }
         >
         </ods-input>

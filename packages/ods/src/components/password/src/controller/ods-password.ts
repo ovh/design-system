aa6a2cfb4adcb2582ca878aed2146e090ab89d73
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-09-23 10:01:24 +0200

    feat(password): add form validity

--- packages/ods/src/components/password/src/controller/ods-password.ts
+++ packages/ods/src/components/password/src/controller/ods-password.ts
@@ -1,7 +1,14 @@
-function setFormValue(internals: ElementInternals, value: number | string | null): void {
+import type { OdsInput } from '../../../input/src';
+import { setInternalsValidityFromOdsComponent } from '../../../../utils/dom';
+
+async function updateInternals(internals: ElementInternals, value: string | null, inputEl?: HTMLElement & OdsInput): Promise<void> {
   internals.setFormValue(value?.toString() ?? '');
+
+  if (inputEl) {
+    await setInternalsValidityFromOdsComponent(inputEl, internals);
+  }
 }
 
 export {
-  setFormValue,
+  updateInternals,
 };

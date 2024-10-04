073ec27b5dcd3219accaec294dc38ba426b512e3
Damien Pellier <dpellier@gmail.com>
2024-10-04 10:36:17 +0200

    refactor(doc): rewrite components stories

--- packages/storybook/src/components/identityCard/IdentityCard.tsx
+++ packages/storybook/src/components/identityCard/IdentityCard.tsx
@@ -6,19 +6,6 @@ import { ATOMIC_TYPE } from '../../constants/atomicDesign';
 import { StorybookLink } from '../storybookLink/StorybookLink';
 import styles from './identityCard.module.css';
 
-// TODO replace IdentityCard by IdentityCard2 and rename
-// TODO fix validateDOMNesting(...): <p> cannot appear as a descendant of <p>.
-
-type Props = {
-  aliases: string[],
-  atomicType: ATOMIC_TYPE,
-  className?: string;
-  figmaLink: string,
-  githubUrl: string,
-  name: string,
-  relatedComponents: { name: string, subtitle?: string }[],
-}
-
 function getAtomicTypeDocUrl(type: ATOMIC_TYPE): string {
   switch (type) {
     case ATOMIC_TYPE.atom:
@@ -34,107 +21,24 @@ function getAtomicTypeDocUrl(type: ATOMIC_TYPE): string {
   }
 }
 
-const IdentityCard = ({ aliases, atomicType, className, figmaLink, githubUrl, name, relatedComponents }: Props) => {
-  return (
-    <Table className={ className || '' }>
-      <tbody>
-        <tr>
-          <th scope="row">
-            Name
-          </th>
-
-          <td>
-            { name }
-          </td>
-        </tr>
-
-        <tr>
-          <th scope="row">
-            Also known as
-          </th>
-
-          <td>
-            { aliases.length > 0 ? aliases.join(', ') : '-' }
-          </td>
-        </tr>
-
-        <tr>
-          <th scope="row">
-            Atomic type
-          </th>
-
-          <td>
-            <OdsLink className={ styles['identity-card__atomic-link'] }
-                     href={ getAtomicTypeDocUrl(atomicType) }
-                     icon={ ODS_ICON_NAME.externalLink }
-                     label={ atomicType }
-                     target="_blank" />
-          </td>
-        </tr>
-
-        <tr>
-          <th scope="row">
-            Related component(s)
-          </th>
-
-          <td>
-            {
-              relatedComponents.length > 0 ?
-              relatedComponents.map((relatedComponent, idx) => (
-                <Fragment key={ idx }>
-                  <StorybookLink kind={ `ODS Components/${!!relatedComponent.subtitle ? `${relatedComponent.subtitle}/` : ''}${relatedComponent.name}` }
-                                 label={ relatedComponent.name }
-                                 story="Documentation" />
-
-                  { idx < (relatedComponents.length - 1) && <span>, </span> }
-                </Fragment>
-              )) : '-'
-            }
-          </td>
-        </tr>
-
-        <tr>
-          <th scope="row">
-            Links
-          </th>
-
-          <td>
-            <OdsLink className={ styles['identity-card__app-link'] }
-                     href={ figmaLink }
-                     icon={ ODS_ICON_NAME.externalLink }
-                     label="Design"
-                     target="_blank" />
-
-            <OdsLink className={ styles['identity-card__app-link'] }
-                     href={ githubUrl }
-                     icon={ ODS_ICON_NAME.externalLink }
-                     label="Github"
-                     target="_blank" />
-          </td>
-        </tr>
-      </tbody>
-    </Table>
-  );
-};
-
-type Prop2 = {
+type Prop = {
   aliases: string[],
   atomicType: ATOMIC_TYPE,
-  children: ReactNode,
+  children?: ReactNode,
   figmaLink: string,
   githubUrl: string,
   name: string,
   relatedComponents: { name: string, subtitle?: string }[],
 }
 
-const IdentityCard2 = ({ aliases, atomicType, children, figmaLink, githubUrl, name, relatedComponents }: Prop2) => {
+const IdentityCard = ({ aliases, atomicType, children, figmaLink, githubUrl, name, relatedComponents }: Prop) => {
   return (
-    <div className={ styles['identity-card2'] }>
-      <p>
-        { children }
-      </p>
+    <div className={ styles['identity-card'] }>
+      <div>
+        { children || '' }
+      </div>
 
-      <Table className={ styles['identity-card2__table'] }>
+      <Table className={ styles['identity-card__table'] }>
         <tbody>
         <tr>
           <th scope="row">
@@ -216,7 +120,4 @@ const IdentityCard2 = ({ aliases, atomicType, children, figmaLink, githubUrl, na
   );
 };
 
-export {
-  IdentityCard,
-  IdentityCard2,
-};
+export { IdentityCard };

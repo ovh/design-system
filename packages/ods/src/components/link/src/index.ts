63e810dba847b4a0ea8d89c8cbc36eea1933e4ba
astagnol <a.stagnol@gmail.com>
2024-10-07 12:25:48 +0200

    feat(link): can display an icon on the left or right
    Signed-off-by: astagnol <a.stagnol@gmail.com>

--- packages/ods/src/components/link/src/index.ts
+++ packages/ods/src/components/link/src/index.ts
@@ -1,2 +1,3 @@
 export { OdsLink } from './components/ods-link/ods-link';
 export { ODS_LINK_COLOR, ODS_LINK_COLORS, type OdsLinkColor } from './constant/link-color';
+export { ODS_LINK_ICON_ALIGNMENT, ODS_LINK_ICON_ALIGNMENTS, type OdsLinkIconAlignment } from './constant/link-icon-alignment';

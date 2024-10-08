4f8f17fb1437bec9ea435f0c253cd836da43529a
Damien Pellier <dpellier@gmail.com>
2024-10-08 15:10:11 +0200

    chore(pagination): explain enum diff with other components

--- packages/ods/src/components/pagination/src/constants/pagination-per-page.ts
+++ packages/ods/src/components/pagination/src/constants/pagination-per-page.ts
@@ -6,6 +6,10 @@ enum ODS_PAGINATION_PER_PAGE {
   option_300 = 300,
 }
 
+// ODS_PAGINATION_PER_PAGE is a numeric enum, so we don't need to expose a literal type like:
+// type OdsPaginationPerPage =`${ODS_PAGINATION_PER_PAGE}`;
+// This still allows user to set the Prop value explicitly instead of using the enum value so it is fine.
+
 const ODS_PAGINATION_PER_PAGE_OPTIONS = Object.freeze([
   ODS_PAGINATION_PER_PAGE.option_10,
   ODS_PAGINATION_PER_PAGE.option_25,

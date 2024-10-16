db5d87a8ff74bf2c3303f48150b9f05bf4a24cc5
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-10-16 17:16:53 +0200

    docs: add description to validity state controls

--- packages/storybook/stories/components/phone-number/phone-number.stories.ts
+++ packages/storybook/stories/components/phone-number/phone-number.stories.ts
@@ -182,6 +182,7 @@ export const Demo: StoryObj = {
         type: { summary: 'boolean' },
       },
       control: 'boolean',
+      description: 'Toggle this to see the component validityState',
     },
   }),
   args: {

83c44be3653dcedbc25fef82a9a9871d57da08a1
Damien Pellier <dpellier@gmail.com>
2024-10-29 17:07:54 +0100

    feat(datepicker): add tests about odsChange emit on first render

--- packages/ods/tests/utils/type.spec.ts
+++ packages/ods/tests/utils/type.spec.ts
@@ -1,6 +1,30 @@
-import { isNumeric } from '../../src/utils/type';
+import { isDate, isNumeric } from '../../src/utils/type';
 
 describe('utils type', () => {
+  describe('isDate', () => {
+    it('should return false if the value is not a date', () => {
+      // @ts-ignore for test purpose
+      expect(isDate()).toBe(false);
+      // @ts-ignore for test purpose
+      expect(isDate(null)).toBe(false);
+      // @ts-ignore for test purpose
+      expect(isDate(NaN)).toBe(false);
+      // @ts-ignore for test purpose
+      expect(isDate('str')).toBe(false);
+      // @ts-ignore for test purpose
+      expect(isDate(false)).toBe(false);
+      // @ts-ignore for test purpose
+      expect(isDate(true)).toBe(false);
+      // @ts-ignore for test purpose
+      expect(isDate({ dummy: 'object' })).toBe(false);
+    });
+
+    it('should return true if the value is a date', () => {
+      expect(isDate(new Date())).toBe(true);
+      expect(isDate(new Date(0))).toBe(true);
+    });
+  });
+
   describe('isNumeric', () => {
     it('should return false if the value is not a number', () => {
       expect(isNumeric()).toBe(false);

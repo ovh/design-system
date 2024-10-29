bcff8c862c0da60a05cbe7e687cbf9c54be1f748
Damien Pellier <dpellier@gmail.com>
2024-10-29 17:07:54 +0100

    feat(datepicker): add tests about odsChange emit on first render

--- packages/ods/src/utils/type.ts
+++ packages/ods/src/utils/type.ts
@@ -1,7 +1,13 @@
+function isDate(date: Date): boolean {
+  // Needed as values from runtime are not TS problem anymore
+  return date instanceof Date && !isNaN(date.valueOf());
+}
+
 function isNumeric(n?: string | number | null): boolean {
   return n === 0 || !!n && !isNaN(parseFloat(n.toString())) && isFinite(n as number);
 }
 
 export {
+  isDate,
   isNumeric,
 };

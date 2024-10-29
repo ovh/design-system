e06fedf11f714e12917502c275f84cf36911b645
Damien Pellier <dpellier@gmail.com>
2024-10-29 17:07:54 +0100

    feat(datepicker): add tests about odsChange emit on first render

--- packages/ods/src/components/datepicker/tests/controller/ods-datepicker.spec.ts
+++ packages/ods/src/components/datepicker/tests/controller/ods-datepicker.spec.ts
@@ -1,14 +1,35 @@
 jest.mock('vanillajs-datepicker', () => ({
   Datepicker: {
     formatDate: jest.fn().mockReturnValue('formatted date'),
+    parseDate: jest.fn().mockReturnValue('2000-01-02'),
   },
 }));
+jest.mock('../../../../utils/dom');
 
 import { Datepicker } from 'vanillajs-datepicker';
-import { formatDate, updateInternals } from '../../src/controller/ods-datepicker';
+import { setInternalsValidityFromHtmlElement } from '../../../../utils/dom';
+import { formatDate, getInitialValue, updateInternals } from '../../src/controller/ods-datepicker';
 
 describe('ods-datepicker controller', () => {
   beforeEach(jest.clearAllMocks);
+
+  describe('getInitialValue', () => {
+    const dummyFormat = 'dummy format';
+
+    it('should return null if value is null and no default value', () => {
+      expect(getInitialValue(null, dummyFormat)).toBe(null);
+    });
+
+    it('should return date if value is set regarding of default value', () => {
+      expect(getInitialValue(new Date(0), dummyFormat)).toEqual(new Date(0));
+      expect(getInitialValue(new Date(0), dummyFormat, '2000-01-02')).toEqual(new Date(0));
+    });
+
+    it('should return default value if value is null', () => {
+      expect(getInitialValue(null, dummyFormat, 'default')).toEqual(new Date('2000-01-02'));
+      expect(Datepicker.parseDate).toHaveBeenCalledWith('default', dummyFormat);
+    });
+  });
 
   describe('formatDate', () => {
     const dummyFormat = 'dd/mm/yyyy';
@@ -40,6 +61,7 @@ describe('ods-datepicker controller', () => {
   });
 
   describe('updateInternals', () => {
+    const dummyInput = { dummy: 'input' };
     const dummyInternal = {
       setFormValue: jest.fn(),
     } as unknown as ElementInternals;
@@ -63,6 +85,18 @@ describe('ods-datepicker controller', () => {
       updateInternals(dummyInternal, dummyValue);
 
       expect(dummyInternal.setFormValue).toHaveBeenCalledWith(dummyValue);
+    });
+
+    it('should not set internal validity if no input element is defined', () => {
+      updateInternals(dummyInternal, 'dummyValue');
+
+      expect(setInternalsValidityFromHtmlElement).not.toHaveBeenCalled();
+    });
+
+    it('should set internal validity if input element is defined', () => {
+      updateInternals(dummyInternal, 'dummyValue', dummyInput as unknown as HTMLInputElement);
+
+      expect(setInternalsValidityFromHtmlElement).toHaveBeenCalledWith(dummyInput, dummyInternal);
     });
   });
 });

8643c4249dca7acde78d4575f37054dc15967a04
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-11-04 15:33:37 +0100

    feat(range): add validity state

--- packages/ods/src/components/range/src/controller/ods-range.ts
+++ packages/ods/src/components/range/src/controller/ods-range.ts
@@ -1,16 +1,32 @@
-function getDefaultValue(min: number, max: number, defaultValue?: number | [number, number]): number | [number, number] {
-  if (defaultValue) {
+import { isNumeric } from '../../../../utils/type';
+
+const VALUE_DEFAULT_VALUE = null;
+type RangeValue = number | [number, number] | null | [null, null];
+
+function getInitialValue(value: RangeValue, min: number, max: number, defaultValue?: number | [number, number]): RangeValue {
+  if (defaultValue !== undefined && value === VALUE_DEFAULT_VALUE) {
     return defaultValue;
   }
-  return max < min ? min : min + (max - min) / 2;
+  return value ?? (max < min ? min : min + (max - min) / 2);
 }
 
-function isDualRange(value: number | [number, number] | null): value is [number, number] {
-  return Array.isArray(value) && value.length === 2 && value.every((v) => typeof v === 'number');
+function isDualRange(value: RangeValue): value is [number, number] | [null, null] {
+  return Array.isArray(value) && value.length === 2 && value.every((v) => typeof v === 'number' || v === null);
 }
 
-function setFormValue(internals: ElementInternals, value: number | [number, number] | null): void {
+function updateInternals(internals: ElementInternals, value: RangeValue, isRequired: boolean): void {
   internals.setFormValue(value?.toString() ?? '');
+
+  const isNotArrayOfNumber = Array.isArray(value) && value.every((v) => !isNumeric(v));
+  if (Number.isNaN(value) || isNotArrayOfNumber) {
+    return internals.setValidity({ customError: true }, 'Type not supported');
+  }
+
+  const isArrayOfNull = Array.isArray(value) && value?.[0] === VALUE_DEFAULT_VALUE && value?.[1] === VALUE_DEFAULT_VALUE;
+  if (isRequired && (value === VALUE_DEFAULT_VALUE || isArrayOfNull)) {
+    return internals.setValidity({ customError: true }, 'Missing value');
+  }
+  internals.setValidity({});
 }
 
 function toPercentage(max: number, min: number, value?: number): number {
@@ -18,8 +34,9 @@ function toPercentage(max: number, min: number, value?: number): number {
 }
 
 export {
-  getDefaultValue,
+  getInitialValue,
   isDualRange,
   toPercentage,
-  setFormValue,
+  updateInternals,
+  VALUE_DEFAULT_VALUE,
 };

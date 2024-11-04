1c4f884ec6226761ebf7f61d3f0671c6f2037901
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-11-04 15:33:37 +0100

    feat(range): add validity state

--- packages/ods/src/components/range/tests/rendering/ods-range.e2e.ts
+++ packages/ods/src/components/range/tests/rendering/ods-range.e2e.ts
@@ -5,6 +5,12 @@ describe('ods-range rendering', () => {
   let el: E2EElement;
   let page: E2EPage;
 
+  async function isInErrorState(): Promise<boolean | undefined> {
+    return page.evaluate(() => {
+      return document.querySelector('ods-range')?.shadowRoot?.querySelector('.ods-range__range')?.classList.contains('ods-range__range--error');
+    });
+  }
+
   async function setup(content: string, customStyle?: string): Promise<void> {
     page = await newE2EPage();
 
@@ -36,6 +42,49 @@ describe('ods-range rendering', () => {
       await setup('<ods-range min="0" max="50"></ods-range>');
 
       expect(await el.getProperty('value')).toBe(25);
+    });
+  });
+
+  describe('error state', () => {
+    it('should not render in error on form submit, before of native default value', async() => {
+      await setup('<form method="get" onsubmit="return false"><ods-range is-required></ods-range></form>');
+
+      await page.evaluate(() => {
+        document.querySelector<HTMLFormElement>('form')?.requestSubmit();
+      });
+      await page.waitForChanges();
+
+      expect(await isInErrorState()).toBe(false);
+    });
+
+    it('should toggle the error state on value change', async() => {
+      await setup('<form method="get" onsubmit="return false"><ods-range is-required></ods-range></form>');
+
+      await el.click();
+      await page.waitForChanges();
+
+      expect(await isInErrorState()).toBe(false);
+
+      await el.callMethod('clear');
+      await page.click('body', { offset: { x: 200, y: 200 } }); // Blur
+      await page.waitForChanges();
+
+      await page.waitForChanges();
+      expect(await isInErrorState()).toBe(true);
+    });
+
+    it('should enforce the error state if has-error is set even on valid range', async() => {
+      await setup('<form method="get" onsubmit="return false"><ods-range is-required has-error value="dummy"></ods-range></form>');
+      await page.waitForChanges();
+
+      expect(await isInErrorState()).toBe(true);
+
+      await page.evaluate(() => {
+        document.querySelector<HTMLFormElement>('form')?.requestSubmit();
+      });
+      await page.waitForChanges();
+
+      expect(await isInErrorState()).toBe(true);
     });
   });
 });

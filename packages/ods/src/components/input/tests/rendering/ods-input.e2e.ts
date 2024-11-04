c24f7104c29fd112f26a96977c72ac6d836d6537
Damien Pellier <dpellier@gmail.com>
2024-11-04 11:19:14 +0100

    feat(input): add native datalist feature

--- packages/ods/src/components/input/tests/rendering/ods-input.e2e.ts
+++ packages/ods/src/components/input/tests/rendering/ods-input.e2e.ts
@@ -10,7 +10,7 @@ describe('ods-input rendering', () => {
     page = await newE2EPage();
 
     await page.setContent(content);
-    await page.evaluate(() => document.body.style.setProperty('margin', '0px'));
+    await page.evaluate(() => document.body.style.setProperty('margin', '0'));
 
     if (customStyle) {
       await page.addStyleTag({ content: customStyle });
@@ -30,12 +30,13 @@ describe('ods-input rendering', () => {
   describe('part', () => {
     it('should render with custom style applied', async() => {
       await setup('<ods-input></ods-input>', 'ods-input::part(input) { width: 100px }');
+
       const partStyle = await part.getComputedStyle();
       expect(partStyle.getPropertyValue('width')).toBe('100px');
     });
   });
 
-  describe('Form', () => {
+  describe('form', () => {
     it('should get form data with button type submit', async() => {
       await setup(`<form method="get">
         <ods-input name="odsInput" value="On Vous Heberge ?"></ods-input>
@@ -44,8 +45,10 @@ describe('ods-input rendering', () => {
         <button type="submit">Submit</button>
       </form>`);
       const submitButton = await page.find('button[type="submit"]');
+
       await submitButton.click();
       await page.waitForNetworkIdle();
+
       const url = new URL(page.url());
       expect(url.searchParams.get('odsInput')).toBe('On Vous Heberge ?');
       expect(url.searchParams.get('natifInput')).toBe('');
@@ -58,15 +61,38 @@ describe('ods-input rendering', () => {
         <button type="reset">Reset</button>
         <button type="submit">Submit</button>
       </form>`);
+
       const resetButton = await page.find('button[type="reset"]');
       await resetButton.click();
 
       const submitButton = await page.find('button[type="submit"]');
       await submitButton.click();
       await page.waitForNetworkIdle();
+
       const url = new URL(page.url());
       expect(url.searchParams.get('natifInput')).toBe('');
       expect(url.searchParams.get('odsInput')).toBe('');
+    });
+  });
+
+  describe('slots', () => {
+    describe('list', () => {
+      it('should not render with datalist nodes if not list prop is set', async() => {
+        await setup('<ods-input><datalist slot="list"><option value="1"></option><option value="2"></option></datalist></ods-input>');
+
+        const innerDatalist = await page.find('ods-input >>> datalist');
+        expect(innerDatalist).toBeNull();
+      });
+
+      it('should render with datalist nodes if list prop is set', async() => {
+        await setup('<ods-input list="my-list"><datalist slot="list"><option value="1"></option><option value="2"></option></datalist></ods-input>');
+
+        const innerDatalist = await page.find('ods-input >>> datalist');
+        expect(innerDatalist).not.toBeNull();
+
+        const innerDatalistOptions = await innerDatalist.findAll('option');
+        expect(innerDatalistOptions.length).toBe(2);
+      });
     });
   });
 });

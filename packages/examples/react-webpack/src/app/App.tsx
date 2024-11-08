a5c7850e6e93f6ee5caf6d744ebd83c808d4ffac
Damien Pellier <dpellier@gmail.com>
2024-11-08 16:48:23 +0100

    chore(examples): update react forms with current QA results

--- packages/examples/react-webpack/src/app/App.tsx
+++ packages/examples/react-webpack/src/app/App.tsx
@@ -1,23 +1,23 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React, { type ReactElement } from 'react';
 import styles from './app.scss';
 // import { Gallery } from './components/gallery/Gallery';
 // import { FormFormik } from './components/formFormik/FormFormik';
-// import { FormNative } from './components/formNative/FormNative';
+import { FormNative } from './components/formNative/FormNative';
 // import { TestModal } from './components/testModal/TestModal';
-import { TestSelect } from './components/testSelect/TestSelect';
+// import { TestSelect } from './components/testSelect/TestSelect';
 
-const queryClient = new QueryClient();
+// const queryClient = new QueryClient();
 
 function App(): ReactElement {
   return (
     <div className={ styles.app }>
       {/* <Gallery /> */}
       {/*<FormFormik />*/}
-      {/* <FormNative /> */}
-      <QueryClientProvider client={queryClient}>
-        <TestSelect />
-      </QueryClientProvider>
+       <FormNative />
+      {/*<QueryClientProvider client={queryClient}>*/}
+      {/*  <TestSelect />*/}
+      {/*</QueryClientProvider>*/}
     </div>
   );
 }

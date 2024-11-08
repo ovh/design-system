3756c0d05cb6db060762b61ff75e1dbe6aacda30
Damien Pellier <dpellier@gmail.com>
2024-11-08 16:48:23 +0100

    chore(examples): update react forms with current QA results

--- packages/examples/react-webpack/src/app/components/formNative/FormNative.tsx
+++ packages/examples/react-webpack/src/app/components/formNative/FormNative.tsx
@@ -6,7 +6,6 @@ import styles from './formNative.scss';
 function FormNative(): ReactElement {
   const formRef = useRef<HTMLFormElement>(null);
   const [areAllRequired, setAreAllRequired] = useState(false);
-  const [withDefaultValue, setWithDefaultValue] = useState(false);
 
   async function onSubmit(e: any) {
     e.preventDefault();
@@ -25,10 +24,6 @@ function FormNative(): ReactElement {
     setAreAllRequired(() => !areAllRequired);
   }
 
-  function onWithDefaultValueToggle() {
-    setWithDefaultValue(() => !withDefaultValue);
-  }
-
   return (
     <form
       className={ styles['form-native'] }
@@ -40,23 +35,17 @@ function FormNative(): ReactElement {
                 type="button">
           Toggle All Required (broken)
         </button>
-
-        <button onClick={ onWithDefaultValueToggle }
-                type="button">
-          Toggle Default value
-        </button>
       </div>
 
       <p>
         Current configuration:
         <br />
         - All fields required: {areAllRequired.toString()}
-        <br />
-        - All fields have default value: {withDefaultValue.toString()}
       </p>
 
+      {/* KO no odsChange event on mount */}
+      {/* KO no odsChangeEvent on reset */}
       <div>
-        {/* OKish no validity method but required is managed by browser directly */}
         <OdsCheckbox
           isRequired={ areAllRequired }
           inputId="checkbox"
@@ -74,48 +63,33 @@ function FormNative(): ReactElement {
         <label htmlFor="checkbox2">Checked 2</label>
       </div>
 
-      {/*/!* KO? reset to "" instead of null *!/*/}
-      {/*<OdsDatepicker*/}
-      {/*  // @ts-ignore IDE try to match another react specific attribute*/}
-      {/*  defaultValue={ new Date() }*/}
-      {/*  hasError={ error.datepicker }*/}
-      {/*  isClearable={ true }*/}
-      {/*  isRequired={ true }*/}
-      {/*  name="datepicker"*/}
-      {/*  onOdsChange={ () => validateField(datepickerRef.current) }*/}
-      {/*  ref={ datepickerRef }*/}
-      {/*/>*/}
+      <OdsDatepicker
+        isClearable={ true }
+        isRequired={ areAllRequired }
+        name="datepicker"
+      />
 
-      {/*/!* KO? reset to "" instead of null *!/*/}
       <OdsInput
-        defaultValue={ withDefaultValue ? 23 : undefined }
         isClearable={ true }
         isRequired={ areAllRequired }
         name="inputNumber"
         type={ ODS_INPUT_TYPE.number }
       />
 
-      {/*/!* OK *!/*/}
       <OdsInput
-        defaultValue={ withDefaultValue ? 'input text' : undefined }
         isClearable={ true }
         isRequired={ areAllRequired }
         name="inputText"
         type={ ODS_INPUT_TYPE.text }
       />
 
-      {/*/!* KO style width different *!/*/}
       <OdsPassword
-        defaultValue={ withDefaultValue ? 'pass' : undefined }
         isClearable={ true }
         isRequired={ areAllRequired }
         name="password"
       />
 
-      {/*/!* KO default value not in formData on immediate submit *!/*/}
-      {/*/!* KO style width different *!/*/}
       <OdsPhoneNumber
-        defaultValue={ withDefaultValue ? '+33123456789' : undefined }
         isClearable={ true }
         isRequired={ areAllRequired }
         isoCode="fr"
@@ -124,21 +98,18 @@ function FormNative(): ReactElement {
 
       <OdsPhoneNumber
         countries="all"
-        defaultValue={ withDefaultValue ? '+33123456789' : undefined }
         isClearable={ true }
         isRequired={ areAllRequired }
         isoCode="fr"
-        name="phoneNumber"
+        name="phoneNumberWithCountries"
       />
 
-      {/*/!* KO reset does not reset formData if no default value *!/*/}
+      {/* KO two odsChangeEvent on change */}
       <OdsQuantity
-        defaultValue={ withDefaultValue ? 22 : undefined }
         isRequired={ areAllRequired }
         name="quantity"
       />
 
-      {/* OKish no validity method but required is managed by browser directly */}
       <div>
         <OdsRadio
           isRequired={ areAllRequired }
@@ -157,68 +128,56 @@ function FormNative(): ReactElement {
         <label htmlFor="radio2">Radio 2</label>
       </div>
 
-      {/*/!* KO required does not return valid: false on empty select *!/*/}
-      {/*/!* KO reset return empty string instead of null *!/*/}
-      {/*<OdsSelect*/}
-      {/*  defaultValue="dog"*/}
-      {/*  hasError={ error.select }*/}
-      {/*  isRequired={ true }*/}
-      {/*  name="select"*/}
-      {/*  onOdsChange={ (event: any) => onChange(event) }*/}
-      {/*  onOdsReset={ (event: any) => onReset(event) }*/}
-      {/*  onOdsClear={ (event: any) => onClear(event) }*/}
-      {/*  ref={ selectRef }*/}
-      {/*>*/}
-      {/*  <option value="dog">Dog</option>*/}
-      {/*  <option value="cat">Cat</option>*/}
-      {/*  <option value="hamster">Hamster</option>*/}
-      {/*  <option value="parrot">Parrot</option>*/}
-      {/*  <option value="spider">Spider</option>*/}
-      {/*  <option value="goldfish">Goldfish</option>*/}
-      {/*</OdsSelect>*/}
+      <OdsSelect
+        isRequired={ areAllRequired }
+        name="select"
+      >
+        <option value="dog">Dog</option>
+        <option value="cat">Cat</option>
+        <option value="hamster">Hamster</option>
+        <option value="parrot">Parrot</option>
+        <option value="spider">Spider</option>
+        <option value="goldfish">Goldfish</option>
+      </OdsSelect>
 
-      {/*<button onClick={ () => checkboxRef.current?.clear() }>Clear</button>*/}
+      {/* KO required not reactive */}
+      {/* KO no odsChange event on mount ??? => same radio */}
+      {/* KO Two event change on reset */}
+      <OdsSwitch
+        isRequired={ areAllRequired }
+        name="switch"
+      >
+        <OdsSwitchItem
+          value="option1"
+        >
+          Option 1
+        </OdsSwitchItem>
+        <OdsSwitchItem
+          value="option2"
+        >
+          Option 2
+        </OdsSwitchItem>
+        <OdsSwitchItem
+          value="option3"
+        >
+          Option 2
+        </OdsSwitchItem>
+      </OdsSwitch>
 
-      {/*/!* KO isRequired => no way to get validity *!/*/}
-      {/*/!* KO should have an hasError status *!/*/}
-      {/*/!* Two event change on reset & clear *!/*/}
-      {/*<OdsSwitch*/}
-      {/*  isRequired={ true }*/}
-      {/*  name="switch"*/}
-      {/*  ref={ switchRef }*/}
-      {/*>*/}
-      {/*  <OdsSwitchItem*/}
-      {/*    isChecked*/}
-      {/*    value="option1"*/}
-      {/*  >*/}
-      {/*    Option 1*/}
-      {/*  </OdsSwitchItem>*/}
-      {/*  <OdsSwitchItem*/}
-      {/*    value="option2"*/}
-      {/*  >*/}
-      {/*    Option 2*/}
-      {/*  </OdsSwitchItem>*/}
-      {/*</OdsSwitch>*/}
-
-      {/* OK */}
       <OdsTextarea
-        defaultValue={ withDefaultValue ? 'textarea' : undefined }
         isRequired={ areAllRequired }
         name="textarea"
       />
 
-      {/*/!* OK *!/*/}
       <OdsTimepicker
-        defaultValue={ withDefaultValue ? '12:34' : undefined }
         isRequired={ areAllRequired }
         name="timepicker"
       />
 
-      {/*/!* OK *!/*/}
-      {/*<input*/}
-      {/*  name="hidden-input"*/}
-      {/*  type="hidden"*/}
-      {/*  value="should be present in form data" />*/}
+      <input
+        name="hidden-input"
+        type="hidden"
+        value="should be present in form data" />
 
       <div>
         <OdsButton label="Reset button"

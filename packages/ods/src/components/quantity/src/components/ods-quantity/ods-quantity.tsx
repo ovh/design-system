b60844c67e4be920922602549acc614cec36c684
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-11-20 09:44:46 +0100

    fix(quantity): error state & double deletions for emptying input
    Signed-off-by: Alexandre Esteves <alexandre.esteves.ae@gmail.com>

--- packages/ods/src/components/quantity/src/components/ods-quantity/ods-quantity.tsx
+++ packages/ods/src/components/quantity/src/components/ods-quantity/ods-quantity.tsx
@@ -115,7 +115,6 @@ export class OdsQuantity {
     }
     const step = this.step || 1;
     this.value = this.value !== null ? Number(this.value) - step : 0;
-    this.shouldUpdateIsInvalidState = true;
   }
 
   private async increment(): Promise<void> {
@@ -124,15 +123,15 @@ export class OdsQuantity {
     }
     const step = this.step || 1;
     this.value = this.value !== null ? Number(this.value) + step : 0;
-    this.shouldUpdateIsInvalidState = true;
   }
 
-  private onOdsBlur(): void {
+  private async onOdsBlur(): Promise<void> {
+    await this.odsInput?.checkValidity();
     this.isInvalid = !this.internals.validity.valid;
   }
 
   private async onOdsChange(event: OdsInputChangeEvent): Promise<void> {
-    if (event.detail.value === null) {
+    if (event.detail.value === null || event.detail.value === '') {
       this.value = null;
     } else {
       this.value = Number(event.detail.value) ?? null;
@@ -143,7 +142,7 @@ export class OdsQuantity {
     // In case the value gets updated from an other source than a blur event
     // we may have to perform an internal validity state update
     if (this.shouldUpdateIsInvalidState) {
-      await this.odsInput?.reportValidity();
+      await this.odsInput?.checkValidity();
       this.isInvalid = !this.internals.validity.valid;
     }
   }
@@ -170,6 +169,7 @@ export class OdsQuantity {
           isDisabled={ isMinusButtonDisabled(this.isDisabled, this.isReadonly, this.value, this.min) }
           icon={ ODS_ICON_NAME.minus }
           label=""
+          onBlur={ () => this.onOdsBlur() }
           onClick={ () => this.decrement() }
           size={ ODS_BUTTON_SIZE.sm }
           variant={ ODS_BUTTON_VARIANT.outline }>
@@ -206,6 +206,7 @@ export class OdsQuantity {
           isDisabled={ isPlusButtonDisabled(this.isDisabled, this.isReadonly, this.value, this.max) }
           icon={ ODS_ICON_NAME.plus }
           label=""
+          onBlur={ () => this.onOdsBlur() }
           onClick={ () => this.increment() }
           size={ ODS_BUTTON_SIZE.sm }
           variant={ ODS_BUTTON_VARIANT.outline }>

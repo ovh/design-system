905224c30e35d5394f2015bceaee85586c5619fd
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2024-11-26 16:59:35 +0100

    feat(range): add example
    Signed-off-by: Alexandre Esteves <alexandre.esteves.ae@gmail.com>

--- packages/examples/react-webpack/src/app/components/formNative/FormNative.tsx
+++ packages/examples/react-webpack/src/app/components/formNative/FormNative.tsx
@@ -1,5 +1,5 @@
 import { ODS_INPUT_TYPE } from '@ovhcloud/ods-components';
-import { OdsButton, OdsCheckbox, OdsDatepicker, OdsInput, OdsPassword, OdsPhoneNumber, OdsQuantity, OdsRadio, OdsSelect, OdsSwitch, OdsSwitchItem, OdsTextarea, OdsTimepicker } from '@ovhcloud/ods-components/react';
+import { OdsButton, OdsCheckbox, OdsDatepicker, OdsInput, OdsPassword, OdsPhoneNumber, OdsQuantity, OdsRadio, OdsRange, OdsSelect, OdsSwitch, OdsSwitchItem, OdsTextarea, OdsTimepicker } from '@ovhcloud/ods-components/react';
 import React, { type FormEvent, type ReactElement, useRef, useState } from 'react';
 import styles from './formNative.scss';
 
@@ -127,6 +127,14 @@ function FormNative(): ReactElement {
           value="radio-2"
         />
         <label htmlFor="radio2">Radio 2</label>
+      </div>
+
+      <div style={{ display: 'inline-flex' }}>
+        <OdsRange
+          isRequired={ areAllRequired }
+          name="range"
+          value={70}
+        />
       </div>
 
       <OdsSelect

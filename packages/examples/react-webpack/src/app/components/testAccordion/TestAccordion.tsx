75c56f593452ddc338204e0188a850cb3f954280
Damien Pellier <dpellier@gmail.com>
2024-12-20 14:49:15 +0100

    feat(doc): add more info on accordion is-open state

--- packages/examples/react-webpack/src/app/components/testAccordion/TestAccordion.tsx
+++ packages/examples/react-webpack/src/app/components/testAccordion/TestAccordion.tsx
@@ -4,7 +4,7 @@ import React, { type ReactElement, useRef, useState } from 'react';
 
 function TestAccordion(): ReactElement {
   const [isOpen, setIsOpen] = useState(false);
-  const accordionRef = useRef<HTMLOdsAccordionElement>(null)
+  const accordionRef = useRef<HTMLOdsAccordionElement>(null);
 
   function onToggleClick() {
     accordionRef.current?.toggle();
@@ -21,7 +21,7 @@ function TestAccordion(): ReactElement {
         <span slot="summary">Hello, world!</span>
 
         <span>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Lorem ipsum dolor sit amet ...
         </span>
       </OdsAccordion>
 

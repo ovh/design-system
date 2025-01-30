2a6b6716f39d3dadf363b2114ed440cc4256036e
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2025-01-30 10:51:16 +0100

    fix(datepicker): modal overflow
    Signed-off-by: Alexandre Esteves <alexandre.esteves.ae@gmail.com>

---
+++ packages/ods/src/components/datepicker/src/constants/datepicker-strategy.ts
@@ -0,0 +1,7 @@
+import { ODS_OVERLAY_STRATEGIES, ODS_OVERLAY_STRATEGY, type OdsOverlayStrategy } from '../../../../utils/overlay';
+
+export {
+  ODS_OVERLAY_STRATEGIES as ODS_DATEPICKER_STRATEGIES,
+  ODS_OVERLAY_STRATEGY as ODS_DATEPICKER_STRATEGY,
+  type OdsOverlayStrategy as OdsDatepickerStrategy,
+};

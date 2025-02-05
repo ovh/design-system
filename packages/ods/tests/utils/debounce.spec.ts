a22b48023d552bed12f25f968bf340678f9f9e94
Alexandre Esteves <alexandre.esteves.ae@gmail.com>
2025-02-05 15:46:24 +0100

    test(unit): add unit test debounce
    Signed-off-by: Alexandre Esteves <alexandre.esteves.ae@gmail.com>

---
+++ packages/ods/tests/utils/debounce.spec.ts
@@ -0,0 +1,42 @@
+import { debounce } from '../../src/utils/debounce';
+
+describe('utils debounce', () => {
+  jest.useFakeTimers();
+
+  beforeEach(jest.clearAllMocks);
+
+  it('should delay the callback', () => {
+    const callback = jest.fn();
+    const debouncedFn = debounce(callback, 1000);
+
+    debouncedFn();
+    expect(callback).not.toHaveBeenCalled();
+
+    jest.advanceTimersByTime(1000);
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call callback once', () => {
+    const callback = jest.fn();
+    const debouncedFn = debounce(callback, 1000);
+
+    debouncedFn();
+    jest.advanceTimersByTime(500);
+    debouncedFn();
+    jest.advanceTimersByTime(500);
+    debouncedFn();
+
+    jest.advanceTimersByTime(1000);
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call callback with parameters', () => {
+    const callback = jest.fn();
+    const debouncedFn = debounce(callback, 1000);
+
+    debouncedFn('hello', 42);
+    jest.advanceTimersByTime(1000);
+
+    expect(callback).toHaveBeenCalledWith('hello', 42);
+  });
+});

29f1956e388ccb83f062c00518ec7e2ac9db342a
Damien Pellier <dpellier@gmail.com>
2025-03-27 09:28:59 +0100

    feat(badge): add missing e2e tests

--- packages/ods-react/src/components/badge/tests/rendering/ods-badge.stories.tsx
+++ packages/ods-react/src/components/badge/tests/rendering/ods-badge.stories.tsx
@@ -1,4 +1,4 @@
-import { OdsBadge } from '../../src';
+import { ODS_BADGE_SIZE, OdsBadge } from '../../src';
 
 export default {
   component: OdsBadge,
@@ -17,4 +17,12 @@ export const render = () => (
   <OdsBadge data-testid="render">
     Render
   </OdsBadge>
+);
+
+export const sizes = () => (
+  <>
+    <OdsBadge data-testid="size-sm" size={ ODS_BADGE_SIZE.sm }>SM</OdsBadge>
+    <OdsBadge data-testid="size-md" size={ ODS_BADGE_SIZE.md }>MD</OdsBadge>
+    <OdsBadge data-testid="size-lg" size={ ODS_BADGE_SIZE.lg }>LG</OdsBadge>
+  </>
 );

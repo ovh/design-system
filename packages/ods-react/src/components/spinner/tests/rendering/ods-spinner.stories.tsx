97ebc6710ab3a242b65fbfda4a057436c3c112c1
Damien Pellier <dpellier@gmail.com>
2025-03-31 17:37:58 +0200

    feat(spinner): add missing e2e tests

--- packages/ods-react/src/components/spinner/tests/rendering/ods-spinner.stories.tsx
+++ packages/ods-react/src/components/spinner/tests/rendering/ods-spinner.stories.tsx
@@ -1,4 +1,4 @@
-import { OdsSpinner } from '../../src';
+import { ODS_SPINNER_SIZE, OdsSpinner } from '../../src';
 
 export default {
   component: OdsSpinner,
@@ -12,6 +12,14 @@ export const customStyle = () => (
 );
 
 export const render = () => (
-  <OdsSpinner
-    data-testid="render" />
+  <OdsSpinner data-testid="render" />
+);
+
+export const sizes = () => (
+  <>
+    <OdsSpinner data-testid="size-xs" size={ ODS_SPINNER_SIZE.xs }>XS</OdsSpinner>
+    <OdsSpinner data-testid="size-sm" size={ ODS_SPINNER_SIZE.sm }>SM</OdsSpinner>
+    <OdsSpinner data-testid="size-md" size={ ODS_SPINNER_SIZE.md }>MD</OdsSpinner>
+    <OdsSpinner data-testid="size-lg" size={ ODS_SPINNER_SIZE.lg }>LG</OdsSpinner>
+  </>
 );

4eba34a9cd29f5f5230c08ea086e7ba55beb46e5
Charles-Antoine Alba <poro@variables.fr>
2025-04-03 18:04:18 +0200

    feat(card): adds card component to react

---
+++ packages/ods-react/src/components/card/src/constants/card-color.ts
@@ -0,0 +1,14 @@
+enum ODS_CARD_COLOR {
+  neutral = 'neutral',
+  primary = 'primary',
+}
+
+type OdsCardColor = `${ODS_CARD_COLOR}`;
+
+const ODS_CARD_COLORS = Object.freeze(Object.values(ODS_CARD_COLOR));
+
+export {
+  ODS_CARD_COLOR,
+  ODS_CARD_COLORS,
+  type OdsCardColor,
+}; 

f85954a0465ad8e34351e67f0993200a67b8eafe
Charles-Antoine Alba <poro@variables.fr>
2025-04-07 11:25:15 +0200

    feat(divider): add OdsDivider component with styles and tests

---
+++ packages/ods-react/src/components/divider/src/dev.stories.tsx
@@ -0,0 +1,10 @@
+import { OdsDivider } from '.';
+
+export default {
+  component: OdsDivider,
+  title: 'OdsDivider dev',
+};
+
+export const Default = () => (
+  <OdsDivider />
+);
---
+++ packages/ods-react/src/components/divider/src/index.ts
@@ -0,0 +1,3 @@
+export { OdsDivider, type OdsDividerProp } from './components/ods-divider/OdsDivider';
+export { ODS_DIVIDER_COLOR, ODS_DIVIDER_COLORS, type OdsDividerColor } from './constants/divider-color';
+export { ODS_DIVIDER_SPACING, ODS_DIVIDER_SPACINGS, type OdsDividerSpacing } from './constants/divider-spacing';

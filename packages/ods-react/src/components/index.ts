5a2815688d0aa4db135228b8862a9886332b683d
Charles-Antoine Alba <poro@variables.fr>
2025-04-07 16:31:22 +0200

    feat(skeleton): initiate skeleton component

--- packages/ods-react/src/components/index.ts
+++ packages/ods-react/src/components/index.ts
@@ -8,5 +8,5 @@ export * from './popover/src';
 export * from './tooltip/src';
 export * from './textarea/src';
 export * from './form-field/src';
-
 export * from './card/src';
+export * from './skeleton/src';

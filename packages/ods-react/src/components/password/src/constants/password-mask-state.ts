ee13e1a4b67b544b8ede471825639da55053f9f0
Damien Pellier <dpellier@gmail.com>
2025-04-08 16:10:29 +0200

    feat(password): implement component

---
+++ packages/ods-react/src/components/password/src/constants/password-mask-state.ts
@@ -0,0 +1,7 @@
+import { ODS_INPUT_MASK_STATE, ODS_INPUT_MASK_STATES, type OdsInputMaskState } from '../../../input/src';
+
+export {
+  ODS_INPUT_MASK_STATE as ODS_PASSWORD_MASK_STATE,
+  ODS_INPUT_MASK_STATES as ODS_PASSWORD_MASK_STATES,
+  type OdsInputMaskState as OdsPasswordMaskState,
+};

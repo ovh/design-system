329e57c2c6b7d19db4d7e81bf72772da9fdf1a7a
Damien Pellier <dpellier@gmail.com>
2025-04-10 15:34:37 +0200

    fix(overlays): add missing className prop

--- packages/ods-react/src/components/tooltip/src/components/ods-tooltip-content/OdsTooltipContent.tsx
+++ packages/ods-react/src/components/tooltip/src/components/ods-tooltip-content/OdsTooltipContent.tsx
@@ -1,5 +1,6 @@
 import { Portal } from '@ark-ui/react/portal';
 import { Tooltip } from '@ark-ui/react/tooltip';
+import classNames from 'classnames';
 import { type ComponentPropsWithRef, type FC, type JSX, forwardRef } from 'react';
 import style from './odsTooltipContent.module.scss';
 
@@ -9,6 +10,7 @@ interface OdsTooltipContentProp extends ComponentPropsWithRef<'div'> {
 
 const OdsTooltipContent: FC<OdsTooltipContentProp> = forwardRef(({
   children,
+  className,
   withArrow = false,
   ...props
 }, ref): JSX.Element => {
@@ -16,7 +18,7 @@ const OdsTooltipContent: FC<OdsTooltipContentProp> = forwardRef(({
     <Portal>
       <Tooltip.Positioner>
         <Tooltip.Content
-          className={ style['ods-tooltip-content'] }
+          className={ classNames(style['ods-tooltip-content'], className) }
           ref={ ref }
           { ...props }>
           {

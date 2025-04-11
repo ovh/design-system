e59c26ec59751197df8ce934e2f06f224541f4c0
Damien Pellier <dpellier@gmail.com>
2025-04-11 11:28:43 +0200

    fix(table): add missing exports & update doc

--- packages/ods-react/src/components/table/src/index.ts
+++ packages/ods-react/src/components/table/src/index.ts
@@ -1,1 +1,3 @@
 export { OdsTable, type OdsTableProp } from './components/ods-table/OdsTable';
+export { ODS_TABLE_SIZE, ODS_TABLE_SIZES, type OdsTableSize } from './constants/table-size';
+export { ODS_TABLE_VARIANT, ODS_TABLE_VARIANTS, type OdsTableVariant } from './constants/table-variant';

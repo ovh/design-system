4a078a4bce9bd1203fad927c0ab5ae8f2f3074af
Damien Pellier <dpellier@gmail.com>
2025-04-17 16:46:47 +0200

    fix(select): add missing name & hidden field && add to example app

--- packages/examples/webpack-app/src/app/App.tsx
+++ packages/examples/webpack-app/src/app/App.tsx
@@ -2,11 +2,10 @@ import React, { type ReactElement } from 'react';
 import { FormFormik } from './components/formFormik/FormFormik';
 // import { FormHookForm } from './components/formHookForm/FormHookForm';
 // import { FormNative } from './components/formNative/FormNative';
-import style from './app.scss';
 
 function App(): ReactElement {
   return (
-    <div className={ style.app }>
+    <div>
       <FormFormik />
       {/*<FormHookForm />*/}
       {/*<FormNative />*/}

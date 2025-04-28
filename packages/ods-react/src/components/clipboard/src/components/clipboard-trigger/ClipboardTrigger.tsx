f6d4e9b9b86b1d6705a48289244006ffe4fe3f1d
Charles-Antoine Alba <poro@variables.fr>
2025-04-28 11:27:33 +0200

    feat(clipboard): refactor component to use composition

---
+++ packages/ods-react/src/components/clipboard/src/components/clipboard-trigger/ClipboardTrigger.tsx
@@ -0,0 +1,68 @@
+import { Clipboard as VendorClipboard } from '@ark-ui/react/clipboard';
+import classNames from 'classnames';
+import { type ComponentPropsWithRef, type FC, type JSX, forwardRef } from 'react';
+import { BUTTON_SIZE, BUTTON_VARIANT, Button } from '../../../../button/src';
+import { ICON_NAME, Icon } from '../../../../icon/src';
+import { TOOLTIP_POSITION, Tooltip, TooltipContent, TooltipTrigger } from '../../../../tooltip/src';
+import { useClipboard } from '../../contexts/useClipboard.tsx';
+import style from './clipboardTrigger.module.scss';
+
+interface ClipboardTriggerProps extends ComponentPropsWithRef<'button'> {
+  labelCopy?: string,
+  labelCopySuccess?: string,
+}
+
+const ClipboardTrigger: FC<ClipboardTriggerProps> = forwardRef(({
+  className,
+  labelCopy = 'Copy to clipboard',
+  labelCopySuccess = 'Copied!',
+  ...props
+}, ref): JSX.Element => {
+  const { disabled } = useClipboard();
+
+  return <Tooltip position={ TOOLTIP_POSITION.right }>
+    <VendorClipboard.Trigger asChild>
+      <VendorClipboard.Indicator
+        className={ style[ 'clipboard__copy' ] }
+        copied={
+          <>
+            <TooltipTrigger asChild ref={ ref } {...props}>
+              <Button
+                disabled={ disabled }
+                size={ BUTTON_SIZE.xs }
+                variant={ BUTTON_VARIANT.ghost }
+              >
+                <Icon name={ ICON_NAME.fileCopy } />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent className={ classNames(style[ 'clipboard__tooltip-success' ], className) }>
+              { labelCopySuccess }
+              <Icon
+                className={ style[ 'clipboard__tooltip-success__check' ] }
+                name={ ICON_NAME.check }
+              />
+
+            </TooltipContent>
+          </>
+        }>
+        <TooltipTrigger asChild ref={ ref } {...props}>
+          <Button
+            disabled={ disabled }
+            size={ BUTTON_SIZE.xs }
+            variant={ BUTTON_VARIANT.ghost }
+          >
+            <Icon
+              name={ ICON_NAME.fileCopy } />
+          </Button>
+        </TooltipTrigger>
+        { !disabled && <TooltipContent className={ style[ 'clipboard__tooltip' ] }>
+          { labelCopy }
+        </TooltipContent> }
+      </VendorClipboard.Indicator>
+    </VendorClipboard.Trigger>
+  </Tooltip>;
+});
+
+ClipboardTrigger.displayName = 'ClipboardTrigger';
+
+export { ClipboardTrigger, type ClipboardTriggerProps };

f8d557455a11c83709f1485a427c066a2002dadc
Charles-Antoine Alba <poro@variables.fr>
2025-04-29 18:13:50 +0200

    feat(accordion): implement component

---
+++ packages/ods-react/src/components/accordion/src/components/accordion-trigger/AccordionTrigger.tsx
@@ -0,0 +1,41 @@
+import { Accordion } from '@ark-ui/react/accordion';
+import classNames from 'classnames';
+import { type ComponentPropsWithRef, type FC, type JSX, forwardRef } from 'react';
+import { ICON_NAME, Icon } from '../../../../icon/src';
+import style from './accordionTrigger.module.scss';
+
+type AccordionTriggerProp = ComponentPropsWithRef<'button'>;
+
+const AccordionTrigger: FC<AccordionTriggerProp> = forwardRef(({
+  className,
+  children,
+  ...props
+}, ref): JSX.Element => {
+  return (
+    <Accordion.ItemTrigger
+      className={ classNames(style[ 'accordion-trigger' ], className) }
+      ref={ ref }
+      { ...props }
+    >
+
+      <div className={ style[ 'accordion-trigger__content' ] }>
+        { children }
+      </div>
+
+      <Accordion.ItemIndicator asChild>
+        <Icon
+          className={ style[ 'accordion-trigger__icon' ] }
+          name={ ICON_NAME.chevronDown }
+        />
+      </Accordion.ItemIndicator>
+
+    </Accordion.ItemTrigger>
+  );
+});
+
+AccordionTrigger.displayName = 'AccordionTrigger';
+
+export {
+  AccordionTrigger,
+  type AccordionTriggerProp,
+};

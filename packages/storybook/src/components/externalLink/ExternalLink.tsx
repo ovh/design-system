05f6fb4dfc3a9536b99e109bd21838d4a8b31f94
Damien Pellier <dpellier@gmail.com>
2025-05-02 10:34:32 +0200

    feat(doc): replace Link by ExternalLink & fix storybook links

---
+++ packages/storybook/src/components/externalLink/ExternalLink.tsx
@@ -0,0 +1,16 @@
+import { ICON_NAME, Icon, Link, type LinkProp } from '@ovhcloud/ods-react';
+import React, { type ComponentPropsWithRef, type JSX } from 'react';
+
+const ExternalLink = ({ children, ...prop }: LinkProp & Omit<ComponentPropsWithRef<'a'>, keyof LinkProp>): JSX.Element => {
+  return (
+    <Link
+      { ...prop }
+      target="_blank">
+      { children } <Icon name={ ICON_NAME.externalLink } />
+    </Link>
+  );
+};
+
+export {
+  ExternalLink,
+};

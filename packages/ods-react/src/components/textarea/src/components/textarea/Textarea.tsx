66a82b0c309bbaaab400b9c38dd108c5cfa6f36f
Damien Pellier <dpellier@gmail.com>
2025-05-16 11:24:56 +0200

    feat(input|textarea): add invalid prop to enforce error style

--- packages/ods-react/src/components/textarea/src/components/textarea/Textarea.tsx
+++ packages/ods-react/src/components/textarea/src/components/textarea/Textarea.tsx
@@ -3,15 +3,19 @@ import classNames from 'classnames';
 import { type ComponentPropsWithRef, type FC, type JSX, forwardRef } from 'react';
 import style from './textarea.module.scss';
 
-interface TextareaProp extends ComponentPropsWithRef<'textarea'> {}
+interface TextareaProp extends ComponentPropsWithRef<'textarea'> {
+  invalid?: boolean,
+}
 
 const Textarea: FC<TextareaProp> = forwardRef(({
   className,
+  invalid,
   ...props
 }, ref): JSX.Element => {
   return (
     <Field.Textarea
       className={ classNames(style['textarea'], className) }
+      data-invalid={ invalid ? true : undefined }
       ref={ ref }
       { ...props } />
   );

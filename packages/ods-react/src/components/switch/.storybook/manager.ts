f4114d7914cf10ab52e472ef841d65a04c584fee
Damien Pellier <dpellier@gmail.com>
2025-05-22 15:05:55 +0200

    feat(switch): implement component

---
+++ packages/ods-react/src/components/switch/.storybook/manager.ts
@@ -0,0 +1,10 @@
+import { addons } from '@storybook/manager-api';
+
+addons.register('custom-panel', (api) => {
+  api.togglePanel(false);
+});
+
+addons.setConfig({
+  enableShortcuts: false,
+  showToolbar: true,
+});
---
+++ packages/ods-react/src/components/switch/src/components/switch/Switch.tsx
@@ -0,0 +1,64 @@
+import { ToggleGroup, type ToggleGroupValueChangeDetails } from '@ark-ui/react/toggle-group';
+import classNames from 'classnames';
+import { type ComponentPropsWithRef, type FC, type JSX, forwardRef } from 'react';
+import { SWITCH_SIZE, type SwitchSize } from '../../constants/switch-size';
+import style from './switch.module.scss';
+
+interface SwitchValueChangeDetail {
+  value: string,
+}
+
+interface SwitchProp extends ComponentPropsWithRef<'div'> {
+  defaultValue?: string,
+  disabled?: boolean,
+  invalid?: boolean,
+  onValueChange?: (detail: SwitchValueChangeDetail) => void,
+  size?: SwitchSize,
+  value?: string,
+}
+
+const Switch: FC<SwitchProp> = forwardRef(({
+  className,
+  defaultValue,
+  disabled,
+  invalid,
+  onValueChange,
+  size = SWITCH_SIZE.md,
+  value,
+  ...props
+}, ref): JSX.Element => {
+  function onSwitchValueChange(detail: ToggleGroupValueChangeDetails): void {
+    if (onValueChange) {
+      onValueChange({ value: detail.value[0] });
+    }
+  }
+
+  return (
+    <ToggleGroup.Root
+      className={ classNames(
+        style['switch'],
+        style[`switch--${size}`],
+        { [style['switch--invalid']]: invalid },
+        className,
+      )}
+      { ...props }
+      defaultValue={ defaultValue ? [defaultValue] : undefined }
+      deselectable={ false }
+      disabled={ disabled }
+      loopFocus={ false }
+      multiple={ false }
+      onValueChange={ onSwitchValueChange }
+      orientation="horizontal"
+      ref={ ref }
+      value={ value ? [value] : undefined }>
+    </ToggleGroup.Root>
+  );
+});
+
+Switch.displayName = 'Switch';
+
+export {
+  Switch,
+  type SwitchProp,
+  type SwitchValueChangeDetail,
+};

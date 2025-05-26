75066b451db8218f5bf04c11a347d75363dc2280
Charles-Antoine Alba <poro@variables.fr>
2025-05-26 09:57:22 +0200

    feat(toggle): improve scss naming

--- packages/ods-react/src/components/toggle/src/components/toggleLabels/ToggleLabels.tsx
+++ packages/ods-react/src/components/toggle/src/components/toggleLabels/ToggleLabels.tsx
@@ -9,22 +9,22 @@ const OFF_LABEL = 'OFF';
 const ToggleLabels: FC = (): JSX.Element => {
   const { checked: isChecked, disabled, focused } = useSwitchContext();
   return (
-    <div className={ style[ 'toggle__control__labels' ] }>
+    <div className={ style[ 'toggle-labels' ] }>
       <span
         className={ classNames(
-          style[ 'toggle__control__labels__label-on' ],
-          { [ style[ 'toggle__control__labels__label-on--checked' ] ]: isChecked },
-          { [ style[ 'toggle__control__labels__label-on--disabled' ] ]: disabled },
+          style[ 'toggle-labels__label-on' ],
+          { [ style[ 'toggle-labels__label-on--checked' ] ]: isChecked },
+          { [ style[ 'toggle-labels__label-on--disabled' ] ]: disabled },
         ) }
       >
         { ON_LABEL }
       </span>
       <span
         className={ classNames(
-          style[ 'toggle__control__labels__label-off' ],
-          { [ style[ 'toggle__control__labels__label-off--unchecked' ] ]: !isChecked },
-          { [ style[ 'toggle__control__labels__label-off--disabled' ] ]: disabled },
-          { [ style[ 'toggle__control__labels__label-off--focused' ] ]: focused },
+          style[ 'toggle-labels__label-off' ],
+          { [ style[ 'toggle-labels__label-off--unchecked' ] ]: !isChecked },
+          { [ style[ 'toggle-labels__label-off--disabled' ] ]: disabled },
+          { [ style[ 'toggle-labels__label-off--focused' ] ]: focused },
         ) }
       >
         { OFF_LABEL }

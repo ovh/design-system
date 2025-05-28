8bd3e2f4fd0cd1a9e49c8588274da69960ab28d3
Charles-Antoine Alba <poro@variables.fr>
2025-05-28 18:04:25 +0200

    feat(pagination): add props and refactor components structure

--- packages/ods-react/src/components/pagination/src/index.ts
+++ packages/ods-react/src/components/pagination/src/index.ts
@@ -1,1 +1,3 @@
 export { Pagination, type PaginationPageChangeDetail, type PaginationProp } from './components/pagination/Pagination';
+export { PaginationItem, type PaginationItemProp } from './components/pagination-item/PaginationItem';
+export { PaginationButtonWithTooltip, type PaginationButtonWithTooltipProp } from './components/pagination-button-with-tooltip/PaginationButtonWithTooltip';

8d1c7ae77cc14c1c4fcb1031c24214de0b792eeb
Poppyto <poppyto@gmail.com>
2025-05-30 11:19:49 +0200

    fix(pagination): create pageList with only visible pages to avoid creating infinite DOM <li> components when totalPage is a big number

--- packages/ods/src/components/pagination/src/controller/ods-pagination.ts
+++ packages/ods/src/components/pagination/src/controller/ods-pagination.ts
@@ -14,45 +14,51 @@ function computeActualTotalPages(itemPerPage: number, totalItems: number | undef
 }
 
 function createPageList(totalPages: number, pageSelected: number): OdsPaginationPageList {
-  const pageList: OdsPaginationPageList = Array.from({ length: totalPages }, () => ({ active: false }));
+  const pageList: OdsPaginationPageList = [];
 
-  let startIndex = Math.max(pageSelected - DEFAULT_PAGE_OFFSET, 1);
+    let startIndex = Math.max(pageSelected - DEFAULT_PAGE_OFFSET, 1);
   const endIndex = Math.min(startIndex + ELLIPSIS_THRESHOLD, totalPages);
 
   // If there are less than or equal to 5 pages, set all pages as active (all displayed).
-  if (totalPages <= (MAX_VISIBLE_ITEMS - DEFAULT_PAGE_OFFSET)) {
-    for (let i = 0; i < pageList.length; i++) {
-      pageList[i].active = true;
-    }
-  } else {
-    // If there are more than 5 pages, set only some of them as active (not all displayed).
-    if (totalPages - pageSelected < DEFAULT_PAGE_OFFSET) {
-      // If selected page is one of the last pages of a long list, show the last 5 pages.
-      startIndex = totalPages - ELLIPSIS_THRESHOLD;
-    }
+  if (totalPages <= (MAX_VISIBLE_ITEMS - DEFAULT_PAGE_OFFSET)) {    
+	for (let i = 0; i < totalPages; i++) {
+      pageList.push({ active: true, pageId: i+1 });
+    }	
+	return pageList;	
+  } 
 
-    for (let i = startIndex; i <= endIndex; i++) {
-      // If i is two pages away from the selected page, skip it if it is between 4 and totalPages-2.
-      if (i == pageSelected - DEFAULT_PAGE_OFFSET && pageSelected < totalPages - 1 && pageSelected > ELLIPSIS_THRESHOLD && pageSelected < totalPages - DEFAULT_PAGE_OFFSET) {
-        continue;
-      }
-      // If i is two pages away from the selected page, skip it if it is greater than 5.
-      if (i == pageSelected + DEFAULT_PAGE_OFFSET && pageSelected < totalPages - (DEFAULT_PAGE_OFFSET + MINIMUM_PAGE) && i > (MAX_VISIBLE_ITEMS - DEFAULT_PAGE_OFFSET)) {
-        continue;
-      }
-      pageList[i - 1].active = true;
-    }
-
-    if (startIndex > 1) {
-      // If startIndex is not 1, show the first page as active.
-      pageList[0].active = true;
-    }
-
-    if (endIndex < totalPages) {
-      // If endIndex is not equal to totalPages, show the last page as active.
-      pageList[totalPages - 1].active = true;
-    }
+	// If there are more than 5 pages, set only some of them as active (not all displayed).
+  if (totalPages - pageSelected < DEFAULT_PAGE_OFFSET) {
+    // If selected page is one of the last pages of a long list, show the last 5 pages.
+    startIndex = totalPages - ELLIPSIS_THRESHOLD;
   }
+
+	if (startIndex > 1) {
+    // If startIndex is not 1, show the first page as active.
+    pageList.push({ active: true, pageId: 1 });
+  }
+  
+	let adjustedStartIndex = startIndex;
+	let adjustedEndIndex = endIndex;
+
+	// get the start range of pages to show
+	if (pageSelected > ELLIPSIS_THRESHOLD && pageSelected < totalPages - DEFAULT_PAGE_OFFSET) {
+	  adjustedStartIndex = Math.max(adjustedStartIndex, pageSelected - DEFAULT_PAGE_OFFSET + 1);
+	}
+	// get the end range of pages to show
+	if (pageSelected < totalPages - (DEFAULT_PAGE_OFFSET + MINIMUM_PAGE) && pageSelected > MAX_VISIBLE_ITEMS - DEFAULT_PAGE_OFFSET) {
+	  adjustedEndIndex = Math.min(adjustedEndIndex, pageSelected + DEFAULT_PAGE_OFFSET - 1);
+	}
+
+	for (let i = adjustedStartIndex; i <= adjustedEndIndex; i++) {
+	  pageList.push({ active: true, pageId: i });
+	}
+
+	if (endIndex < totalPages)
+	{
+	  // If endIndex is not equal to totalPages, show the last page as active.
+	  pageList.push({ active: true, pageId: totalPages + 1 });
+	}  
 
   return pageList;
 }

8e70784406a21732d4f309b24041d7c4d097f318
Damien Pellier <dpellier@gmail.com>
2025-06-06 16:42:20 +0200

    refactor(components): normalize colors

--- packages/ods-react/src/components/button/src/dev.stories.tsx
+++ packages/ods-react/src/components/button/src/dev.stories.tsx
@@ -1,10 +1,39 @@
-import { Button } from '.';
+import { BUTTON_COLORS, BUTTON_VARIANTS, Button } from '.';
 import style from './dev.module.css';
 
 export default {
   component: Button,
   title: 'Button dev',
 };
+
+export const Colors = () => (
+  <div style={{ display: 'flex', flexFlow: 'column', rowGap: '16px' }}>
+    {
+      BUTTON_VARIANTS.map((variant) => (
+        <div
+          key={ variant }
+          style={{ display: 'flex', flexFlow: 'row', columnGap: '8px' }}>
+          {
+            BUTTON_COLORS.map((color) => (
+              <Button
+                color={ color }
+                key={ color }
+                variant={ variant }>
+                { color }
+              </Button>
+            ))
+          }
+        </div>
+      ))
+    }
+  </div>
+);
+
+export const CustomStyle = () => (
+  <Button className={ style['custom-button'] }>
+    Custom Styled Button
+  </Button>
+);
 
 export const Default = () => (
   <Button>
@@ -15,11 +44,5 @@ export const Default = () => (
 export const IsLoading = () => (
   <Button loading={ true }>
     IsLoading
-  </Button>
-);
-
-export const CustomStyle = () => (
-  <Button className={ style['custom-button'] }>
-    Custom Styled Button
   </Button>
 );

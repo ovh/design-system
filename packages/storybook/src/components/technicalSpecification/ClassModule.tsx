ee3a38dc8860f9a75141015630b7fab98bacf6f3
Damien Pellier <dpellier@gmail.com>
2025-06-16 15:22:23 +0200

    feat(doc): add all extraInfo & update rendering

--- packages/storybook/src/components/technicalSpecification/ClassModule.tsx
+++ packages/storybook/src/components/technicalSpecification/ClassModule.tsx
@@ -33,62 +33,66 @@ const ClassModule = ({ component, extraInfo }: ClassModuleProp): JSX.Element => 
       <Heading label={ component.name } level={ 2 } />
 
       {
-        extraAttributeInfo &&
-        <p>
-          This component extends all the native <ExternalLink href={ extraAttributeInfo.url }>{ extraAttributeInfo.name } attributes</ExternalLink>.
-        </p>
-      }
-
-      {
-        component.props.length <= 0 ?
+        (component.props.length <= 0 && !extraAttributeInfo) ?
           <p>
             This component has no specific properties.
           </p> :
           <Table>
             <thead className={ styles['class-module__properties__header'] }>
-            <tr>
-              <td>Property</td>
-              <td>Type</td>
-              <td>Required</td>
-              <td>Default value</td>
-            </tr>
+              <tr>
+                <td>Property</td>
+                <td>Type</td>
+                <td>Required</td>
+                <td>Default value</td>
+              </tr>
             </thead>
 
             <tbody className={ styles['class-module__properties__body'] }>
-            {
-              component.props.map((prop, idx) => (
-                <tr key={ idx }>
-                  <td>
-                    { prop.name }
-                  </td>
-
-                  <td>
-                    <CodeOrSourceMdx>
-                      { prop.type }
-                    </CodeOrSourceMdx>
-                  </td>
-
-                  <td className={ styles['class-module__properties__body__is-required'] }>
-                    {
-                      prop.isOptional ? '-' :
-                        <Icon aria-label="Required"
-                              className={ styles['class-module__properties__body__is-required--required'] }
-                              name={ ICON_NAME.check } />
-                    }
-                  </td>
-
-                  <td>
-                    <CodeOrSourceMdx>
-                      {
-                        prop.defaultValue === undefined ?
-                          <div className={ styles['class-module__properties__body__default-value'] }>undefined</div> :
-                          prop.defaultValue
-                      }
-                    </CodeOrSourceMdx>
+              {
+                extraAttributeInfo &&
+                <tr>
+                  <td
+                    className={ styles['class-module__properties__body__extend'] }
+                    colSpan={ 4 }>
+                    This component extends all the native <ExternalLink href={ extraAttributeInfo.url }>{ extraAttributeInfo.name } attributes</ExternalLink>.
                   </td>
                 </tr>
-              ))
-            }
+              }
+
+              {
+                component.props.map((prop, idx) => (
+                  <tr key={ idx }>
+                    <td>
+                      { prop.name }
+                    </td>
+
+                    <td>
+                      <CodeOrSourceMdx>
+                        { prop.type }
+                      </CodeOrSourceMdx>
+                    </td>
+
+                    <td className={ styles['class-module__properties__body__is-required'] }>
+                      {
+                        prop.isOptional ? '-' :
+                          <Icon aria-label="Required"
+                                className={ styles['class-module__properties__body__is-required--required'] }
+                                name={ ICON_NAME.check } />
+                      }
+                    </td>
+
+                    <td>
+                      <CodeOrSourceMdx>
+                        {
+                          prop.defaultValue === undefined ?
+                            <div className={ styles['class-module__properties__body__default-value'] }>undefined</div> :
+                            prop.defaultValue
+                        }
+                      </CodeOrSourceMdx>
+                    </td>
+                  </tr>
+                ))
+              }
             </tbody>
           </Table>
       }

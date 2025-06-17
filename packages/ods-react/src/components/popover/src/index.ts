fc742b683528f9dc16445408b009fcdfef1f9629
Damien Pellier <dpellier@gmail.com>
2025-06-17 15:13:37 +0200

    feat(popover): add onOpenChange prop

--- packages/ods-react/src/components/popover/src/index.ts
+++ packages/ods-react/src/components/popover/src/index.ts
@@ -1,4 +1,4 @@
-export { Popover, type PopoverProp } from './components/popover/Popover';
+export { Popover, type PopoverOpenChangeDetail, type PopoverProp } from './components/popover/Popover';
 export { PopoverContent, type PopoverContentProp } from './components/popover-content/PopoverContent';
 export { PopoverTrigger, type PopoverTriggerProp } from './components/popover-trigger/PopoverTrigger';
 export { POPOVER_POSITION, POPOVER_POSITIONS, type PopoverPosition } from './constants/popover-position';

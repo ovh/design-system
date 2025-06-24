0e56ca1d29150880ac1e1d7f04bd1514d7001e31
Charles-Antoine Alba <poro@variables.fr>
2025-06-24 16:16:48 +0200

    refactor(combobox): improve handling of multiple selection and tag focus management

--- packages/ods-react/src/components/combobox/src/components/combobox-control/ComboboxControl.tsx
+++ packages/ods-react/src/components/combobox/src/components/combobox-control/ComboboxControl.tsx
@@ -1,9 +1,9 @@
 import { Combobox as VendorCombobox, useComboboxContext } from '@ark-ui/react/combobox';
 import classNames from 'classnames';
-import { type FC, type FocusEvent, type JSX, type KeyboardEvent, type MouseEvent, forwardRef, useEffect, useRef, useState } from 'react';
+import { type FC, type FocusEvent, type JSX, type KeyboardEvent, type MouseEvent, forwardRef, useEffect, useRef } from 'react';
 import { Input } from '../../../../input/src';
 import { type ComboboxControlProp as ComboboxControlRootProp, useCombobox } from '../../contexts/useCombobox';
-import { ComboboxTags, handleTagsKeyDown } from '../combobox-tags/ComboboxTags';
+import { ComboboxTags } from '../combobox-tags/ComboboxTags';
 import style from './comboboxControl.module.scss';
 
 interface ComboboxControlProp extends ComboboxControlRootProp {}
@@ -16,20 +16,29 @@ const ComboboxControl: FC<ComboboxControlProp> = forwardRef(({
   ...props
 }, ref): JSX.Element => {
   const comboboxContext = useComboboxContext();
-  const { getContentProps, open, value = [] } = comboboxContext;
-  const { items, multiple } = useCombobox();
+  const { getContentProps, open } = comboboxContext;
+  const {
+    multiple,
+    tagFocus,
+    handleTagsKeyDown,
+    setIsOpen,
+  } = useCombobox();
+
   const contentProps = getContentProps() as {
     'data-placement'?: 'bottom' | 'top';
   };
   const placement = contentProps['data-placement'] as 'top' | 'bottom' | undefined;
   const inputRef = useRef<HTMLInputElement>(null);
   const controlRef = useRef<HTMLDivElement>(null);
-  const [focusedTagIndex, setFocusedTagIndex] = useState<number | null>(null);
+
+  useEffect(() => {
+    setIsOpen?.(open);
+  }, [open, setIsOpen]);
 
   useEffect(() => {
     function handleClickOutside(event: Event): void {
       if (controlRef.current && !controlRef.current.contains(event.target as Node)) {
-        setFocusedTagIndex(null);
+        tagFocus.resetTagFocus();
       }
     }
 
@@ -38,15 +47,11 @@ const ComboboxControl: FC<ComboboxControlProp> = forwardRef(({
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
     };
-  }, []);
-
-  useEffect(() => {
-    setFocusedTagIndex(null);
-  }, [open]);
+  }, [tagFocus]);
 
   function handleControlBlur(e: FocusEvent<HTMLDivElement>): void {
     if (!controlRef.current?.contains(e.relatedTarget as Node)) {
-      setFocusedTagIndex(null);
+      tagFocus.resetTagFocus();
     }
   }
 
@@ -56,30 +61,16 @@ const ComboboxControl: FC<ComboboxControlProp> = forwardRef(({
     }
 
     if (multiple) {
-      handleTagsKeyDown(e, {
-        focusedTagIndex,
-        inputRef,
-        onFocusedTagChange: setFocusedTagIndex,
-        onTagRemove: handleTagRemove,
-        value,
-      });
+      handleTagsKeyDown(e, inputRef);
     }
-  }
-
-  function handleInputFocus(): void {
-    setFocusedTagIndex(null);
   }
 
   function handleControlClick(e: MouseEvent<HTMLDivElement>): void {
-    if (e.target === e.currentTarget || (e.target as HTMLElement)?.closest('input')) {
-      setFocusedTagIndex(null);
-    }
-  }
+    const target = e.target as HTMLElement;
+    const isClickOnTag = target.closest('[data-scope="tag"]') || target.closest('.combobox-tag');
 
-  function handleTagRemove(tagValue: string): void {
-    if (value) {
-      const newValue = value.filter((val) => val !== tagValue);
-      comboboxContext.setValue(newValue);
+    if (!isClickOnTag && (e.target === e.currentTarget || target?.closest('input'))) {
+      tagFocus.resetTagFocus();
     }
   }
 
@@ -95,17 +86,7 @@ const ComboboxControl: FC<ComboboxControlProp> = forwardRef(({
       onClick={handleControlClick}
       onBlur={handleControlBlur}
     >
-      {multiple && (
-        <ComboboxTags
-          items={items}
-          value={value}
-          onTagRemove={handleTagRemove}
-          onFocusedTagChange={setFocusedTagIndex}
-          focusedTagIndex={focusedTagIndex}
-          inputRef={inputRef}
-          open={open}
-        />
-      )}
+      {multiple && <ComboboxTags />}
       <VendorCombobox.Trigger
         className={classNames(style['combobox-control__trigger'], className)}
         ref={ref}
@@ -119,7 +100,6 @@ const ComboboxControl: FC<ComboboxControlProp> = forwardRef(({
             )}
             clearable={clearable}
             loading={loading}
-            onFocus={handleInputFocus}
             onKeyDown={handleKeyDown}
             placeholder={placeholder}
             ref={inputRef}

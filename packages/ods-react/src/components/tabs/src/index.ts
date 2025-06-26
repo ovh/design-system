a386c04548c1c30b1659f57a29e70ff16abf700d
Damien Pellier <dpellier@gmail.com>
2025-06-26 15:22:36 +0200

    refactor(tabs): normalize callback name with switch

--- packages/ods-react/src/components/tabs/src/index.ts
+++ packages/ods-react/src/components/tabs/src/index.ts
@@ -1,4 +1,4 @@
-export { Tabs, type TabsProp, type TabsChangeEvent } from './components/tabs/Tabs';
+export { Tabs, type TabsProp, type TabsValueChangeEvent } from './components/tabs/Tabs';
 export { TabList, type TabListProp } from './components/tab-list/TabList';
 export { Tab, type TabProp } from './components/tab/Tab';
 export { TabContent, type TabContentProp } from './components/tab-content/TabContent';

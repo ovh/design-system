6e51305476518571f5f0296dc1a0a53bc23e2cdf
Damien Pellier <dpellier@gmail.com>
2025-07-25 11:23:54 +0200

    fix(overlays): prevent positionning issue if an id is set on popover/tooltip trigger

--- packages/ods-react/src/components/popover/src/components/popover/Popover.tsx
+++ packages/ods-react/src/components/popover/src/components/popover/Popover.tsx
@@ -1,6 +1,7 @@
 import { Popover as VendorPopover } from '@ark-ui/react/popover';
 import { type FC, type JSX, type PropsWithChildren } from 'react';
 import { POPOVER_POSITION, type PopoverPosition } from '../../constants/popover-position';
+import { PopoverProvider, usePopover } from '../../contexts/usePopover';
 
 interface PopoverOpenChangeDetail {
   open: boolean,
@@ -22,15 +23,20 @@ interface PopoverProp {
   position?: PopoverPosition,
 }
 
-const Popover: FC<PropsWithChildren<PopoverProp>> = ({
+const PopoverRoot: FC<PropsWithChildren<PopoverProp>> = ({
   children,
   onOpenChange,
   open,
   position = POPOVER_POSITION.top,
   ...props
 }): JSX.Element => {
+  const { triggerId } = usePopover();
+
   return (
     <VendorPopover.Root
+      ids={{
+        trigger: triggerId,
+      }}
       onOpenChange={ onOpenChange }
       open={ open }
       positioning={{
@@ -39,6 +45,14 @@ const Popover: FC<PropsWithChildren<PopoverProp>> = ({
       { ...props }>
       { children }
     </VendorPopover.Root>
+  );
+};
+
+const Popover: FC<PropsWithChildren<PopoverProp>> = (props): JSX.Element => {
+  return (
+    <PopoverProvider>
+      <PopoverRoot { ...props } />
+    </PopoverProvider>
   );
 };
 
--- packages/ods-react/src/components/tooltip/src/components/tooltip-trigger/TooltipTrigger.tsx
+++ packages/ods-react/src/components/tooltip/src/components/tooltip-trigger/TooltipTrigger.tsx
@@ -1,26 +1,43 @@
 import { Tooltip } from '@ark-ui/react/tooltip';
 import classNames from 'classnames';
-import { type ComponentPropsWithRef, type FC, type JSX, forwardRef } from 'react';
+import { type ComponentPropsWithRef, type FC, type JSX, type ReactElement, forwardRef, useEffect } from 'react';
+import { useTooltip } from '../../contexts/useTooltip';
 import style from './tooltipTrigger.module.scss';
 
 interface TooltipTriggerProp extends ComponentPropsWithRef<'button'> {
   /**
    * Use the provided child element as the default rendered element, combining their props and behavior.
+   * Be careful to pass an actual Node, not a Fragment.
    */
   asChild?: boolean,
 }
 
 const TooltipTrigger: FC<TooltipTriggerProp> = forwardRef(({
+  asChild,
   children,
   className,
   ...props
 }, ref): JSX.Element => {
+  const { setTriggerId } = useTooltip();
+
+  useEffect(() => {
+    if (!asChild && props.id) {
+      setTriggerId?.(props.id);
+      return;
+    }
+
+    if (children && (children as ReactElement).props?.id) {
+      setTriggerId?.((children as ReactElement).props.id);
+    }
+  }, [asChild, children, props, setTriggerId]);
+
   return (
     <Tooltip.Trigger
+      asChild={ asChild }
       className={ classNames(style['tooltip-trigger'], className) }
       ref={ ref }
       role="button"
-      tabIndex={0}
+      tabIndex={ 0 }
       { ...props }>
       { children }
     </Tooltip.Trigger>

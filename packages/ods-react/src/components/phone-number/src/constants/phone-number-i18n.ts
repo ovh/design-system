1580166b07cfbdd024a851af6c0f1317dd3bbb56
Charles-Antoine Alba <poro@variables.fr>
2025-08-13 09:00:50 +0200

    docs(i18n): update translations for multiple components

--- packages/ods-react/src/components/phone-number/src/constants/phone-number-i18n.ts
+++ packages/ods-react/src/components/phone-number/src/constants/phone-number-i18n.ts
@@ -8,12 +8,12 @@ const TRANSLATION = {
   [PHONE_NUMBER_I18N.countrySelect]: {
     [LOCALE.de]: 'Land auswählen',
     [LOCALE.en]: 'Select a country',
-    [LOCALE.es]: 'Seleccionar un país',
+    [LOCALE.es]: 'Seleccione un país',
     [LOCALE.fr]: 'Sélectionner un pays',
-    [LOCALE.it]: 'Selezionare un paese',
+    [LOCALE.it]: 'Seleziona un paese',
     [LOCALE.nl]: 'Selecteer een land',
-    [LOCALE.pl]: 'Wybrać kraj',
-    [LOCALE.pt]: 'Selecione um país',
+    [LOCALE.pl]: 'Wybierz kraj',
+    [LOCALE.pt]: 'Selecionar um país',
   },
 };
 
--- packages/ods-react/src/components/timepicker/src/constants/timepicker-i18n.ts
+++ packages/ods-react/src/components/timepicker/src/constants/timepicker-i18n.ts
@@ -6,14 +6,14 @@ enum TIMEPICKER_I18N {
 
 const TRANSLATION = {
   [TIMEPICKER_I18N.timezoneSelect]: {
-    [LOCALE.de]: 'Wählen Sie eine Zeitzone aus',
+    [LOCALE.de]: 'Zeitzone auswählen',
     [LOCALE.en]: 'Select a timezone',
-    [LOCALE.es]: 'Seleccione una zona horaria',
+    [LOCALE.es]: 'Seleccione una franja horaria',
     [LOCALE.fr]: 'Sélectionner un fuseau horaire',
     [LOCALE.it]: 'Seleziona un fuso orario',
     [LOCALE.nl]: 'Selecteer een tijdzone',
     [LOCALE.pl]: 'Wybierz strefę czasową',
-    [LOCALE.pt]: 'Selecione um fuso horário',
+    [LOCALE.pt]: 'Selecionar um fuso horário',
   },
 };
 

8048947b6dbbc7c04649b3ad8f2438dd96bf9225
Charles-Antoine Alba <poro@variables.fr>
2025-08-21 10:35:36 +0200

    docs(tree-view): update technical information stories with OdsCanvas usage

--- packages/storybook/stories/components/tree-view/tree-view.stories.tsx
+++ packages/storybook/stories/components/tree-view/tree-view.stories.tsx
@@ -1,6 +1,11 @@
 import { type Meta, type StoryObj } from '@storybook/react';
 import React from 'react';
-import { TreeView, type TreeViewProp } from '../../../../ods-react/src/components/tree-view/src';
+import {
+  TreeView,
+  type TreeViewProp,
+  TreeViewNode,
+  type TreeViewValueChangeDetail,
+} from '../../../../ods-react/src/components/tree-view/src';
 import { CONTROL_CATEGORY } from '../../../src/constants/controls';
 import { orderControls } from '../../../src/helpers/controls';
 
@@ -8,11 +13,32 @@ type Story = StoryObj<TreeViewProp>;
 
 const meta: Meta<TreeViewProp> = {
   component: TreeView,
-  // subcomponents: { TreeViewXxx }, // Uncomment if sub components, otherwise remove
-  title: 'ODS Components/TODO/TreeView',
+  subcomponents: { TreeViewNode },
+  title: 'React Components/TreeView',
 };
 
 export default meta;
+
+const sampleCollection = [
+  {
+    id: 'src',
+    name: 'src',
+    children: [
+      { id: 'app.tsx', name: 'app.tsx' },
+      { id: 'index.ts', name: 'index.ts' },
+      {
+        id: 'components',
+        name: 'components',
+        children: [
+          { id: 'Button.tsx', name: 'Button.tsx' },
+          { id: 'Card.tsx', name: 'Card.tsx' },
+        ],
+      },
+    ],
+  },
+  { id: 'package.json', name: 'package.json' },
+  { id: 'readme.md', name: 'README.md' },
+];
 
 export const Demo: Story = {
   argTypes: orderControls({
@@ -32,7 +58,51 @@ export const Demo: Story = {
 
 export const Default: Story = {
   tags: ['!dev'],
-  render: ({}) => (
-    <TreeView />
-  ),
+  render: ({}) => {
+    const collection = sampleCollection;
+
+    return (
+      <TreeView
+        onValueChange={(d: TreeViewValueChangeDetail) => console.log('onValueChange', d)}>
+        { collection.map((item) => (
+          <TreeViewNode key={ item.id } item={ item } />
+        )) }
+      </TreeView>
+    );
+  }
 };
+
+export const Overview: Story = {
+  tags: ['!dev'],
+  render: ({}) => {
+    const collection = sampleCollection;
+
+    return (
+      <TreeView
+        onValueChange={(d: TreeViewValueChangeDetail) => console.log('onValueChange', d)}>
+        { collection.map((item) => (
+          <TreeViewNode key={ item.id } item={ item } />
+        )) }
+      </TreeView>
+    );
+  }
+}
+
+export const Multiple: Story = {
+  globals: {
+    imports: `import { TreeView, type TreeViewProp, TreeViewNode, type TreeViewValueChangeDetail } from '@ovhcloud/ods-react';`,
+  },
+  tags: ['!dev'],
+  render: ({}) => {
+    const collection = sampleCollection;
+    return (
+      <TreeView
+        multiple
+        onValueChange={(d: TreeViewValueChangeDetail) => console.log('onValueChange', d)}>
+        { collection.map((item) => (
+          <TreeViewNode key={ item.id } item={ item } />
+        )) }
+      </TreeView>
+    )
+  }
+}

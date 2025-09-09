10c56cfa2da49e473dd78b967513ba7fbe7b7d26
Damien Pellier <dpellier@gmail.com>
2025-09-09 16:59:16 +0200

    feat(theme): expose variable font file & normalizer

--- packages/examples/vite-app/src/index.tsx
+++ packages/examples/vite-app/src/index.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app/App';
-import '@ovhcloud/ods-themes/default';
+import '@ovhcloud/ods-themes/default/css';
+import '@ovhcloud/ods-themes/default/fonts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

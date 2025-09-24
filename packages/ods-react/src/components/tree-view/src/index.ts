67aec3d2d6cf701b7717fc09774b4845a06eca4d
Charles-Antoine Alba <poro@variables.fr>
2025-09-24 11:30:05 +0200

    feat(tree-view): update typings, enhance callback details, and simplify stories

--- packages/ods-react/src/components/tree-view/src/index.ts
+++ packages/ods-react/src/components/tree-view/src/index.ts
@@ -1,4 +1,4 @@
-export { TreeView, type TreeViewProp, type TreeViewValueChangeDetail } from './components/tree-view/TreeView';
+export { TreeView, type TreeViewProp, type TreeViewExpandedChangeDetail, type TreeViewValueChangeDetail } from './components/tree-view/TreeView';
 export { TreeViewNode, type TreeViewNodeProp } from './components/tree-view-node/TreeViewNode';
 export { TreeViewNodes, type TreeViewNodesProp } from './components/tree-view-nodes/TreeViewNodes';
 export { type TreeViewCustomRendererArg, type TreeViewItem } from './contexts/useTreeView';

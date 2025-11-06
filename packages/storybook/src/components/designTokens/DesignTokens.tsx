812155f86747d31e9682f43d2547f91ad25d6c41
Charles-Antoine Alba <poro@variables.fr>
2025-11-06 15:53:49 +0100

    feat(theme-generator): add ThemeGeneratorInput component and enhance token categorization

--- packages/storybook/src/components/designTokens/DesignTokens.tsx
+++ packages/storybook/src/components/designTokens/DesignTokens.tsx
@@ -2,76 +2,15 @@ import { BADGE_COLOR, Badge } from '@ovhcloud/ods-react';
 import lightThemeTokens from '@ovhcloud/ods-themes/default/tokens';
 import { CodeOrSourceMdx } from '@storybook/blocks';
 import React, { useMemo } from 'react';
-import { MONO_COLORS, PALETTES, TOKEN_CATEGORY, TOKEN_TYPE, type Token } from '../../constants/designTokens';
-import { guessTokenType } from '../../helpers/designTokens';
+import { MONO_COLORS, PALETTES, TOKEN_CATEGORY, type Token } from '../../constants/designTokens';
+import { categorizeTokens, splitPalettes } from '../../helpers/designTokens';
 import { Heading } from '../heading/Heading';
 import { ColorPalette } from './colorPalette/ColorPalette';
 import { TokensTable } from './tokensTable/TokensTable';
 
-function isPalette(name: string): boolean {
-  const paletteNameSplits = name.split(/^--ods-color-([a-z]+)(-.)*/);
-
-  if (paletteNameSplits.length > 0) {
-    return MONO_COLORS.indexOf(paletteNameSplits[1]) > -1 || PALETTES.indexOf(paletteNameSplits[1]) > -1;
-  }
-
-  return false;
-}
-
-function splitPalettes(tokens: Token[]): Token[][] {
-  let paletteIdx = 0;
-
-  return tokens.reduce<Token[][]>((res, token, idx, list) => {
-    if (idx) {
-      const paletteName = token.name.split(/-\d{3}$/)[0];
-      const previousPaletteName = list[idx - 1].name.split(/-\d{3}$/)[0];
-
-      if (previousPaletteName !== paletteName) {
-        paletteIdx++;
-        res.push([]);
-      }
-    }
-    res[paletteIdx].push(token);
-    return res;
-  }, [[]]);
-}
-
 const DesignTokens = () => {
   const tokenByCategory = useMemo(() => {
-    return Object.entries(lightThemeTokens.root).reduce<Record<TOKEN_CATEGORY, Token[]>>((res, [name, value]) => {
-      if (name.startsWith('--ods-color')) {
-        if (isPalette(name)) {
-          res[TOKEN_CATEGORY.palette].push({ name, type: TOKEN_TYPE.palette, value });
-        } else {
-          res[TOKEN_CATEGORY.deprecated].push({ name, type: guessTokenType(name), value });
-        }
-      } else if (!name.startsWith('--ods-theme')) {
-        res[TOKEN_CATEGORY.deprecated].push({ name, type: guessTokenType(name), value });
-      } else if (name.startsWith('--ods-theme-font-family')) {
-        res[TOKEN_CATEGORY.fontFamily].push({ name, type: TOKEN_TYPE.fontFamily, value });
-      } else if (name.startsWith('--ods-theme-input')) {
-        res[TOKEN_CATEGORY.formElement].push({ name, type: guessTokenType(name), value });
-      } else if (name.startsWith('--ods-theme-outline')) {
-        res[TOKEN_CATEGORY.outline].push({ name, type: TOKEN_TYPE.outline, value });
-      } else if (name.startsWith('--ods-theme-backdrop') || name.startsWith('--ods-theme-overlay')) {
-        res[TOKEN_CATEGORY.overlay].push({ name, type: guessTokenType(name), value });
-      } else if (!name.includes('-color')) {
-        res[TOKEN_CATEGORY.spacing].push({ name, type: guessTokenType(name), value });
-      } else {
-        res[TOKEN_CATEGORY.color].push({ name, type: TOKEN_TYPE.color, value });
-      }
-
-      return res;
-    }, {
-      [TOKEN_CATEGORY.color]: [],
-      [TOKEN_CATEGORY.deprecated]: [],
-      [TOKEN_CATEGORY.fontFamily]: [],
-      [TOKEN_CATEGORY.formElement]: [],
-      [TOKEN_CATEGORY.outline]: [],
-      [TOKEN_CATEGORY.overlay]: [],
-      [TOKEN_CATEGORY.palette]: [],
-      [TOKEN_CATEGORY.spacing]: [],
-    });
+    return categorizeTokens(lightThemeTokens.root);
   }, [lightThemeTokens.root]);
 
   const { monoColors, palettes } = useMemo(() => {

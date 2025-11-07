bb91957aad5bd0d0ac352e96faa9d901a84ad3fe
Charles-Antoine Alba <poro@variables.fr>
2025-11-07 15:32:47 +0100

    refactor(theme-generator): improve theme preview rendering and color handling

--- packages/storybook/src/components/themeGenerator/themeGeneratorPreview/ThemeGeneratorPreview.tsx
+++ packages/storybook/src/components/themeGenerator/themeGeneratorPreview/ThemeGeneratorPreview.tsx
@@ -1,59 +1,232 @@
-import React from 'react';
-import { StoryGrid } from '../../storyGrid/StoryGrid';
+import { composeStories } from '@storybook/react';
+import * as React from 'react';
+import * as AccordionStories from '../../../../stories/components/accordion/accordion.stories';
+import * as BadgeStories from '../../../../stories/components/badge/badge.stories';
+import * as BreadcrumbStories from '../../../../stories/components/breadcrumb/breadcrumb.stories';
+import * as ButtonStories from '../../../../stories/components/button/button.stories';
+import * as CardStories from '../../../../stories/components/card/card.stories';
+import * as CheckboxStories from '../../../../stories/components/checkbox/checkbox.stories';
+import * as ClipboardStories from '../../../../stories/components/clipboard/clipboard.stories';
+import * as CodeStories from '../../../../stories/components/code/code.stories';
+import * as ComboboxStories from '../../../../stories/components/combobox/combobox.stories';
+import * as DatepickerStories from '../../../../stories/components/datepicker/datepicker.stories';
+import * as DividerStories from '../../../../stories/components/divider/divider.stories';
+import * as DrawerStories from '../../../../stories/components/drawer/drawer.stories';
+import * as FileUploadStories from '../../../../stories/components/file-upload/file-upload.stories';
+import * as FormFieldStories from '../../../../stories/components/form-field/form-field.stories';
+import * as IconStories from '../../../../stories/components/icon/icon.stories';
+import * as InputStories from '../../../../stories/components/input/input.stories';
+import * as LinkStories from '../../../../stories/components/link/link.stories';
+import * as MediumStories from '../../../../stories/components/medium/medium.stories';
+import * as MessageStories from '../../../../stories/components/message/message.stories';
+import * as MeterStories from '../../../../stories/components/meter/meter.stories';
+import * as ModalStories from '../../../../stories/components/modal/modal.stories';
+import * as PaginationStories from '../../../../stories/components/pagination/pagination.stories';
+import * as PasswordStories from '../../../../stories/components/password/password.stories';
+import * as PhoneNumberStories from '../../../../stories/components/phone-number/phone-number.stories';
+import * as PopoverStories from '../../../../stories/components/popover/popover.stories';
+import * as ProgressBarStories from '../../../../stories/components/progress-bar/progress-bar.stories';
+import * as QuantityStories from '../../../../stories/components/quantity/quantity.stories';
+import * as RadioGroupStories from '../../../../stories/components/radio-group/radio-group.stories';
+import * as RangeStories from '../../../../stories/components/range/range.stories';
+import * as SelectStories from '../../../../stories/components/select/select.stories';
+import * as SkeletonStories from '../../../../stories/components/skeleton/skeleton.stories';
+import * as SpinnerStories from '../../../../stories/components/spinner/spinner.stories';
+import * as SwitchStories from '../../../../stories/components/switch/switch.stories';
+import * as TableStories from '../../../../stories/components/table/table.stories';
+import * as TabsStories from '../../../../stories/components/tabs/tabs.stories';
+import * as TagStories from '../../../../stories/components/tag/tag.stories';
+import * as TextStories from '../../../../stories/components/text/text.stories';
+import * as TextareaStories from '../../../../stories/components/textarea/textarea.stories';
+import * as TimepickerStories from '../../../../stories/components/timepicker/timepicker.stories';
+import * as ToggleStories from '../../../../stories/components/toggle/toggle.stories';
+import * as TooltipStories from '../../../../stories/components/tooltip/tooltip.stories';
+import * as TreeViewStories from '../../../../stories/components/tree-view/tree-view.stories';
 import { REACT_COMPONENTS_TITLE } from '../../../constants/meta';
+import { ResetTheme } from '../../resetTheme/ResetTheme';
 
-interface ThemeGeneratorPreviewProps {
+type ThemeGeneratorPreviewProps = {
   themeVariables?: Record<string, string>;
+};
+
+const THEME_STORY_MODULES = {
+  Accordion: AccordionStories,
+  Badge: BadgeStories,
+  Breadcrumb: BreadcrumbStories,
+  Button: ButtonStories,
+  Card: CardStories,
+  Checkbox: CheckboxStories,
+  Clipboard: ClipboardStories,
+  Code: CodeStories,
+  Combobox: ComboboxStories,
+  Datepicker: DatepickerStories,
+  Divider: DividerStories,
+  Drawer: DrawerStories,
+  FileUpload: FileUploadStories,
+  FormField: FormFieldStories,
+  Icon: IconStories,
+  Input: InputStories,
+  Link: LinkStories,
+  Medium: MediumStories,
+  Message: MessageStories,
+  Meter: MeterStories,
+  Modal: ModalStories,
+  Pagination: PaginationStories,
+  Password: PasswordStories,
+  PhoneNumber: PhoneNumberStories,
+  Popover: PopoverStories,
+  ProgressBar: ProgressBarStories,
+  Quantity: QuantityStories,
+  RadioGroup: RadioGroupStories,
+  Range: RangeStories,
+  Select: SelectStories,
+  Skeleton: SkeletonStories,
+  Spinner: SpinnerStories,
+  Switch: SwitchStories,
+  Table: TableStories,
+  Tabs: TabsStories,
+  Tag: TagStories,
+  Text: TextStories,
+  Textarea: TextareaStories,
+  Timepicker: TimepickerStories,
+  Toggle: ToggleStories,
+  Tooltip: TooltipStories,
+  TreeView: TreeViewStories,
+} as const;
+
+type ThemeComponentKey = keyof typeof THEME_STORY_MODULES;
+
+type ThemePreviewItem = {
+  /** Matches the key used in THEME_STORY_MODULES / composed stories map. */
+  key: ThemeComponentKey;
+  /** Human readable name displayed above the preview. */
+  label: string;
+  /** Story `kind` used for contextual information (e.g. Storybook navigation). */
+  kind: string;
+};
+
+const THEME_PREVIEW_COMPONENTS: ThemePreviewItem[] = [
+  { key: 'Accordion', kind: REACT_COMPONENTS_TITLE.accordion, label: 'Accordion' },
+  { key: 'Badge', kind: REACT_COMPONENTS_TITLE.badge, label: 'Badge' },
+  { key: 'Breadcrumb', kind: REACT_COMPONENTS_TITLE.breadcrumb, label: 'Breadcrumb' },
+  { key: 'Button', kind: REACT_COMPONENTS_TITLE.button, label: 'Button' },
+  { key: 'Card', kind: REACT_COMPONENTS_TITLE.card, label: 'Card' },
+  { key: 'Checkbox', kind: REACT_COMPONENTS_TITLE.checkbox, label: 'Checkbox' },
+  { key: 'Clipboard', kind: REACT_COMPONENTS_TITLE.clipboard, label: 'Clipboard' },
+  { key: 'Code', kind: REACT_COMPONENTS_TITLE.code, label: 'Code' },
+  { key: 'Combobox', kind: REACT_COMPONENTS_TITLE.combobox, label: 'Combobox' },
+  { key: 'Datepicker', kind: REACT_COMPONENTS_TITLE.datepicker, label: 'Datepicker' },
+  { key: 'Divider', kind: REACT_COMPONENTS_TITLE.divider, label: 'Divider' },
+  { key: 'Drawer', kind: REACT_COMPONENTS_TITLE.drawer, label: 'Drawer' },
+  { key: 'FileUpload', kind: REACT_COMPONENTS_TITLE.fileUpload, label: 'File Upload' },
+  { key: 'FormField', kind: REACT_COMPONENTS_TITLE.formField, label: 'Form Field' },
+  { key: 'Icon', kind: REACT_COMPONENTS_TITLE.icon, label: 'Icon' },
+  { key: 'Input', kind: REACT_COMPONENTS_TITLE.input, label: 'Input' },
+  { key: 'Link', kind: REACT_COMPONENTS_TITLE.link, label: 'Link' },
+  { key: 'Medium', kind: REACT_COMPONENTS_TITLE.medium, label: 'Medium' },
+  { key: 'Message', kind: REACT_COMPONENTS_TITLE.message, label: 'Message' },
+  { key: 'Meter', kind: REACT_COMPONENTS_TITLE.meter, label: 'Meter' },
+  { key: 'Modal', kind: REACT_COMPONENTS_TITLE.modal, label: 'Modal' },
+  { key: 'Pagination', kind: REACT_COMPONENTS_TITLE.pagination, label: 'Pagination' },
+  { key: 'Password', kind: REACT_COMPONENTS_TITLE.password, label: 'Password' },
+  { key: 'PhoneNumber', kind: REACT_COMPONENTS_TITLE.phoneNumber, label: 'Phone Number' },
+  { key: 'Popover', kind: REACT_COMPONENTS_TITLE.popover, label: 'Popover' },
+  { key: 'ProgressBar', kind: REACT_COMPONENTS_TITLE.progressBar, label: 'Progress Bar' },
+  { key: 'Quantity', kind: REACT_COMPONENTS_TITLE.quantity, label: 'Quantity' },
+  { key: 'RadioGroup', kind: REACT_COMPONENTS_TITLE.radioGroup, label: 'RadioGroup' },
+  { key: 'Range', kind: REACT_COMPONENTS_TITLE.range, label: 'Range' },
+  { key: 'Select', kind: REACT_COMPONENTS_TITLE.select, label: 'Select' },
+  { key: 'Skeleton', kind: REACT_COMPONENTS_TITLE.skeleton, label: 'Skeleton' },
+  { key: 'Spinner', kind: REACT_COMPONENTS_TITLE.spinner, label: 'Spinner' },
+  { key: 'Switch', kind: REACT_COMPONENTS_TITLE.switch, label: 'Switch' },
+  { key: 'Table', kind: REACT_COMPONENTS_TITLE.table, label: 'Table' },
+  { key: 'Tabs', kind: REACT_COMPONENTS_TITLE.tabs, label: 'Tabs' },
+  { key: 'Tag', kind: REACT_COMPONENTS_TITLE.tag, label: 'Tag' },
+  { key: 'Text', kind: REACT_COMPONENTS_TITLE.text, label: 'Text' },
+  { key: 'Textarea', kind: REACT_COMPONENTS_TITLE.textarea, label: 'Textarea' },
+  { key: 'Timepicker', kind: REACT_COMPONENTS_TITLE.timepicker, label: 'Timepicker' },
+  { key: 'Toggle', kind: REACT_COMPONENTS_TITLE.toggle, label: 'Toggle' },
+  { key: 'Tooltip', kind: REACT_COMPONENTS_TITLE.tooltip, label: 'Tooltip' },
+  { key: 'TreeView', kind: REACT_COMPONENTS_TITLE.treeView, label: 'Tree View' },
+];
+
+const COMPOSED_THEME_STORIES: Partial<Record<ThemeComponentKey, React.ComponentType>> = Object.entries(THEME_STORY_MODULES)
+  .reduce((acc, [key, module]) => {
+    const composedStories = composeStories(module as Parameters<typeof composeStories>[0]) as Record<string, React.ComponentType>;
+    const story = composedStories.ThemeGenerator as React.ComponentType | undefined;
+
+    if (story) {
+      acc[key as ThemeComponentKey] = story;
+    }
+
+    return acc;
+  }, {} as Partial<Record<ThemeComponentKey, React.ComponentType>>);
+
+function VariablesScope({
+  themeClass,
+  themeVariables,
+  children,
+}: {
+  themeClass: string;
+  themeVariables?: Record<string, string>;
+  children: React.ReactElement;
+}): React.ReactElement {
+  const scopedStyle = React.useMemo(() => (
+    themeVariables ? (themeVariables as React.CSSProperties) : undefined
+  ), [themeVariables]);
+
+  return (
+    <div className={ themeClass } style={ scopedStyle }>
+      <ResetTheme style={{ backgroundColor: 'transparent' }}>
+        { children }
+      </ResetTheme>
+    </div>
+  );
 }
 
-const ThemeGeneratorPreview = ({ themeVariables }: ThemeGeneratorPreviewProps): JSX.Element => {
-  return <StoryGrid
-    themeClass="theme-generator-preview"
-    themeVariables={themeVariables}
-    components={ [
-      { kind: REACT_COMPONENTS_TITLE.accordion, name: 'Accordion', stories: [ { id: 'react-components-accordion--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.badge, name: 'Badge', stories: [ { id: 'react-components-badge--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.breadcrumb, name: 'Breadcrumb', stories: [ { id: 'react-components-breadcrumb--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.button, name: 'Button', stories: [ { id: 'react-components-button--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.card, name: 'Card', stories: [ { id: 'react-components-card--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.checkbox, name: 'Checkbox', stories: [ { id: 'react-components-checkbox--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.clipboard, name: 'Clipboard', stories: [ { id: 'react-components-clipboard--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.code, name: 'Code', stories: [ { id: 'react-components-code--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.combobox, name: 'Combobox', stories: [ { id: 'react-components-combobox--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.datepicker, name: 'Datepicker', stories: [ { id: 'react-components-datepicker--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.divider, name: 'Divider', stories: [ { id: 'react-components-divider--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.drawer, name: 'Drawer', stories: [ { id: 'react-components-drawer--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.fileUpload, name: 'File Upload', stories: [ { id: 'react-components-file-upload--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.formField, name: 'Form Field', stories: [ { id: 'react-components-form-field--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.icon, name: 'Icon', stories: [ { id: 'react-components-icon--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.input, name: 'Input', stories: [ { id: 'react-components-input--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.link, name: 'Link', stories: [ { id: 'react-components-link--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.medium, name: 'Medium', stories: [ { id: 'react-components-medium--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.message, name: 'Message', stories: [ { id: 'react-components-message--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.meter, name: 'Meter', stories: [ { id: 'react-components-meter--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.modal, name: 'Modal', stories: [ { id: 'react-components-modal--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.pagination, name: 'Pagination', stories: [ { id: 'react-components-pagination--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.password, name: 'Password', stories: [ { id: 'react-components-password--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.phoneNumber, name: 'Phone Number', stories: [ { id: 'react-components-phone-number--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.popover, name: 'Popover', stories: [ { id: 'react-components-popover--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.progressBar, name: 'Progress Bar', stories: [ { id: 'react-components-progress-bar--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.quantity, name: 'Quantity', stories: [ { id: 'react-components-quantity--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.radioGroup, name: 'RadioGroup', stories: [ { id: 'react-components-radio-group--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.range, name: 'Range', stories: [ { id: 'react-components-range--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.select, name: 'Select', stories: [ { id: 'react-components-select--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.skeleton, name: 'Skeleton', stories: [ { id: 'react-components-skeleton--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.spinner, name: 'Spinner', stories: [ { id: 'react-components-spinner--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.switch, name: 'Switch', stories: [ { id: 'react-components-switch--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.table, name: 'Table', stories: [ { id: 'react-components-table--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.tabs, name: 'Tabs', stories: [ { id: 'react-components-tabs--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.tag, name: 'Tag', stories: [ { id: 'react-components-tag--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.text, name: 'Text', stories: [ { id: 'react-components-text--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.textarea, name: 'Textarea', stories: [ { id: 'react-components-textarea--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.timepicker, name: 'Timepicker', stories: [ { id: 'react-components-timepicker--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.toggle, name: 'Toggle', stories: [ { id: 'react-components-toggle--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.tooltip, name: 'Tooltip', stories: [ { id: 'react-components-tooltip--theme-generator', label: 'ThemeGenerator' } ] },
-      { kind: REACT_COMPONENTS_TITLE.treeView, name: 'Tree View', stories: [ { id: 'react-components-tree-view--theme-generator', label: 'ThemeGenerator' } ] },
-  ] } />;
+const ThemeGeneratorPreview = ({ themeVariables }: ThemeGeneratorPreviewProps): React.ReactElement => {
+  const renderSectionHeading = (label: string, description?: string): React.ReactElement => (
+    <header style={{ marginBottom: '0.75rem' }}>
+      <h3 style={{ margin: 0 }}>{ label }</h3>
+      { description ? (
+        <p style={{ color: 'var(--ods-theme-text-color-muted, #516173)', margin: '0.25rem 0 0' }}>
+          { description }
+        </p>
+      ) : null }
+    </header>
+  );
+
+  return (
+    <div>
+      <section style={{ marginBottom: '2rem' }}>
+        { renderSectionHeading('Theme previews') }
+
+        <div style={{ display: 'grid', gap: '1.5rem' }}>
+          {
+            THEME_PREVIEW_COMPONENTS.map(({ key, label }) => {
+              const Composed = COMPOSED_THEME_STORIES[key];
+
+              if (!Composed) {
+                return null;
+              }
+
+              return (
+                <div key={`compose-${key}`}>
+                  <h4 style={{ marginBottom: '0.5rem', marginTop: 0 }}>{ label }</h4>
+
+                  <VariablesScope
+                    themeClass="theme-generator-preview"
+                    themeVariables={ themeVariables }>
+                    <Composed />
+                  </VariablesScope>
+                </div>
+              );
+            })
+          }
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export { ThemeGeneratorPreview };

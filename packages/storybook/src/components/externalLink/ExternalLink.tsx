189cdc84a2379101a739051fc9c3d25eabf269dc
Damien Pellier <dpellier@gmail.com>
2025-11-25 14:30:04 +0100

    fix(doc): add origin to relative path to make CI deployment works

--- packages/storybook/src/components/externalLink/ExternalLink.tsx
+++ packages/storybook/src/components/externalLink/ExternalLink.tsx
@@ -1,10 +1,28 @@
 import { ICON_NAME, Icon, Link, type LinkProp } from '@ovhcloud/ods-react';
-import React, { type ComponentPropsWithRef, type JSX } from 'react';
+import React, { type ComponentPropsWithRef, type JSX, useMemo } from 'react';
 
-const ExternalLink = ({ children, ...prop }: LinkProp & Omit<ComponentPropsWithRef<'a'>, keyof LinkProp>): JSX.Element => {
+const ExternalLink = ({ children, href, ...prop }: LinkProp & Omit<ComponentPropsWithRef<'a'>, keyof LinkProp>): JSX.Element => {
+  const resolvedHref = useMemo(() => {
+    if (!href) {
+      return;
+    }
+
+    if (href.startsWith('http')) {
+      return href;
+    }
+
+    // Make relative paths works when deployed on CI
+    const pathNames = window.location.pathname.split('/');
+    if (['iframe.html', 'index.html'].indexOf(pathNames[pathNames.length - 1]) >= 0) {
+      pathNames.pop();
+    }
+    return `${window.location.origin}${pathNames.join('/')}${href.startsWith('/') ? href : `/${href}`}`;
+  }, [href, window.location]);
+
   return (
     <Link
       { ...prop }
+      href={ resolvedHref }
       target="_blank">
       { children } <Icon name={ ICON_NAME.externalLink } />
     </Link>

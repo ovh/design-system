b57ffe87d8906670cde27db1865b5e78a5600c77
Damien Pellier <dpellier@gmail.com>
2025-12-09 10:49:13 +0100

    feat(ods-recipes): add tags & types

--- packages/storybook/stories/recipes/app/toto/demos/location-tile/index.ts
+++
@@ -1,8 +0,0 @@
-import { createDemoWithVariants } from '../../../../../../src/helpers/createDemo';
-import CssModules from './css-modules';
-// import Tailwind from './tailwind';
-console.log('HERE')
-console.log(import.meta)
-//export const DemoAccordionHero = createDemoWithVariants(import.meta.url, { CssModules, Tailwind });
-//export const RecipeLocationTile = createDemoWithVariants('file://Users/dpellier/workspace_ods/design-system/packages/ods-recipes/src/components/location-tile/src/css-modules/index.tsx', { CssModules });
-export const RecipeLocationTile = createDemoWithVariants('file://./css-modules/index.tsx', { CssModules });

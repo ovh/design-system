5c7e857a6f425c2c1216aea17520b5c3a8ea44e4
Damien Pellier <dpellier@gmail.com>
2025-12-16 15:23:45 +0100

    feat(recipes): implement EmailField

---
+++ packages/ods-recipes/src/components/email-field/src/README.md
@@ -0,0 +1,4 @@
+# Email Field
+
+The Email Field helps users enter an email address more accurately by splitting it into two dedicated input fields
+separated by a read-only "@", clarifying the expected format and reducing input mistakes.
---
+++ packages/ods-recipes/src/components/email-field/src/index.ts
@@ -0,0 +1,21 @@
+import { EmailField as CssModule } from './css-modules';
+import { type ComponentMetadata, type ComponentRecipe } from '../../../constants/recipe';
+import { SEARCH_TAG } from '../../../constants/search-tag';
+
+const metadata: ComponentMetadata = {
+  name: 'Email Field',
+  reactTag: 'EmailField',
+  tags: [
+    SEARCH_TAG.form,
+    SEARCH_TAG.interactive,
+  ],
+};
+
+const EmailField: ComponentRecipe = {
+  CssModule,
+  metadata,
+};
+
+export {
+  EmailField,
+};

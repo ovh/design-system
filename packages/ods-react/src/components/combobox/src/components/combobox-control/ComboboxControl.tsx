7eea35006b1c4e8f30cd5fa5aef3d978ddecb87d
Damien Pellier <dpellier@gmail.com>
2025-12-19 11:28:31 +0100

    fix(combobox): prevent content close on scrollbar click

--- packages/ods-react/src/components/combobox/src/components/combobox-control/ComboboxControl.tsx
+++ packages/ods-react/src/components/combobox/src/components/combobox-control/ComboboxControl.tsx
@@ -87,8 +87,9 @@ const ComboboxControl: FC<ComboboxControlProp> = forwardRef(({
   }
 
   function handleBlur(e: FocusEvent): void {
-    if (elementParentHasAttribute(e.relatedTarget as HTMLElement, 'data-ods', ['combobox-content', 'combobox-control']) &&
-      elementParentHasAttribute(e.relatedTarget as HTMLElement, 'id', [contentId, controlId])) {
+    if (e.relatedTarget?.id === contentId
+      || (elementParentHasAttribute(e.relatedTarget as HTMLElement, 'data-ods', ['combobox-content', 'combobox-control']) &&
+      elementParentHasAttribute(e.relatedTarget as HTMLElement, 'id', [contentId, controlId]))) {
       return;
     }
 

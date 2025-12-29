5c1ac403cee3644a4d0d4ce05b8afe06d098a696
Damien Pellier <dpellier@gmail.com>
2025-12-29 14:05:07 +0100

    refactor(recipes): remove Price

--- packages/ods-recipes/src/components/index.ts
+++ packages/ods-recipes/src/components/index.ts
@@ -1,8 +1,6 @@
 export * from './email-field/src';
 export * from './feature-list/src';
-export * from './price/src';
 export * from './status-message/src';
-
 export * from './status-modal/src';
 export * from './media-product-card/src';
 export * from './order-button/src';

7d91b7aeb0ddeede0ed305c73945d425e85fead1
Damien Pellier <dpellier@gmail.com>
2026-04-03 10:05:40 +0200

    feat(recipes): implement data-grid-with-query-filter

---
+++ packages/ods-recipes/src/components/data-grid-with-query-filter/src/dev.stories.tsx
@@ -0,0 +1,14 @@
+import { DataGridWithQueryFilter } from '.';
+
+export default {
+  component: DataGridWithQueryFilter,
+  title: 'DataGridWithQueryFilter dev',
+};
+
+export const CssModule = () => (
+  <DataGridWithQueryFilter.CssModule />
+);
+
+export const Tailwind = () => (
+  <DataGridWithQueryFilter.Tailwind />
+);

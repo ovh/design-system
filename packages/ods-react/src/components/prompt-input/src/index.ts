adb3e5931f21ae8deaaf5c9c9ffa4cd733ea2dc9
Charles Rotru <charles.rotru.ext@corp.ovh.com>
2026-04-14 18:14:17 +0200

    feat(promptInput): update e2e

--- packages/ods-react/src/components/prompt-input/src/index.ts
+++ packages/ods-react/src/components/prompt-input/src/index.ts
@@ -1,6 +1,11 @@
 export { PromptInput, type PromptInputProp } from './components/prompt-input/PromptInput';
 export { PromptInputControls, type PromptInputControlsProp } from './components/prompt-input-controls/PromptInputControls';
 export {
+  PromptInputFileUploadButton,
+  type PromptInputFileUploadButtonProp,
+} from './components/prompt-input-file-upload-button/PromptInputFileUploadButton';
+export { PromptInputFiles, type PromptInputFilesProp } from './components/prompt-input-files/PromptInputFiles';
+export {
   PromptInputSendButton,
   type PromptInputSendButtonProp,
 } from './components/prompt-input-send-button/PromptInputSendButton';
--- packages/ods-react/src/components/prompt-input/tests/rendering/prompt-input.e2e.ts
+++ packages/ods-react/src/components/prompt-input/tests/rendering/prompt-input.e2e.ts
@@ -1,21 +1,15 @@
 import 'jest-puppeteer';
 import { gotoStory } from '../../../../helpers/test';
 
-describe('PromptInput rendering', () => {
+describe('Menu rendering', () => {
   it('should render the component', async() => {
     await gotoStory(page, 'rendering/render');
 
-    expect(await page.waitForSelector('[data-ods="prompt-input"]')).not.toBeNull();
-  });
-
-  describe('custom style', () => {
-    it('should render with custom style applied', async() => {
-      await gotoStory(page, 'rendering/custom-style');
-
-      const promptInput = await page.waitForSelector('[data-ods="prompt-input"]');
-      const height = await promptInput?.evaluate((el: Element) => el.getBoundingClientRect().height);
-
-      expect(height).toBe(42);
-    });
+    expect(await page.waitForSelector('[data-testid="render-trigger"]')).not.toBeNull();
+    expect(await page.waitForSelector('[data-ods="prompt-input-files"]')).not.toBeNull();
+    expect(await page.waitForSelector('[data-ods="prompt-input-controls"]')).not.toBeNull();
+    expect(await page.waitForSelector('[data-ods="prompt-input-file-upload-button"]')).not.toBeNull();
+    expect(await page.waitForSelector('[data-ods="prompt-input-text-control"]')).not.toBeNull();
+    expect(await page.waitForSelector('[data-ods="prompt-input-send-button"]')).not.toBeNull();
   });
 });

4c9af2c43950949b56798cbe65a1d78934b0fd29
Charles Rotru <charles.rotru.ext@corp.ovh.com>
2026-04-24 10:28:53 +0200

    fix(PromptInput): sendButton enabme/disable behavior fix

--- packages/ods-react/src/components/prompt-input/tests/rendering/prompt-input.stories.tsx
+++ packages/ods-react/src/components/prompt-input/tests/rendering/prompt-input.stories.tsx
@@ -1,5 +1,6 @@
-import { PromptInput, PromptInputControls, PromptInputFileUploadButton, PromptInputSendButton, PromptInputTextControl } from '../../src';
-import { PromptInputFiles } from '../../src/components/prompt-input-files/PromptInputFiles';
+import { useState } from 'react';
+import { FileThumbnail } from '../../../file-thumbnail/src';
+import { PromptInput, PromptInputControls, PromptInputFileUploadButton, PromptInputFiles, PromptInputSendButton, PromptInputTextControl } from '../../src';
 
 export default {
   component: PromptInput,
@@ -19,3 +20,80 @@ export const render = (): JSX.Element => (
     </PromptInputControls>
   </PromptInput>
 );
+
+export const sendButtonDisabledNoTextNoFile = (): JSX.Element => (
+  <PromptInput>
+    <PromptInputControls>
+      <PromptInputFileUploadButton />
+      <PromptInputTextControl />
+      <PromptInputSendButton />
+    </PromptInputControls>
+  </PromptInput>
+);
+
+export const sendButtonEnabledWithText = (): JSX.Element => (
+  <PromptInput value="input text value">
+    <PromptInputControls>
+      <PromptInputFileUploadButton />
+      <PromptInputTextControl />
+      <PromptInputSendButton />
+    </PromptInputControls>
+  </PromptInput>
+);
+
+export const sendButtonEnabledWithFile = (): JSX.Element => (
+  <PromptInput fileCollection={[new File(['foo'], 'text-file.txt', { type: 'text/plain' })]}>
+    <PromptInputControls>
+      <PromptInputFileUploadButton />
+      <PromptInputTextControl />
+      <PromptInputSendButton />
+    </PromptInputControls>
+  </PromptInput>
+);
+
+export const sendButtonEnabledWithTextAndFile = (): JSX.Element => (
+  <PromptInput value="input text value" fileCollection={[new File(['foo'], 'text-file.txt', { type: 'text/plain' })]}>
+    <PromptInputControls>
+      <PromptInputFileUploadButton />
+      <PromptInputTextControl />
+      <PromptInputSendButton />
+    </PromptInputControls>
+  </PromptInput>
+);
+
+export const sendButtonEnablesOnFileAdd = (): JSX.Element => {
+  const [fileCollection, setFileCollection] = useState<File[]>([]);
+
+  return (
+    <PromptInput fileCollection={fileCollection} onFileChange={({ files }) => setFileCollection(files)}>
+      <PromptInputControls>
+        <PromptInputFileUploadButton />
+        <PromptInputTextControl />
+        <PromptInputSendButton />
+      </PromptInputControls>
+    </PromptInput>
+  );
+};
+
+export const sendButtonDisablesOnFileRemove = (): JSX.Element => {
+  const [fileCollection, setFileCollection] = useState<File[]>([new File(['foo'], 'text-file.txt', { type: 'text/plain' })]);
+
+  return (
+    <PromptInput fileCollection={fileCollection} onFileChange={({ files }) => setFileCollection(files)}>
+      <PromptInputFiles>
+        {fileCollection.map((file, index) => (
+          <FileThumbnail
+            key={index}
+            file={file}
+            onFileRemove={() => setFileCollection((prev) => prev.filter((_, i) => i !== index))}
+          />
+        ))}
+      </PromptInputFiles>
+      <PromptInputControls>
+        <PromptInputFileUploadButton />
+        <PromptInputTextControl />
+        <PromptInputSendButton />
+      </PromptInputControls>
+    </PromptInput>
+  );
+};

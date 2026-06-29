80bab785208ed13f71df8ab2e12031ec16ea693c
Charles Rotru <charles.rotru.ext@corp.ovh.com>
2026-06-29 17:14:07 +0200

    chore(deps): update all example packages dependencies

--- packages/examples/vite-app/vite.config.ts
+++ packages/examples/vite-app/vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom'],
+  },
   server: {
     port: 8080,
   },
